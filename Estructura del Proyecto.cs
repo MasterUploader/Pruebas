33d6f60fcf784065cb6ac9a5c63bd6fe8bb5c580
MasterUploader <MasterUploader@users.noreply.github.com>
2025-03-07 17:47:28 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -2,45 +2,13 @@
     ID="SqlDBGridView" 
     runat="server" 
     ConnectionString="<%$ ConnectionStrings:TuConexion %>" 
+    SelectCommand="SELECT CODCCO, NOMAGE FROM BCAH96DTA.RSAGE01"
     UpdateCommand="UPDATE BCAH96DTA.RSAGE01 
-                   SET NOMAGE = @descripcion, 
-                       zona = @zona, 
-                       marquesina = @marque, 
-                       rstbranch = @rstbrch, 
-                       NOMBD = @nombd 
-                   WHERE CODCCO = @codcco">
+                   SET NOMAGE = @Nombre 
+                   WHERE CODCCO = @CODCCO">
+    
     <UpdateParameters>
-        <asp:Parameter Name="descripcion" Type="String" />
-        <asp:Parameter Name="zona" Type="Int32" />
-        <asp:Parameter Name="marque" Type="String" />
-        <asp:Parameter Name="rstbrch" Type="String" />
-        <asp:Parameter Name="nombd" Type="String" />
-        <asp:Parameter Name="codcco" Type="Int32" />
+        <asp:Parameter Name="Nombre" Type="String" />
+        <asp:Parameter Name="CODCCO" Type="Int32" />
     </UpdateParameters>
 </asp:SqlDataSource>
-
-
-private bool actualizarAgencia(int codcco, string descripcion, int zona, string marque, 
-                               string rstbrch, string nombd)
-{
-    try
-    {
-        // Asigna los valores a los parámetros del SqlDataSource
-        SqlDBGridView.UpdateParameters["descripcion"].DefaultValue = descripcion;
-        SqlDBGridView.UpdateParameters["zona"].DefaultValue = zona.ToString();
-        SqlDBGridView.UpdateParameters["marque"].DefaultValue = marque;
-        SqlDBGridView.UpdateParameters["rstbrch"].DefaultValue = rstbrch;
-        SqlDBGridView.UpdateParameters["nombd"].DefaultValue = nombd;
-        SqlDBGridView.UpdateParameters["codcco"].DefaultValue = codcco.ToString();
-
-        // Ejecutar actualización
-        SqlDBGridView.Update();
-
-        return true; // Si no hay errores, retorna true
-    }
-    catch (Exception ex)
-    {
-        error = ex.Message;
-        return false;
-    }
-}
