6883ec43de16ce6a5b772f22fb5a8ab71c88992f
MasterUploader <MasterUploader@users.noreply.github.com>
2025-03-14 09:32:23 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,26 +1,21 @@
 <script src="Scripts/jquery-1.7.1.min.js"></script>
 <script>
     $(document).ready(function () {
-        iniciarEventos();
-    });
-
-    // Detectar actualizaciones en el UpdatePanel y reactivar eventos
-    Sys.WebForms.PageRequestManager.getInstance().add_endRequest(function () {
-        iniciarEventos(); // Volver a adjuntar eventos después de cada PostBack
-        $("#loadingScreen").fadeOut();
-        $("#btnResetear").prop("disabled", false);
-    });
-
-    function iniciarEventos() {
-        // Mostrar pantalla de carga cuando el usuario cambia de sucursal
-        $("#ddlAgencias").change(function () {
-            $("#loadingScreen").fadeIn();
-        });
-
-        // Mostrar pantalla de carga cuando se presiona el botón Resetear
+        // Mostrar ventana de carga cuando se presiona el botón Resetear
         $("#btnResetear").click(function () {
             $("#loadingScreen").fadeIn();
             $(this).prop("disabled", true);
         });
-    }
+
+        // Mostrar ventana de carga cuando se selecciona una sucursal en el DropDownList
+        $("#ddlAgencias").change(function () {
+            $("#loadingScreen").fadeIn();
+        });
+
+        // Ocultar la ventana de carga cuando la respuesta del servidor llega
+        Sys.WebForms.PageRequestManager.getInstance().add_endRequest(function () {
+            $("#loadingScreen").fadeOut();
+            $("#btnResetear").prop("disabled", false);
+        });
+    });
 </script>
