8f94498c9cc4ec8a089decfe4d89eaf0a976c694
MasterUploader <MasterUploader@users.noreply.github.com>
2025-03-20 16:01:05 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,10 +1,10 @@
 /// <summary>
-/// Representa una solicitud para obtener la tasa de cambio mayorista.
+/// Representa una solicitud para obtener la tasa de cambio extranjera.
 /// </summary>
 [XmlRoot(ElementName = "REQUEST")]
-public class GetWholesaleExchangeRateRequest : BaseRequest
+public class GetForeignExchangeRateRequest : BaseRequest
 {
-    public GetWholesaleExchangeRateRequest() { Type = "GET_WHOLESALE_EXCHANGE_RATE"; }
+    public GetForeignExchangeRateRequest() { Type = "FOREIGN_EXCHANGE_RATE"; }
 
     [XmlElement(ElementName = "AGENT_TRANS_TYPE_CODE")]
     public string AgentTransactionTypeCode { get; set; }
@@ -12,24 +12,6 @@ public class GetWholesaleExchangeRateRequest : BaseRequest
     [XmlElement(ElementName = "AGENT_CD")]
     public string AgentCode { get; set; }
 
-    [XmlElement(ElementName = "ORIG_COUNTRY_CD")]
-    public string OriginCountryCode { get; set; }
-
-    [XmlElement(ElementName = "ORIG_CURRENCY_CD")]
-    public string OriginCurrencyCode { get; set; }
-
-    [XmlElement(ElementName = "SERVICE_CD")]
-    public string ServiceCode { get; set; }
-
-    [XmlElement(ElementName = "DEST_COUNTRY_CD")]
-    public string DestinationCountryCode { get; set; }
-
-    [XmlElement(ElementName = "DEST_CURRENCY_CD")]
-    public string DestinationCurrencyCode { get; set; }
-
-    [XmlElement(ElementName = "PAYMENT_TYPE_CD")]
-    public string PaymentTypeCode { get; set; }
-
-    [XmlElement(ElementName = "PAY_AGENT_CD")]
-    public string PaymentAgentCode { get; set; }
+    [XmlElement(ElementName = "CURRENCY_CD")]
+    public string BaseCurrencyCode { get; set; }
 }
