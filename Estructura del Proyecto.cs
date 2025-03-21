76625b5d25cc466d68cf1e861469d836fb48cb44
MasterUploader <MasterUploader@users.noreply.github.com>
2025-03-21 11:40:33 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,35 +1,14 @@
-private string SerializeToXml<T>(T obj)
+if (obj is BaseRequest baseRequest)
 {
-    try
+    baseRequest.Type = obj switch
     {
-        XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
-        ns.Add("xsi", "http://www.w3.org/2001/XMLSchema-instance");
-        ns.Add("gp", "http://www.btsincusa.com/gp");
-
-        // Forzar que el campo Type tenga el valor correcto antes de serializar
-        if (obj is BaseRequest baseRequest)
-        {
-            baseRequest.Type = obj.GetType().Name; 
-        }
-
-        XmlSerializer serializer = new XmlSerializer(typeof(T), new Type[]
-        {
-            typeof(GetServiceRequest),
-            typeof(GetForeignExchangeRateRequest),
-            typeof(GetIdentificationsRequest),
-            typeof(GetPaymentAgentsRequest),
-            typeof(GetProductsRequest),
-            typeof(GetWholesaleExchangeRateRequest)
-        });
-
-        using (StringWriter stringWriter = new StringWriter())
-        {
-            serializer.Serialize(stringWriter, obj, ns);
-            return stringWriter.ToString();
-        }
-    }
-    catch (Exception ex)
-    {
-        throw new Exception($"Error serializando XML: {ex.Message}");
-    }
+        GetServiceRequest => "GetServiceRequest",
+        GetDataRequest => "GetDataRequest",
+        GetProductsRequest => "GetProductsRequest",
+        GetPaymentAgentsRequest => "GetPaymentAgentsRequest",
+        GetWholesaleExchangeRateRequest => "GetWholesaleExchangeRateRequest",
+        GetForeignExchangeRateRequest => "GetForeignExchangeRateRequest",
+        GetIdentificationsRequest => "GetIdentificationsRequest",
+        _ => throw new Exception("Tipo de solicitud desconocido")
+    };
 }
