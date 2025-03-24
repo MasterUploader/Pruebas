27bac675d165ed95fd24350f0146e206137cbe59
MasterUploader <MasterUploader@users.noreply.github.com>
2025-03-23 23:14:10 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,153 +1,63 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
 using System.Xml.Serialization;
 
-namespace Models.DTO.BTS.Common
+namespace Utilities
 {
     /// <summary>
-    /// Representa la sección de seguridad del encabezado SOAP (HEADER).
+    /// Proporciona métodos para serializar y deserializar objetos XML compatibles con SOAP.
     /// </summary>
-    [XmlRoot(ElementName = "SECURITY", Namespace = "http://www.btsincusa.com/gp")]
-    public class Security
+    public static class XmlHelper
     {
         /// <summary>
-        /// Código del usuario autenticado.
+        /// Serializa un objeto a una cadena XML codificada en UTF-8.
         /// </summary>
-        [XmlElement(ElementName = "UserCode")]
-        public string UserCode { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Contraseña del usuario.
-        /// </summary>
-        [XmlElement(ElementName = "Password")]
-        public string Password { get; set; } = string.Empty;
-    }
-}
-
-
-
-
-
-using System.Xml.Serialization;
-
-namespace Models.DTO.BTS.Common
-{
-    /// <summary>
-    /// Representa los parámetros base requeridos en cualquier solicitud SOAP.
-    /// </summary>
-    public class RequestBase
-    {
-        /// <summary>
-        /// Código del agente que realiza la solicitud.
-        /// </summary>
-        [XmlElement(ElementName = "AGENT_CD")]
-        public string AgentCode { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Código del tipo de transacción que se solicita ejecutar.
-        /// </summary>
-        [XmlElement(ElementName = "AGENT_TRANS_TYPE_CODE")]
-        public string AgentTransactionTypeCode { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Contenedor de datos adicionales de la solicitud.
-        /// Puede omitirse si no se requiere.
-        /// </summary>
-        [XmlElement(ElementName = "DATA")]
-        public string? Data { get; set; }
-    }
-}
-
-
-
-
-
-using Models.DTO.BTS.Common;
-using System.Xml.Serialization;
-
-namespace Models.DTO.BTS.ExecTR
-{
-    /// <summary>
-    /// Representa una solicitud de tipo ExecTR enviada en el cuerpo del mensaje SOAP.
-    /// </summary>
-    [XmlRoot(ElementName = "ExecTR", Namespace = "http://www.btsincusa.com/gp")]
-    public class ExecTRRequest
-    {
-        /// <summary>
-        /// Parámetros base requeridos por ExecTR.
-        /// </summary>
-        [XmlElement(ElementName = "Request")]
-        public RequestBase Request { get; set; } = new();
-    }
-}
-
-
-using Models.DTO.BTS.Common;
-using System.Xml.Serialization;
-
-namespace Models.DTO.BTS.GetData
-{
-    /// <summary>
-    /// Representa una solicitud de tipo GetData enviada en el cuerpo del mensaje SOAP.
-    /// </summary>
-    [XmlRoot(ElementName = "GetData", Namespace = "http://www.btsincusa.com/gp")]
-    public class GetDataRequest
-    {
-        /// <summary>
-        /// Parámetros base requeridos por GetData.
-        /// </summary>
-        [XmlElement(ElementName = "Request")]
-        public RequestBase Request { get; set; } = new();
-    }
-}
-
-
-using System.Xml.Serialization;
-
-namespace Models.DTO.BTS.Common
-{
-    /// <summary>
-    /// Representa el encabezado de la solicitud SOAP.
-    /// </summary>
-    public class Header
-    {
-        /// <summary>
-        /// Información de autenticación.
-        /// </summary>
-        [XmlElement(ElementName = "SECURITY", Namespace = "http://www.btsincusa.com/gp")]
-        public Security Security { get; set; } = new();
-    }
-}
-
-
-using System.Xml.Serialization;
-
-namespace Models.DTO.BTS.Common
-{
-    /// <summary>
-    /// Representa el sobre SOAP completo, incluyendo Header y Body.
-    /// Esta clase es genérica para admitir diferentes tipos de cuerpo como ExecTR o GetData.
-    /// </summary>
-    [XmlRoot(ElementName = "Envelope", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
-    public class Envelope<TBody>
-    {
-        [XmlNamespaceDeclarations]
-        public XmlSerializerNamespaces Xmlns { get; set; } = new();
-
-        public Envelope()
+        /// <typeparam name="T">Tipo del objeto a serializar.</typeparam>
+        /// <param name="obj">Instancia del objeto.</param>
+        /// <returns>Cadena en formato XML.</returns>
+        public static string SerializeToXml<T>(T obj)
         {
-            Xmlns.Add("soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
-            Xmlns.Add("gp", "http://www.btsincusa.com/gp");
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj), "El objeto no puede ser null.");
+
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            var settings = new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                Indent = true,
+                OmitXmlDeclaration = false
+            };
+
+            using var stringWriter = new Utf8StringWriter();
+            using var writer = XmlWriter.Create(stringWriter, settings);
+            xmlSerializer.Serialize(writer, obj);
+            return stringWriter.ToString();
         }
 
         /// <summary>
-        /// Encabezado SOAP con autenticación.
+        /// Deserializa una cadena XML a una instancia del tipo especificado.
         /// </summary>
-        [XmlElement(ElementName = "Header", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
-        public Header Header { get; set; } = new();
+        /// <typeparam name="T">Tipo de destino.</typeparam>
+        /// <param name="xml">XML como cadena.</param>
+        /// <returns>Instancia deserializada del tipo especificado.</returns>
+        public static T DeserializeFromXml<T>(string xml)
+        {
+            if (string.IsNullOrWhiteSpace(xml))
+                throw new ArgumentNullException(nameof(xml), "El XML no puede ser null o vacío.");
+
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            using var stringReader = new StringReader(xml);
+            return (T)xmlSerializer.Deserialize(stringReader)!;
+        }
 
         /// <summary>
-        /// Cuerpo SOAP, parametrizable por tipo de operación (ExecTR, GetData, etc.).
+        /// Implementación de StringWriter que asegura la codificación en UTF-8.
         /// </summary>
-        [XmlElement(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
-        public TBody Body { get; set; } = default!;
+        private class Utf8StringWriter : StringWriter
+        {
+            public override Encoding Encoding => Encoding.UTF8;
+        }
     }
 }
