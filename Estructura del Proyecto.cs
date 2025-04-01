a71121b46f347a8c9f645f258b301c733fa00a02
MasterUploader <MasterUploader@users.noreply.github.com>
2025-04-01 14:54:35 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,31 +1,93 @@
-// Prototipos para manejo IFS
-dcl-pr IFS_OPEN int(10) extproc('_C_IFS_open');
-  pathname pointer value;
-  openFlags int(10) value;
-  mode int(10) value;
-end-pr;
+**FREE
+//------------------------------------------------------------
+// Archivo: BTSCNSRDS_H.rpgle
+// Descripción: Estructuras de datos para mapear la respuesta
+//              del servicio de consulta (BTS Consulta)
+//------------------------------------------------------------
 
-dcl-pr IFS_WRITE int(10) extproc('_C_IFS_write');
-  fd int(10) value;
-  buffer pointer value;
-  length int(10) value;
-end-pr;
+dcl-ds HeaderDS qualified;
+  responseId      char(40);
+  timestamp       char(30);
+  processingTime  char(20);
+  statusCode      char(10);
+  message         char(100);
+end-ds;
 
-dcl-pr IFS_CLOSE int(10) extproc('_C_IFS_close');
-  fd int(10) value;
-end-pr;
+dcl-ds AddressDS qualified;
+  address    char(50);
+  city       char(30);
+  stateCd    char(5);
+  countryCd  char(5);
+  zipCode    char(10);
+  phone      char(20);
+end-ds;
 
+dcl-ds IdentificationDS qualified;
+  typeCd           char(10);
+  issuerCd         char(10);
+  issuerStateCd    char(10);
+  issuerCountryCd  char(10);
+  identifNm        char(30);
+  expirationDt     char(8);
+end-ds;
 
-dcl-c O_WRONLY 1;
-dcl-c O_CREAT 8;
-dcl-c O_TRUNC 512;
+dcl-ds PersonDS qualified;
+  firstName     char(20);
+  middleName    char(20);
+  lastName      char(20);
+  motherMName   char(20);
+  address       likeds(AddressDS);
+end-ds;
 
+dcl-ds RecipientDS qualified;
+  firstName     char(20);
+  middleName    char(20);
+  lastName      char(20);
+  motherMName   char(20);
+  identifTypeCd char(10);
+  identifNm     char(30);
+  foreignName   likeds(PersonDS);
+  address       likeds(AddressDS);
+end-ds;
 
+dcl-ds DataDetailDS qualified;
+  saleDt         char(8);
+  saleTm         char(6);
+  serviceCd      char(10);
+  paymentTypeCd  char(10);
+  origCountryCd  char(5);
+  origCurrencyCd char(5);
+  destCountryCd  char(5);
+  destCurrencyCd char(5);
+  originAm       char(20);
+  destinationAm  char(20);
+  exchRateFx     char(20);
+  marketRefCurrencyCd  char(5);
+  marketRefCurrencyAm  char(20);
+  sAgentCd       char(10);
+  rAccountTypeCd char(10);
+  rAccountNm     char(50);
+  rAgentCd       char(10);
+  rAgentRegionSd char(10);
+  rAgentBranchSd char(10);
+  sender         likeds(PersonDS);
+  recipient      likeds(RecipientDS);
+  senderIdent    likeds(IdentificationDS);
+  recipientIdent likeds(IdentificationDS);
+end-ds;
 
-filePath = %addr(vFullFile);
-fd = IFS_OPEN(filePath: O_WRONLY + O_CREAT + O_TRUNC: 438); // 438 = permisos 666 en decimal
+dcl-ds DataDS qualified;
+  opCode         char(10);
+  processMsg     char(100);
+  errorParamFullName char(100);
+  transStatusCd  char(10);
+  transStatusDt  char(8);
+  processDt      char(8);
+  processTm      char(6);
+  detail         likeds(DataDetailDS);
+end-ds;
 
-if fd >= 0;
-  rc = IFS_WRITE(fd: %addr(response): %len(%trim(response)));
-  callp IFS_CLOSE(fd);
-endif;
+dcl-ds FullResponseDS qualified;
+  header likeds(HeaderDS);
+  data   likeds(DataDS);
+end-ds;
