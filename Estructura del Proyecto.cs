ad4904ab336ca88edf31762e0da3efea5f94ea89
MasterUploader <MasterUploader@users.noreply.github.com>
2025-04-01 14:26:14 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,11 +1,31 @@
-**FREE
+// Prototipos para manejo IFS
+dcl-pr IFS_OPEN int(10) extproc('_C_IFS_open');
+  pathname pointer value;
+  openFlags int(10) value;
+  mode int(10) value;
+end-pr;
 
-// ========================================================
-// Constantes de flags para apertura de archivos en IFS
-// ========================================================
-dcl-c O_RDONLY 1;
-dcl-c O_WRONLY 2;
-dcl-c O_RDWR   4;
-dcl-c O_CREAT  8;
-dcl-c O_TRUNC 16;
-dcl-c O_APPEND 256;
+dcl-pr IFS_WRITE int(10) extproc('_C_IFS_write');
+  fd int(10) value;
+  buffer pointer value;
+  length int(10) value;
+end-pr;
+
+dcl-pr IFS_CLOSE int(10) extproc('_C_IFS_close');
+  fd int(10) value;
+end-pr;
+
+
+dcl-c O_WRONLY 1;
+dcl-c O_CREAT 8;
+dcl-c O_TRUNC 512;
+
+
+
+filePath = %addr(vFullFile);
+fd = IFS_OPEN(filePath: O_WRONLY + O_CREAT + O_TRUNC: 438); // 438 = permisos 666 en decimal
+
+if fd >= 0;
+  rc = IFS_WRITE(fd: %addr(response): %len(%trim(response)));
+  callp IFS_CLOSE(fd);
+endif;
