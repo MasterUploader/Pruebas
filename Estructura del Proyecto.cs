4d42a699f08218c1ac836e044d36871fe5b61dd2
MasterUploader <MasterUploader@users.noreply.github.com>
2025-04-01 16:58:39 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,93 +1,24 @@
-**FREE
-//------------------------------------------------------------
-// Archivo: BTSCNSRDS_H.rpgle
-// Descripción: Estructuras de datos para mapear la respuesta
-//              del servicio de consulta (BTS Consulta)
-//------------------------------------------------------------
+// ==============================================
+// Devuelve una cadena desde un nodo JSON
+// Si el nodo o valor es nulo, retorna *blanks
+// ==============================================
+dcl-proc SafeGetString;
+   dcl-pi *n char(100);
+      jsonPtr pointer value;
+   end-pi;
 
-dcl-ds HeaderDS qualified;
-  responseId      char(40);
-  timestamp       char(30);
-  processingTime  char(20);
-  statusCode      char(10);
-  message         char(100);
-end-ds;
+   // Validamos primero que el puntero no sea nulo
+   if jsonPtr <> *null;
 
-dcl-ds AddressDS qualified;
-  address    char(50);
-  city       char(30);
-  stateCd    char(5);
-  countryCd  char(5);
-  zipCode    char(10);
-  phone      char(20);
-end-ds;
+      // Luego validamos que yajl_get_string también devuelva un puntero no nulo
+      if yajl_get_string(jsonPtr) <> *null;
+         return %str(yajl_get_string(jsonPtr));
+      else;
+         return *blanks;
+      endif;
 
-dcl-ds IdentificationDS qualified;
-  typeCd           char(10);
-  issuerCd         char(10);
-  issuerStateCd    char(10);
-  issuerCountryCd  char(10);
-  identifNm        char(30);
-  expirationDt     char(8);
-end-ds;
+   else;
+      return *blanks;
+   endif;
 
-dcl-ds PersonDS qualified;
-  firstName     char(20);
-  middleName    char(20);
-  lastName      char(20);
-  motherMName   char(20);
-  address       likeds(AddressDS);
-end-ds;
-
-dcl-ds RecipientDS qualified;
-  firstName     char(20);
-  middleName    char(20);
-  lastName      char(20);
-  motherMName   char(20);
-  identifTypeCd char(10);
-  identifNm     char(30);
-  foreignName   likeds(PersonDS);
-  address       likeds(AddressDS);
-end-ds;
-
-dcl-ds DataDetailDS qualified;
-  saleDt         char(8);
-  saleTm         char(6);
-  serviceCd      char(10);
-  paymentTypeCd  char(10);
-  origCountryCd  char(5);
-  origCurrencyCd char(5);
-  destCountryCd  char(5);
-  destCurrencyCd char(5);
-  originAm       char(20);
-  destinationAm  char(20);
-  exchRateFx     char(20);
-  marketRefCurrencyCd  char(5);
-  marketRefCurrencyAm  char(20);
-  sAgentCd       char(10);
-  rAccountTypeCd char(10);
-  rAccountNm     char(50);
-  rAgentCd       char(10);
-  rAgentRegionSd char(10);
-  rAgentBranchSd char(10);
-  sender         likeds(PersonDS);
-  recipient      likeds(RecipientDS);
-  senderIdent    likeds(IdentificationDS);
-  recipientIdent likeds(IdentificationDS);
-end-ds;
-
-dcl-ds DataDS qualified;
-  opCode         char(10);
-  processMsg     char(100);
-  errorParamFullName char(100);
-  transStatusCd  char(10);
-  transStatusDt  char(8);
-  processDt      char(8);
-  processTm      char(6);
-  detail         likeds(DataDetailDS);
-end-ds;
-
-dcl-ds FullResponseDS qualified;
-  header likeds(HeaderDS);
-  data   likeds(DataDS);
-end-ds;
+end-proc;
