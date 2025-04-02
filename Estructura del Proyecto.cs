8e17dbea10f5040fccf5a1d860b6075f352794e4
MasterUploader <MasterUploader@users.noreply.github.com>
2025-04-02 07:38:25 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,39 +1,39 @@
-dcl-proc SafeGetString;
-   dcl-pi *n char(100);
-      jsonPtr pointer value;
-   end-pi;
+dcl-proc SafeGetDecimal;
+  dcl-pi *n packed(15:4);
+    jsonPtr pointer value;
+  end-pi;
 
-   dcl-s tempStrPtr pointer;
+  dcl-s result packed(15:4);
+  dcl-s tempNumPtr pointer;
+  dcl-s tempNum char(64) based(tempNumPtr); // o tamaño que consideres seguro
 
-   if jsonPtr <> *null;
-      tempStrPtr = yajl_get_string(jsonPtr);
+  result = 0;
 
-      if tempStrPtr <> *null;
-         return %str(tempStrPtr);
-      endif;
-   endif;
+  if jsonPtr <> *null;
+    tempNumPtr = yajl_get_number(jsonPtr);
+    if tempNumPtr <> *null;
+      result = %dec(%str(tempNumPtr): 15: 4);
+    endif;
+  endif;
 
-   return *blanks;
+  return result;
 end-proc;
 
 
-dcl-proc SafeGetDecimal;
-   dcl-pi *n packed(15:4);
-      jsonPtr pointer value;
-   end-pi;
+dcl-proc SafeGetString;
+  dcl-pi *n char(100);
+    jsonPtr pointer value;
+  end-pi;
 
-   dcl-s result packed(15:4);
-   dcl-s tempNumPtr pointer;
+  dcl-s tempStrPtr pointer;
+  dcl-s tempStr char(100) based(tempStrPtr);
 
-   result = 0;
+  if jsonPtr <> *null;
+    tempStrPtr = yajl_get_string(jsonPtr);
+    if tempStrPtr <> *null;
+      return %str(tempStrPtr);
+    endif;
+  endif;
 
-   if jsonPtr <> *null;
-      tempNumPtr = yajl_get_number(jsonPtr);
-
-      if tempNumPtr <> *null;
-         result = %dec(%str(tempNumPtr): 15: 4);
-      endif;
-   endif;
-
-   return result;
+  return *blanks;
 end-proc;
