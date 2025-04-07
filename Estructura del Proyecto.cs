1b557e6a12272f0c81a575d0d5e15cec9b04ba6d
MasterUploader <MasterUploader@users.noreply.github.com>
2025-04-07 15:02:59 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,28 +1,11 @@
-<asp:DropDownList ID="ddlFiltroAgencia" runat="server"
-    DataSourceID="DBDataSource2"
-    DataTextField="NOMAGE"
-    DataValueField="CODCCO"
-    AutoPostBack="True"
-    OnSelectedIndexChanged="ddlFiltroAgencia_SelectedIndexChanged"
-    OnDataBound="ddlFiltroAgencia_DataBound">
-</asp:DropDownList>
-
-
-
 protected void ddlFiltroAgencia_DataBound(object sender, EventArgs e)
 {
     if (!IsPostBack)
     {
-        ListItem item = ddlFiltroAgencia.Items.FindByValue("0");
-        if (item != null)
-        {
-            ddlFiltroAgencia.SelectedValue = "0";
-        }
-    }
-}
+        // Insertar item al inicio
+        ddlFiltroAgencia.Items.Insert(0, new ListItem("-- Seleccione Agencia --", "0"));
 
-protected void ddlFiltroAgencia_SelectedIndexChanged(object sender, EventArgs e)
-{
-    // Si necesitas recargar algo (por ejemplo, un GridView), hazlo aquí:
-    rvLstVideos.DataBind(); // solo si tienes el GridView que depende del filtro
+        // Si quieres seleccionar ese por defecto
+        ddlFiltroAgencia.SelectedIndex = 0;
+    }
 }
