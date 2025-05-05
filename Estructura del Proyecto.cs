8c889146c550a9c4472946aae5f546c40030c50d
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-05 10:54:56 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,27 +1,15 @@
-using X.PagedList;
-
-[HttpGet]
-public async Task<IActionResult> Index(int? page, string codcco)
-{
-    var agencias = _videoService.ObtenerAgenciasSelectList();
-    var videos = string.IsNullOrEmpty(codcco)
-        ? await _videoService.ObtenerListaVideosAsync()
-        : _videoService.ListarVideos(codcco);
-
-    int pageSize = 10;
-    int pageNumber = page ?? 1;
-
-    ViewBag.Agencias = agencias;
-    ViewBag.CodccoSeleccionado = codcco;
-
-    return View(videos.ToPagedList(pageNumber, pageSize));
-}
-
-@using X.PagedList.Mvc.Core
-@using X.PagedList
-
-<!-- Tu tabla aquí -->
-
-<div class="text-center mt-3">
-    @Html.PagedListPager((IPagedList)Model, page => Url.Action("Index", new { page, codcco = ViewBag.CodccoSeleccionado }))
-</div>
+@Html.PagedListPager(
+    Model,
+    page => Url.Action("Index", new { page, codcco = ViewBag.CodccoSeleccionado }),
+    new PagedList.Mvc.Core.Common.PagedListRenderOptions
+    {
+        LiElementClasses = new[] { "page-item" },
+        PageClasses = new[] { "page-link" },
+        UlElementClasses = new[] { "pagination", "justify-content-center" },
+        DisplayLinkToFirstPage = PagedList.Mvc.Core.Common.PagedListDisplayMode.Always,
+        DisplayLinkToLastPage = PagedList.Mvc.Core.Common.PagedListDisplayMode.Always,
+        DisplayLinkToPreviousPage = PagedList.Mvc.Core.Common.PagedListDisplayMode.Always,
+        DisplayLinkToNextPage = PagedList.Mvc.Core.Common.PagedListDisplayMode.Always,
+        MaximumPageNumbersToDisplay = 5
+    }
+)
