f9cfbf913d757e4955903616ae0671113f46ef04
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-07 10:30:22 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,18 +1,13 @@
-<form method="get" asp-action="Index" class="row mb-3">
-    <div class="col-md-4">
-        <label for="codcco" class="form-label">Filtrar por Agencia</label>
-        <select id="codcco" name="codcco" class="form-select" onchange="this.form.submit()">
-            @{
-                var codccoSel = ViewBag.CodccoSeleccionado?.ToString();
-                var selectedDefault = string.IsNullOrEmpty(codccoSel) ? "selected" : "";
-            }
-            <option value="" @Html.Raw(selectedDefault)>-- Todas las Agencias --</option>
+<select id="codcco" name="codcco" class="form-select" onchange="this.form.submit()">
+    @{
+        var codccoSel = ViewBag.CodccoSeleccionado?.ToString();
+    }
 
-            @foreach (var agencia in (List<SelectListItem>)ViewBag.AgenciasFiltro)
-            {
-                var selected = agencia.Value == codccoSel ? "selected" : "";
-                <option value="@agencia.Value" @Html.Raw(selected)>@agencia.Text</option>
-            }
-        </select>
-    </div>
-</form>
+    @:<option value="" @(string.IsNullOrEmpty(codccoSel) ? "selected" : "")>-- Todas las Agencias --</option>
+
+    @foreach (var agencia in (List<SelectListItem>)ViewBag.AgenciasFiltro)
+    {
+        var selected = agencia.Value == codccoSel ? "selected" : "";
+        @:<option value="@agencia.Value" @selected>@agencia.Text</option>
+    }
+</select>
