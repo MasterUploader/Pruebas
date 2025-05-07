82ba7bc6ca2717f6a3d7103e099a261398d026ca
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-07 01:14:08 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,59 +1,8 @@
-[HttpGet]
-[AutorizarPorTipoUsuario("1")]
-public async Task<IActionResult> Index(int? page, int? codcco)
+@foreach (var agencia in (List<SelectListItem>)ViewBag.AgenciasFiltro)
 {
-    var agencias = await _agenciaService.ObtenerAgenciasAsync();
+    var selected = ViewBag.CodccoSeleccionado?.ToString() == agencia.Value ? "selected" : "";
 
-    // Filtro por código de agencia si se selecciona
-    if (codcco.HasValue && codcco.Value > 0)
-        agencias = agencias.Where(a => a.Codcco == codcco.Value).ToList();
-
-    // Generar lista desplegable para el filtro
-    ViewBag.AgenciasFiltro = agencias
-        .Select(a => new SelectListItem
-        {
-            Value = a.Codcco.ToString(),
-            Text = $"{a.Codcco} - {a.NomAge}"
-        })
-        .OrderBy(a => a.Text)
-        .ToList();
-
-    ViewBag.CodccoSeleccionado = codcco;
-
-    int pageSize = 10;
-    int pageNumber = page ?? 1;
-    return View(agencias.ToPagedList(pageNumber, pageSize));
+    <option value="@agencia.Value" @selected>
+        @agencia.Text
+    </option>
 }
-
-
-<form method="get" asp-action="Index" class="row mb-3">
-    <div class="col-md-4">
-        <label for="codcco" class="form-label">Filtrar por Agencia</label>
-        <select id="codcco" name="codcco" class="form-select" onchange="this.form.submit()">
-            <option value="">-- Todas las Agencias --</option>
-            @foreach (var agencia in (List<SelectListItem>)ViewBag.AgenciasFiltro)
-            {
-                <option value="@agencia.Value" @(ViewBag.CodccoSeleccionado?.ToString() == agencia.Value ? "selected" : "")>
-                    @agencia.Text
-                </option>
-            }
-        </select>
-    </div>
-</form>
-
-
-@Html.PagedListPager(
-    Model,
-    page => Url.Action("Index", new { page, codcco = ViewBag.CodccoSeleccionado }),
-    new PagedListRenderOptions
-    {
-        UlElementClasses = new[] { "pagination", "justify-content-center" },
-        LiElementClasses = new[] { "page-item" },
-        PageClasses = new[] { "page-link" },
-        DisplayLinkToFirstPage = PagedListDisplayMode.Always,
-        DisplayLinkToLastPage = PagedListDisplayMode.Always,
-        DisplayLinkToPreviousPage = PagedListDisplayMode.Always,
-        DisplayLinkToNextPage = PagedListDisplayMode.Always,
-        MaximumPageNumbersToDisplay = 5
-    }
-)
