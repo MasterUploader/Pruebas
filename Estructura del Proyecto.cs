4d7ddc1fdf401d4bdbf67c27780af2b3b52f4a20
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-07 01:17:58 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,8 +1,6 @@
 @foreach (var agencia in (List<SelectListItem>)ViewBag.AgenciasFiltro)
 {
-    var selected = ViewBag.CodccoSeleccionado?.ToString() == agencia.Value ? "selected" : "";
+    string selected = ViewBag.CodccoSeleccionado?.ToString() == agencia.Value ? "selected" : "";
 
-    <option value="@agencia.Value" @selected>
-        @agencia.Text
-    </option>
+    @:<option value="@agencia.Value" @selected>@agencia.Text</option>
 }
