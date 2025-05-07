0d4872827dc51004f9095feeda2ad9bd1152232b
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-07 10:27:30 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -2,12 +2,16 @@
     <div class="col-md-4">
         <label for="codcco" class="form-label">Filtrar por Agencia</label>
         <select id="codcco" name="codcco" class="form-select" onchange="this.form.submit()">
-            <option value="" @(ViewBag.CodccoSeleccionado == null ? "selected" : "")>-- Todas las Agencias --</option>
+            @{
+                var codccoSel = ViewBag.CodccoSeleccionado?.ToString();
+                var selectedDefault = string.IsNullOrEmpty(codccoSel) ? "selected" : "";
+            }
+            <option value="" @Html.Raw(selectedDefault)>-- Todas las Agencias --</option>
 
             @foreach (var agencia in (List<SelectListItem>)ViewBag.AgenciasFiltro)
             {
-                var isSelected = ViewBag.CodccoSeleccionado?.ToString() == agencia.Value ? "selected" : "";
-                <option value="@agencia.Value" @Html.Raw(isSelected)>@agencia.Text</option>
+                var selected = agencia.Value == codccoSel ? "selected" : "";
+                <option value="@agencia.Value" @Html.Raw(selected)>@agencia.Text</option>
             }
         </select>
     </div>
