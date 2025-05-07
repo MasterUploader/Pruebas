f77ceafd5d44f4950ea5678800ed88ee41fba206
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-07 10:32:15 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,13 +1,4 @@
-<select id="codcco" name="codcco" class="form-select" onchange="this.form.submit()">
-    @{
-        var codccoSel = ViewBag.CodccoSeleccionado?.ToString();
-    }
-
-    @:<option value="" @(string.IsNullOrEmpty(codccoSel) ? "selected" : "")>-- Todas las Agencias --</option>
-
-    @foreach (var agencia in (List<SelectListItem>)ViewBag.AgenciasFiltro)
-    {
-        var selected = agencia.Value == codccoSel ? "selected" : "";
-        @:<option value="@agencia.Value" @selected>@agencia.Text</option>
-    }
-</select>
+@{
+    var selectedDefault = string.IsNullOrEmpty(codccoSel) ? "selected" : "";
+}
+@:<option value="" @selectedDefault>-- Todas las Agencias --</option>
