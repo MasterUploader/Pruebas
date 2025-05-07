d961f07faabfb1079ae12a208212ed680a20e094
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-07 13:36:06 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,22 +1,7 @@
-[HttpPost]
-public async Task<IActionResult> GuardarEdicion(AgenciaModel model)
-{
-    if (!ModelState.IsValid)
+var agencias = await _agenciaService.ObtenerAgenciasAsync();
+ViewBag.AgenciasFiltro = agencias
+    .Select(a => new SelectListItem
     {
-        var agencias = await _agenciaService.ObtenerAgenciasAsync();
-        var modelo = new AgenciaIndexViewModel
-        {
-            Lista = agencias.ToPagedList(1, 10),
-            AgenciaEnEdicion = model,
-            CodccoSeleccionado = model.Codcco.ToString()
-        };
-
-        ViewBag.AgenciasFiltro = await _agenciaService.ObtenerAgenciasFiltroAsync(); // << ASEGURA ESTO
-
-        return View("Index", modelo);
-    }
-
-    await _agenciaService.ActualizarAgenciaAsync(model);
-    TempData["Mensaje"] = "Agencia actualizada correctamente.";
-    return RedirectToAction("Index");
-}
+        Value = a.Codcco.ToString(),
+        Text = a.NomAge
+    }).ToList();
