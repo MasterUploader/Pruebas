c460176ce3f5784d8ddaf5f2ca84dcbb7a59535a
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-08 09:54:39 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,1 +1,10 @@
-callp yajl_copyBuf(0 : %addr(jsonBuffer) : %size(jsonBuffer) : jsonLength);
+    "header": {█        "h-request-id": "8d0f19e5-a617-4bc2-
+ad2d",█        "h-channel": "CAJA",█        "h-session-id": 
+"8d0f19e5-a617-4bc2-ad2d",█        "h-client-ip": "127.0.0.1
+",█        "h-user-id": "TLORTEGA",█        "h-provider": "B
+TS",█        "h-organization": "Davivi",█        "h-terminal
+": "HN50401436",█        "h-timestamp": "TIEMPO"█    },█    
+"body": {█        "execTR": {█            "request": {█     
+           "data": {█                    "confirmationNm": "
+79901025733",█                    "agent": {█               
+         "regionSd": "CSU",█                        "branchS
