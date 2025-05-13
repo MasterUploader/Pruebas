18ab1ea9b134bbe24a866193c784674625dfddef
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-13 09:29:10 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,25 +1,54 @@
-dcl-proc GetStringFromJson;
-  dcl-pi *n char(200);
+dcl-proc GetDecimalFromJson;
+  dcl-pi *n packed(15:4);
     parentNode pointer value;
     fieldName varchar(50) const;
-    maxLength int(5) const;
   end-pi;
 
-  dcl-s result char(200);
+  dcl-s result packed(15:4) inz(0);
   dcl-s val pointer;
-
-  result = *blanks;
+  dcl-s strVal varchar(50);
 
   val = yajl_object_find(parentNode: %trim(fieldName));
-
   if val <> *null;
-    result = yajl_get_string(val);
-    if %trim(result) = '';
-      result = ' ';
+    strVal = yajl_get_string(val);
+    if %trim(strVal) <> '';
+      result = %dec(strVal: 15: 4);
     endif;
-  else;
-    result = ' ';
   endif;
 
-  return %subst(result:1:maxLength);
+  return result;
+end-proc;
+
+dcl-proc GetDateFromJson;
+  dcl-pi *n date;
+    parentNode pointer value;
+    fieldName varchar(50) const;
+  end-pi;
+
+  dcl-s result date inz(*loval);
+  dcl-s val pointer;
+  dcl-s strVal char(8);
+
+  val = yajl_object_find(parentNode: %trim(fieldName));
+  if val <> *null;
+    strVal = yajl_get_string(val);
+    if %len(%trim(strVal)) = 8;
+      result = %date(%subst(strVal:1:4) + '-' +
+                     %subst(strVal:5:2) + '-' +
+                     %subst(strVal:7:2): *iso);
+    endif;
+  endif;
+
+  return result;
+end-proc;
+
+dcl-proc GetBooleanFromJson;
+  dcl-pi *n ind;
+    parentNode pointer value;
+    fieldName varchar(50) const;
+  end-pi;
+
+  dcl-s val pointer;
+  val = yajl_object_find(parentNode: %trim(fieldName));
+  return (val <> *null and yajl_is_true(val));
 end-proc;
