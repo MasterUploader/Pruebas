0e98a2907730541a38f5bf5e0513f8b7d07dd9cd
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-12 20:50:41 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,68 +1,110 @@
-dcl-proc MostrarJsonNodos;
-  dcl-pi *n;
-    nodo pointer value;
-    nivel int(5) value;
-  end-pi;
-
-  dcl-s i int(5);
-  dcl-s key varchar(100);
-  dcl-s subnodo pointer;
-  dcl-s tipo int(5);
-
-  dcl-s totalKeys int(5);
-  dcl-s indent varchar(50);
-
-  // Indentación visual según nivel
-  indent = %subst('                                                  ': 1: nivel * 2);
-
-  tipo = yajl_get_type(nodo);
-
-  select;
-    when tipo = YAJL_TYPE_OBJ;
-
-      totalKeys = yajl_get_keys(nodo);
-      for i = 1 to totalKeys;
-        key = yajl_get_key(nodo: i);
-        subnodo = yajl_object_find(nodo: key);
-        dsply indent + 'Obj: ' + key;
-        MostrarJsonNodos(subnodo: nivel + 1);
-      endfor;
-
-    when tipo = YAJL_TYPE_ARR;
-
-      for i = 1 to yajl_array_size(nodo);
-        subnodo = yajl_array_elem(nodo: i - 1);
-        dsply indent + 'Array[' + %char(i) + ']';
-        MostrarJsonNodos(subnodo: nivel + 1);
-      endfor;
-
-    when tipo = YAJL_TYPE_STR;
-      dsply indent + 'Valor: ' + %str(yajl_get_string(nodo));
-
-    when tipo = YAJL_TYPE_NUM;
-      dsply indent + 'Número: ' + %str(yajl_get_string(nodo));
-
-    when tipo = YAJL_TYPE_BOOL;
-      dsply indent + 'Boolean: ' + %str(yajl_get_string(nodo));
-
-    when tipo = YAJL_TYPE_NULL;
-      dsply indent + 'Null';
-
-    other;
-      dsply indent + 'Tipo desconocido';
-  endsl;
-
-end-proc;
-
-
-dcl-s rootNode pointer;
-dcl-s errmsg varchar(500);
-
-rootNode = yajl_stmf_load_tree(%trim(vFullFileR): errmsg);
-
-if rootNode = *null;
-   dsply 'Error al parsear JSON: ' + errmsg;
-else;
-   MostrarJsonNodos(rootNode: 0);
-   yajl_tree_free(rootNode);
-endif;
+{
+    "Header": {
+        "ReponseId": "46067917-5bb5-4c51-8cbe-89a0c3fd532e",
+        "Timestamp": "2025-05-13T02:48:58.4383703Z",
+        "ProcessingTime": "100ms",
+        "StatusCode": "1000",
+        "Message": "QRYI ACCEPTED ORDER",
+        "RequestHeader": {
+            "h-request-id": "35ad0ede-98b5-49a8-98c9-1430d0df8334",
+            "h-channel": "Caja",
+            "h-terminal": "POSTAM",
+            "h-organization": "Davivienda",
+            "h-user-id": "PRUEBAS",
+            "h-provider": "POSTAM",
+            "h-session-id": "0ef470d1-9660-4b38-aaa4-13f1ec06c3a4",
+            "h-client-ip": "localhost",
+            "h-timestamp": "20250221"
+        }
+    },
+    "Data": {
+        "OpCode": "1000",
+        "ProcessMsg": "QRYI ACCEPTED ORDER",
+        "ErrorParamFullName": "",
+        "TransStatusCd": "ONP",
+        "TransStatusDt": "20250207",
+        "ProcessDt": "20250512",
+        "ProcessTm": "214858",
+        "Data": {
+            "SaleDt": "20250207",
+            "SaleTm": "174941",
+            "ServiceCd": "MTR",
+            "PaymentTypeCd": "CSA",
+            "OrigCountryCd": "USA",
+            "OrigCurrencyCd": "USD",
+            "DestCountryCd": "HND",
+            "DestCurrencyCd": "HNL",
+            "DestAmount": "7657.0200",
+            "OrigAmount": "300.0000",
+            "ExchangeRateFx": "25.5234000000",
+            "MarketRefCurrencyCd": "USD",
+            "MarketRefCurrencyFx": "25.52340",
+            "MarketRefCurrencyAm": "300.00",
+            "SAgentCd": "BTS",
+            "SPaymentTypeCd": "",
+            "SAccountTypeCd": "",
+            "SAccountNm": "",
+            "SBankCd": "",
+            "SBankRefNm": "",
+            "RAccountTypeCd": "",
+            "RAccountName": "",
+            "RAgentCd": "",
+            "RAgentRegionSd": "",
+            "RAgentBranchSd": "",
+            "BankRefNm": "",
+            "PromotionCode": "",
+            "Sender": {
+                "FirstName": "JOSE",
+                "MiddleName": "LUIS",
+                "LastName": "DELAGARZA",
+                "MotherMName": "VALDOVINOS",
+                "Address": {
+                    "Address": "5403 UNIVERSITY AVE",
+                    "City": "SAN DIEGO",
+                    "StateCd": "CA ",
+                    "CountryCd": "USA",
+                    "ZipCode": "92105",
+                    "Phone": "+16192659701"
+                }
+            },
+            "Recipient": {
+                "FirstName": "LUZ",
+                "MiddleName": "AYDEE",
+                "LastName": "ALVAREZ",
+                "MotherMName": "CRUZ",
+                "Identif_Type_Cd": "",
+                "Identif_Nm": "",
+                "Foreing_Name": {
+                    "FirstName": "",
+                    "MiddleName": "",
+                    "LastName": "",
+                    "MotherMName": ""
+                },
+                "Address": {
+                    "Address": "DOMICILIO CONOCIDO",
+                    "City": "CIUDAD CONOCIDA",
+                    "StateCd": "ATL",
+                    "CountryCd": "HND",
+                    "ZipCode": "31001",
+                    "Phone": "+5047414233"
+                }
+            },
+            "RecipientIdentification": {
+                "TypeCd": "",
+                "IssuerCd": "",
+                "IssuerStateCd": "",
+                "IssuerCountryCd": "",
+                "IdentFnum": "",
+                "ExpirationDt": ""
+            },
+            "SenderIdentification": {
+                "TypeCd": "",
+                "IssuerCd": "",
+                "IssuerStateCd": "",
+                "IssuerCountryCd": "",
+                "IdentFnum": "",
+                "ExpirationDt": ""
+            }
+        }
+    }
+}
