844e16e2812320086240695dac8d78993b28945b
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-13 09:45:25 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,68 +1,184 @@
-dcl-proc ProcesarRespuesta;
+dcl-proc ProcesarDataGeneral;
+  dcl-pi *n;
+    dataNode pointer value;
+  end-pi;
 
-  dcl-s docNode pointer;
-  dcl-s headerNode pointer;
-  dcl-s dataNode pointer;
-  dcl-s innerDataNode pointer;
-  dcl-s requestHeader pointer;
-  dcl-s senderNode pointer;
-  dcl-s recipientNode pointer;
-  dcl-s foreignNode pointer;
-  dcl-s senderAddr pointer;
-  dcl-s recipientAddr pointer;
-  dcl-s senderIdent pointer;
-  dcl-s recipientIdent pointer;
+  FullResponseDS.data.detail.saleDt =
+    GetStringFromJson(dataNode: 'saleDt': 8);
 
-  dcl-s errmsg varchar(500);
+  FullResponseDS.data.detail.saleTm =
+    GetStringFromJson(dataNode: 'saleTm': 6);
 
-  docNode = yajl_stmf_load_tree(%trim(vFullFileR): errmsg);
+  FullResponseDS.data.detail.serviceCd =
+    GetStringFromJson(dataNode: 'serviceCd': 10);
 
-  if errmsg <> *blanks;
-     // Manejo del error de lectura
-     HDR_MSG = 'Error al cargar JSON: ' + errmsg;
-     return;
+  FullResponseDS.data.detail.paymentTypeCd =
+    GetStringFromJson(dataNode: 'paymentTypeCd': 10);
+
+  FullResponseDS.data.detail.origCountryCd =
+    GetStringFromJson(dataNode: 'origCountryCd': 5);
+
+  FullResponseDS.data.detail.origCurrencyCd =
+    GetStringFromJson(dataNode: 'origCurrencyCd': 5);
+
+  FullResponseDS.data.detail.destCountryCd =
+    GetStringFromJson(dataNode: 'destCountryCd': 5);
+
+  FullResponseDS.data.detail.destCurrencyCd =
+    GetStringFromJson(dataNode: 'DestCurrencyCd': 5);
+
+  FullResponseDS.data.detail.originAm =
+    GetStringFromJson(dataNode: 'origAmount': 20);
+
+  FullResponseDS.data.detail.destinationAm =
+    GetStringFromJson(dataNode: 'destAmount': 20);
+
+  FullResponseDS.data.detail.exchRateFx =
+    GetStringFromJson(dataNode: 'exchangeRateFx': 20);
+
+  FullResponseDS.data.detail.marketRefCurrencyCd =
+    GetStringFromJson(dataNode: 'marketRefCurrencyCd': 5);
+
+  FullResponseDS.data.detail.marketRefCurrencyAm =
+    GetStringFromJson(dataNode: 'marketRefCurrencyAm': 20);
+end-proc;
+
+
+dcl-proc ProcesarSender;
+  dcl-pi *n;
+    dataNode pointer value;
+  end-pi;
+
+  dcl-s sender pointer;
+  dcl-s addr pointer;
+
+  sender = yajl_object_find(dataNode: 'sender');
+  if sender <> *null;
+    FullResponseDS.data.detail.sender.firstName =
+      GetStringFromJson(sender: 'firstName': 40);
+    FullResponseDS.data.detail.sender.middleName =
+      GetStringFromJson(sender: 'middleName': 40);
+    FullResponseDS.data.detail.sender.lastName =
+      GetStringFromJson(sender: 'lastName': 40);
+    FullResponseDS.data.detail.sender.motherMName =
+      GetStringFromJson(sender: 'motherMName': 40);
+
+    addr = yajl_object_find(sender: 'address');
+    if addr <> *null;
+      FullResponseDS.data.detail.sender.address.address =
+        GetStringFromJson(addr: 'address': 65);
+      FullResponseDS.data.detail.sender.address.city =
+        GetStringFromJson(addr: 'city': 30);
+      FullResponseDS.data.detail.sender.address.stateCd =
+        GetStringFromJson(addr: 'stateCd': 3);
+      FullResponseDS.data.detail.sender.address.countryCd =
+        GetStringFromJson(addr: 'countryCd': 3);
+      FullResponseDS.data.detail.sender.address.zipCode =
+        GetStringFromJson(addr: 'zipCode': 10);
+      FullResponseDS.data.detail.sender.address.phone =
+        GetStringFromJson(addr: 'phone': 15);
+    endif;
   endif;
+end-proc;
 
-  //-------------------------------------------------------
-  // HEADER
-  //-------------------------------------------------------
-  headerNode = yajl_object_find(docNode: 'header');
-  if headerNode <> *null;
-     HDR_RSPID  = GetString(headerNode: 'responseId': 100);
-     HDR_TMSTMP = GetString(headerNode: 'timestamp': 50);
-     HDR_PRTIME = GetString(headerNode: 'processingtime': 20);
-     HDR_STSCD  = GetString(headerNode: 'statuscode': 10);
-     HDR_MSG    = GetString(headerNode: 'message': 500);
+dcl-proc ProcesarRecipient;
+  dcl-pi *n;
+    dataNode pointer value;
+  end-pi;
 
-     requestHeader = yajl_object_find(headerNode: 'requestheader');
-     if requestHeader <> *null;
-        // Puedes mapear los campos de requestHeader aquí si lo necesitas
-     endif;
+  dcl-s rec pointer;
+  dcl-s addr pointer;
+  dcl-s fName pointer;
+
+  rec = yajl_object_find(dataNode: 'recipient');
+  if rec <> *null;
+    FullResponseDS.data.detail.recipient.firstName =
+      GetStringFromJson(rec: 'firstName': 40);
+    FullResponseDS.data.detail.recipient.middleName =
+      GetStringFromJson(rec: 'middleName': 40);
+    FullResponseDS.data.detail.recipient.lastName =
+      GetStringFromJson(rec: 'lastName': 40);
+    FullResponseDS.data.detail.recipient.motherMName =
+      GetStringFromJson(rec: 'motherMName': 40);
+    FullResponseDS.data.detail.recipient.identifTypeCd =
+      GetStringFromJson(rec: 'identif_Type_Cd': 10);
+    FullResponseDS.data.detail.recipient.identifNm =
+      GetStringFromJson(rec: 'identif_Nm': 30);
+
+    fName = yajl_object_find(rec: 'foreing_Name');
+    if fName <> *null;
+      FullResponseDS.data.detail.recipient.foreignName.firstName =
+        GetStringFromJson(fName: 'firstName': 40);
+      FullResponseDS.data.detail.recipient.foreignName.middleName =
+        GetStringFromJson(fName: 'middleName': 40);
+      FullResponseDS.data.detail.recipient.foreignName.lastName =
+        GetStringFromJson(fName: 'lastName': 40);
+      FullResponseDS.data.detail.recipient.foreignName.motherMName =
+        GetStringFromJson(fName: 'motherMName': 40);
+    endif;
+
+    addr = yajl_object_find(rec: 'address');
+    if addr <> *null;
+      FullResponseDS.data.detail.recipient.address.address =
+        GetStringFromJson(addr: 'address': 65);
+      FullResponseDS.data.detail.recipient.address.city =
+        GetStringFromJson(addr: 'city': 30);
+      FullResponseDS.data.detail.recipient.address.stateCd =
+        GetStringFromJson(addr: 'stateCd': 3);
+      FullResponseDS.data.detail.recipient.address.countryCd =
+        GetStringFromJson(addr: 'countryCd': 3);
+      FullResponseDS.data.detail.recipient.address.zipCode =
+        GetStringFromJson(addr: 'zipCode': 10);
+      FullResponseDS.data.detail.recipient.address.phone =
+        GetStringFromJson(addr: 'phone': 15);
+    endif;
   endif;
+end-proc;
 
-  //-------------------------------------------------------
-  // DATA
-  //-------------------------------------------------------
-  dataNode = yajl_object_find(docNode: 'data');
-  if dataNode <> *null;
-     OPCODE         = GetString(dataNode: 'opCode': 4);
-     ProcessMsg     = GetString(dataNode: 'processMsg': 70);
-     ErrorParamFullName = GetString(dataNode: 'errorParamFullName': 255);
-     TransStatusCd  = GetString(dataNode: 'transStatusCd': 3);
-     TransStatusDt  = GetString(dataNode: 'transStatusDt': 8);
-     ProcessDt      = GetString(dataNode: 'processDt': 8);
-     ProcessTm      = GetString(dataNode: 'processTm': 6);
+dcl-proc ProcesarSenderIdent;
+  dcl-pi *n;
+    dataNode pointer value;
+  end-pi;
 
-     innerDataNode = yajl_object_find(dataNode: 'data');
-     if innerDataNode <> *null;
-        ProcesarDataGeneral(innerDataNode);
-        ProcesarSender(innerDataNode);
-        ProcesarRecipient(innerDataNode);
-        ProcesarSenderIdent(innerDataNode);
-        ProcesarRecipientIdent(innerDataNode);
-     endif;
+  dcl-s ident pointer;
+
+  ident = yajl_object_find(dataNode: 'senderIdentification');
+  if ident <> *null;
+    FullResponseDS.data.detail.senderIdent.typeCd =
+      GetStringFromJson(ident: 'typeCd': 10);
+    FullResponseDS.data.detail.senderIdent.issuerCd =
+      GetStringFromJson(ident: 'issuerCd': 10);
+    FullResponseDS.data.detail.senderIdent.issuerStateCd =
+      GetStringFromJson(ident: 'issuerStateCd': 10);
+    FullResponseDS.data.detail.senderIdent.issuerCountryCd =
+      GetStringFromJson(ident: 'issuerCountryCd': 10);
+    FullResponseDS.data.detail.senderIdent.identifNm =
+      GetStringFromJson(ident: 'identFnum': 30);
+    FullResponseDS.data.detail.senderIdent.expirationDt =
+      GetStringFromJson(ident: 'expirationDt': 8);
   endif;
+end-proc;
 
-  yajl_tree_free(docNode);
+dcl-proc ProcesarRecipientIdent;
+  dcl-pi *n;
+    dataNode pointer value;
+  end-pi;
 
+  dcl-s ident pointer;
+
+  ident = yajl_object_find(dataNode: 'recipientIdentification');
+  if ident <> *null;
+    FullResponseDS.data.detail.recipientIdent.typeCd =
+      GetStringFromJson(ident: 'typeCd': 10);
+    FullResponseDS.data.detail.recipientIdent.issuerCd =
+      GetStringFromJson(ident: 'issuerCd': 10);
+    FullResponseDS.data.detail.recipientIdent.issuerStateCd =
+      GetStringFromJson(ident: 'issuerStateCd': 10);
+    FullResponseDS.data.detail.recipientIdent.issuerCountryCd =
+      GetStringFromJson(ident: 'issuerCountryCd': 10);
+    FullResponseDS.data.detail.recipientIdent.identifNm =
+      GetStringFromJson(ident: 'identFnum': 30);
+    FullResponseDS.data.detail.recipientIdent.expirationDt =
+      GetStringFromJson(ident: 'expirationDt': 8);
+  endif;
 end-proc;
