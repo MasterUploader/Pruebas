6cea7ccb4b144a0406b5cf1de071f6a87911edc6
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-12 21:37:07 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,179 +1,110 @@
-dcl-proc GetSafeString export;
-  dcl-pi *n char(100);
-    node pointer value;
-    name varchar(50) const;
-    len int(5) const;
-  end-pi;
-
-  dcl-s result char(100);
-  dcl-s tmpPtr pointer;
-  dcl-s tmpStr char(500) based(tmpPtr);
-
-  result = *blanks;
-
-  tmpPtr = yajl_object_find(node: %trim(name));
-
-  if tmpPtr <> *null;
-    result = %subst(%str(%addr(tmpStr)): 1: len);
-    if %trim(result) = '';
-      result = ' '; // Asigna un espacio si viene vacío
-    endif;
-  else;
-    result = ' ';
-  endif;
-
-  return result;
-end-proc;
-
-
-
-
-dcl-proc ProcesarRespuestaExtendida;
-  dcl-s root pointer;
-  dcl-s header pointer;
-  dcl-s reqHeader pointer;
-  dcl-s data pointer;
-  dcl-s dataBody pointer;
-  dcl-s sender pointer;
-  dcl-s recipient pointer;
-  dcl-s identRec pointer;
-  dcl-s identSend pointer;
-  dcl-s addr pointer;
-  dcl-s fName pointer;
-  dcl-s msg varchar(500);
-
-  root = yajl_stmf_load_tree(%trim(vFullFileR): msg);
-
-  header = yajl_object_find(root: 'Header');
-  if header <> *null;
-    FullResponseDS.header.responseId =
-      GetSafeString(header: 'ReponseId': 40);
-    FullResponseDS.header.timestamp =
-      GetSafeString(header: 'Timestamp': 30);
-    FullResponseDS.header.processingTime =
-      GetSafeString(header: 'ProcessingTime': 20);
-    FullResponseDS.header.statusCode =
-      GetSafeString(header: 'StatusCode': 10);
-    FullResponseDS.header.message =
-      GetSafeString(header: 'Message': 100);
-
-    reqHeader = yajl_object_find(header: 'RequestHeader');
-    if reqHeader <> *null;
-      // Si deseas mapear esto, hazlo aquí
-    endif;
-  endif;
-
-  data = yajl_object_find(root: 'Data');
-  if data <> *null;
-    FullResponseDS.data.opCode =
-      GetSafeString(data: 'OpCode': 4);
-    FullResponseDS.data.processMsg =
-      GetSafeString(data: 'ProcessMsg': 5000);
-    FullResponseDS.data.errorParamFullName =
-      GetSafeString(data: 'ErrorParamFullName': 255);
-    FullResponseDS.data.transStatusCd =
-      GetSafeString(data: 'TransStatusCd': 3);
-    FullResponseDS.data.transStatusDt =
-      GetSafeString(data: 'TransStatusDt': 8);
-    FullResponseDS.data.processDt =
-      GetSafeString(data: 'ProcessDt': 8);
-    FullResponseDS.data.processTm =
-      GetSafeString(data: 'ProcessTm': 6);
-
-    dataBody = yajl_object_find(data: 'Data');
-    if dataBody <> *null;
-
-      FullResponseDS.data.detail.saleDt =
-        GetSafeString(dataBody: 'SaleDt': 8);
-      FullResponseDS.data.detail.saleTm =
-        GetSafeString(dataBody: 'SaleTm': 6);
-      FullResponseDS.data.detail.serviceCd =
-        GetSafeString(dataBody: 'ServiceCd': 10);
-      FullResponseDS.data.detail.paymentTypeCd =
-        GetSafeString(dataBody: 'PaymentTypeCd': 10);
-      FullResponseDS.data.detail.origCountryCd =
-        GetSafeString(dataBody: 'OrigCountryCd': 5);
-      FullResponseDS.data.detail.destCountryCd =
-        GetSafeString(dataBody: 'DestCountryCd': 5);
-      FullResponseDS.data.detail.destCurrencyCd =
-        GetSafeString(dataBody: 'DestCurrencyCd': 5);
-      FullResponseDS.data.detail.destinationAm =
-        GetSafeString(dataBody: 'DestAmount': 20);
-      FullResponseDS.data.detail.originAm =
-        GetSafeString(dataBody: 'OrigAmount': 20);
-
-      // Sender
-      sender = yajl_object_find(dataBody: 'Sender');
-      if sender <> *null;
-        FullResponseDS.data.detail.sender.firstName =
-          GetSafeString(sender: 'FirstName': 40);
-        FullResponseDS.data.detail.sender.middleName =
-          GetSafeString(sender: 'MiddleName': 40);
-        FullResponseDS.data.detail.sender.lastName =
-          GetSafeString(sender: 'LastName': 40);
-        FullResponseDS.data.detail.sender.motherMName =
-          GetSafeString(sender: 'MotherMName': 40);
-
-        addr = yajl_object_find(sender: 'Address');
-        if addr <> *null;
-          FullResponseDS.data.detail.sender.address.address =
-            GetSafeString(addr: 'Address': 65);
-          FullResponseDS.data.detail.sender.address.city =
-            GetSafeString(addr: 'City': 30);
-          FullResponseDS.data.detail.sender.address.stateCd =
-            GetSafeString(addr: 'StateCd': 3);
-          FullResponseDS.data.detail.sender.address.countryCd =
-            GetSafeString(addr: 'CountryCd': 3);
-          FullResponseDS.data.detail.sender.address.zipCode =
-            GetSafeString(addr: 'ZipCode': 10);
-          FullResponseDS.data.detail.sender.address.phone =
-            GetSafeString(addr: 'Phone': 15);
-        endif;
-      endif;
-
-      // Recipient
-      recipient = yajl_object_find(dataBody: 'Recipient');
-      if recipient <> *null;
-        FullResponseDS.data.detail.recipient.firstName =
-          GetSafeString(recipient: 'FirstName': 40);
-        FullResponseDS.data.detail.recipient.middleName =
-          GetSafeString(recipient: 'MiddleName': 40);
-        FullResponseDS.data.detail.recipient.lastName =
-          GetSafeString(recipient: 'LastName': 40);
-        FullResponseDS.data.detail.recipient.motherMName =
-          GetSafeString(recipient: 'MotherMName': 40);
-
-        addr = yajl_object_find(recipient: 'Address');
-        if addr <> *null;
-          FullResponseDS.data.detail.recipient.address.address =
-            GetSafeString(addr: 'Address': 65);
-          FullResponseDS.data.detail.recipient.address.city =
-            GetSafeString(addr: 'City': 30);
-          FullResponseDS.data.detail.recipient.address.stateCd =
-            GetSafeString(addr: 'StateCd': 3);
-          FullResponseDS.data.detail.recipient.address.countryCd =
-            GetSafeString(addr: 'CountryCd': 3);
-          FullResponseDS.data.detail.recipient.address.zipCode =
-            GetSafeString(addr: 'ZipCode': 10);
-          FullResponseDS.data.detail.recipient.address.phone =
-            GetSafeString(addr: 'Phone': 15);
-        endif;
-      endif;
-
-      identSend = yajl_object_find(dataBody: 'SenderIdentification');
-      if identSend <> *null;
-        FullResponseDS.data.detail.senderIdent.typeCd =
-          GetSafeString(identSend: 'TypeCd': 10);
-      endif;
-
-      identRec = yajl_object_find(dataBody: 'RecipientIdentification');
-      if identRec <> *null;
-        FullResponseDS.data.detail.recipientIdent.typeCd =
-          GetSafeString(identRec: 'TypeCd': 10);
-      endif;
-
-    endif;
-  endif;
-
-  yajl_tree_free(root);
-end-proc;
+{
+    "header": {
+        "responseId": "2611ddf4-64db-4c52-ad78-36a51823d920",
+        "timestamp": "2025-05-13T03:36:42.7610835Z",
+        "processingtime": "100ms",
+        "statuscode": "1000",
+        "message": "QRYI ACCEPTED ORDER",
+        "requestheader": {
+            "h-request-id": "010604a7-30b1-496a-af69-5019820a35c3",
+            "h-channel": "Caja",
+            "h-terminal": "POSTAM",
+            "h-organization": "Davivienda",
+            "h-user-id": "PRUEBAS",
+            "h-provider": "POSTAM",
+            "h-session-id": "0c6a608d-808d-4b4b-8ed9-b1465f95b17b",
+            "h-client-ip": "localhost",
+            "h-timestamp": "20250221"
+        }
+    },
+    "data": {
+        "opCode": "1000",
+        "processMsg": "QRYI ACCEPTED ORDER",
+        "errorParamFullName": "",
+        "transStatusCd": "ONP",
+        "transStatusDt": "20250207",
+        "processDt": "20250512",
+        "processTm": "223642",
+        "data": {
+            "saleDt": "20250207",
+            "saleTm": "174941",
+            "serviceCd": "MTR",
+            "paymentTypeCd": "CSA",
+            "origCountryCd": "USA",
+            "origCurrencyCd": "USD",
+            "destCountryCd": "HND",
+            "DestCurrencyCd": "",
+            "destAmount": "7657.0200",
+            "origAmount": "300.0000",
+            "exchangeRateFx": "25.5234000000",
+            "marketRefCurrencyCd": "USD",
+            "marketRefCurrencyFx": "25.52340",
+            "marketRefCurrencyAm": "300.00",
+            "sAgentCd": "BTS",
+            "sPaymentTypeCd": "",
+            "sAccountTypeCd": "",
+            "sAccountNm": "",
+            "sBankCd": "",
+            "sBankRefNm": "",
+            "rAccountTypeCd": "",
+            "rAccountName": "",
+            "rAgentCd": "",
+            "rAgentRegionSd": "",
+            "rAgentBranchSd": "",
+            "bankRefNm": "",
+            "promotionCode": "",
+            "sender": {
+                "firstName": "JOSE",
+                "middleName": "LUIS",
+                "lastName": "DELAGARZA",
+                "motherMName": "VALDOVINOS",
+                "address": {
+                    "address": "5403 UNIVERSITY AVE",
+                    "city": "SAN DIEGO",
+                    "stateCd": "CA ",
+                    "countryCd": "USA",
+                    "zipCode": "92105",
+                    "phone": "+16192659701"
+                }
+            },
+            "recipient": {
+                "firstName": "LUZ",
+                "middleName": "AYDEE",
+                "lastName": "ALVAREZ",
+                "motherMName": "CRUZ",
+                "identif_Type_Cd": "",
+                "identif_Nm": "",
+                "foreing_Name": {
+                    "firstName": "",
+                    "middleName": "",
+                    "lastName": "",
+                    "motherMName": ""
+                },
+                "address": {
+                    "address": "DOMICILIO CONOCIDO",
+                    "city": "CIUDAD CONOCIDA",
+                    "stateCd": "ATL",
+                    "countryCd": "HND",
+                    "zipCode": "31001",
+                    "phone": "+5047414233"
+                }
+            },
+            "recipientIdentification": {
+                "typeCd": "",
+                "issuerCd": "",
+                "issuerStateCd": "",
+                "issuerCountryCd": "",
+                "identFnum": "",
+                "expirationDt": ""
+            },
+            "senderIdentification": {
+                "typeCd": "",
+                "issuerCd": "",
+                "issuerStateCd": "",
+                "issuerCountryCd": "",
+                "identFnum": "",
+                "expirationDt": ""
+            }
+        }
+    }
+}
