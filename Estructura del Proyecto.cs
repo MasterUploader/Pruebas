5cab8eb82762fdeda0ce8b977f20bab377577578
MasterUploader <MasterUploader@users.noreply.github.com>
2025-05-14 10:14:20 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,4 +1,101 @@
-dcl-pr MIACLLE extpgm('MIACLLE');
-  entrada1 char(10);
-  entrada2 char(20);
-end-pr;
+{
+    "header": {
+        "responseId": "1eac433d-dcb4-4d5a-9787-de938551907d",
+        "timestamp": "2025-05-14T16:11:08.2694026Z",
+        "processingtime": "100ms",
+        "statuscode": "1100",
+        "message": "PAYI ACCEPTED ORDER",
+        "requestheader": {
+            "h-request-id": "f9a98b33-4d4b-467b-b514-8b4447c1cce8",
+            "h-channel": "Caja",
+            "h-terminal": "POSTAM",
+            "h-organization": "Davivienda",
+            "h-user-id": "PRUEBAS",
+            "h-provider": "POSTAM",
+            "h-session-id": "6cf32006-134b-4e65-9409-2ec8acfd2376",
+            "h-client-ip": "localhost",
+            "h-timestamp": "20250221"
+        }
+    },
+    "data": {
+        "jsonResponse": {
+            "OpCode": "1100",
+            "ProcessMsg": "PAYI ACCEPTED ORDER",
+            "ErrorParamFullName": "",
+            "TransStatusCd": "",
+            "TransStatusDt": "",
+            "ProcessDt": "20250514",
+            "ProcessTm": "111058",
+            "Data": {
+                "SaleDt": "20250207",
+                "SaleTm": "174941",
+                "ServiceCd": "MTR",
+                "PaymentTypeCd": "CSA",
+                "OrigCountryCd": "USA",
+                "OrigCurrencyCd": "USD",
+                "DestCountryCd": "HND",
+                "DestCurrencyCd": "HNL",
+                "DestAmount": "7657.0200",
+                "OrigAmount": "300.0000",
+                "ExchangeRateFx": "25.5234000000",
+                "MarketRefCurrencyCd": "USD",
+                "MarketRefCurrencyFx": "25.52340",
+                "MarketRefCurrencyAm": "300.00",
+                "S_Agent_Cd": "BTS",
+                "S_Agent_Country_Cd": "USA",
+                "S_Agent_State_Cd": "TX ",
+                "S_Payment_Type_Cd": "",
+                "S_Account_Type_Cd": "",
+                "S_Account_Nm": "",
+                "S_Bank_Cd": "",
+                "S_Bank_Ref_Nm": "",
+                "RAgentCd": "",
+                "RAgentRegionSd": "CSU",
+                "RAgentBranchSd": "101",
+                "Bank_Ref_Nm": "GTIKVLCOK4FZNLNX9HD7",
+                "Sender": {
+                    "firstName": "JOSE",
+                    "middleName": "LUIS",
+                    "lastName": "DELAGARZA",
+                    "motherMName": "VALDOVINOS",
+                    "address": {
+                        "address": "5403 UNIVERSITY AVE",
+                        "city": "SAN DIEGO",
+                        "stateCd": "CA ",
+                        "countryCd": "USA",
+                        "zipCode": "92105",
+                        "phone": "+16192659701"
+                    }
+                },
+                "Recipient": {
+                    "FirstName": "LUZ",
+                    "MiddleName": "AYDEE",
+                    "LastName": "ALVAREZ",
+                    "MotherMName": "CRUZ",
+                    "Foreing_Name": {
+                        "firstName": "",
+                        "middleName": "",
+                        "lastName": "",
+                        "motherMName": ""
+                    },
+                    "Address": {
+                        "address": "DOMICILIO CONOCIDO",
+                        "city": "CIUDAD CONOCIDA",
+                        "stateCd": "ATL",
+                        "countryCd": "HND",
+                        "zipCode": "31001",
+                        "phone": "+5047414233"
+                    }
+                },
+                "SenderIdentification": {
+                    "typeCd": "",
+                    "issuerCd": "",
+                    "issuerStateCd": "",
+                    "issuerCountryCd": "",
+                    "identFnum": "",
+                    "expirationDt": ""
+                }
+            }
+        }
+    }
+}
