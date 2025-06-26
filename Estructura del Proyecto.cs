1af3aa4cc1b1e9f16b794cc89337017e368d4f1d
MasterUploader <MasterUploader@users.noreply.github.com>
2025-06-26 16:37:58 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,53 +1,107 @@
-<Project Sdk="Microsoft.NET.Sdk">
+---------------------------Inicio de Log-------------------------
+Inicio: 2025-06-26 16:36:20
+-------------------------------------------------------------------
 
-  <PropertyGroup>
-    <TargetFramework>net8.0</TargetFramework>
-    <ImplicitUsings>enable</ImplicitUsings>
-    <Nullable>enable</Nullable>
+---------------------------Enviroment Info-------------------------
+Inicio: 2025-06-26 16:36:20
+-------------------------------------------------------------------
+Application: MS_BAN_38_UTH_RECAUDACION_PAGOS
+Environment: Development
+ContentRoot: C:\Git\MS_BAN_38_UTH_RecaudacionPagos\MS_BAN_38_UTH_RecaudacionPagos\MS_BAN_38_UTH_RECAUDACION_PAGOS
+Execution ID: 0HNDL05P32V63:00000017
+Client IP: ::1
+User Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36
+Machine Name: HNCSTG015243WAP
+OS: Microsoft Windows NT 10.0.20348.0
+Host: localhost:7266
+Distribución: N/A
+  -- Extras del HttpContext --
+    Scheme              : https
+    Protocol            : HTTP/2
+    Method              : POST
+    Path                : /Login/login
+    Query               : 
+    ContentType         : N/A
+    ContentLength       : 0
+    ClientPort          : 51236
+    LocalIp             : ::1
+    LocalPort           : 7266
+    ConnectionId        : 0HNDL05P32V63
+    Referer             : https://localhost:7266/swagger/index.html
+----------------------------------------------------------------------
+---------------------------Enviroment Info-------------------------
+Fin: 2025-06-26 16:36:20
+-------------------------------------------------------------------
 
-    <!-- NuGet Metadata -->
-    <PackageId>RestUtilities.Common</PackageId>
-    <Version>1.0.0</Version>
-    <Authors>Brayan René Banegas Mejía</Authors>
-    <Description>Componentes comunes reutilizables para utilidades en .NET 8: helpers, constantes y modelos base para APIs.</Description>
-    <PackageTags>Common;Utilities;Helpers;Regex;JSON;XML</PackageTags>
-    <GeneratePackageOnBuild>true</GeneratePackageOnBuild>
-    <PlatformTarget>x64</PlatformTarget>
-    <IncludeBuildOutput>true</IncludeBuildOutput>
-    <RemoveUnnecessaryImports>true</RemoveUnnecessaryImports>
-  </PropertyGroup>
+---------------------------Inicio de Log-------------------------
+Inicio: 2025-06-26 16:36:20
+-------------------------------------------------------------------
 
-  <ItemGroup>
-    <Compile Include="Constants\CommonConstants.cs" />
-    
-    <Compile Include="Helpers\DateTimeHelper.cs" />
-    <Compile Include="Helpers\EnumHelper.cs" />
-    <Compile Include="Helpers\EnvironmentHelper.cs" />
-    <Compile Include="Helpers\HttpHelper.cs" />
-    <Compile Include="Helpers\JsonHelper.cs" />
-    <Compile Include="Helpers\RegexPatterns.cs" />
-    <Compile Include="Helpers\RetryHelper.cs" />
-    <Compile Include="Helpers\StopwatchHelper.cs" />
-    <Compile Include="Helpers\StringHelper.cs" />
-    <Compile Include="Helpers\TimeSpanHelper.cs" />
-    <Compile Include="Helpers\ValidationHelper.cs" />
-    <Compile Include="Helpers\XmlHelper.cs" />
-    
-    <Compile Include="Models\ApiResponse.cs" />
-  </ItemGroup>
+-------------------------------------------------------------------------------
+Controlador: Login
+Action: Login
+Inicio: 2025-06-26 16:36:20
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-</Project>
+----------------------------------Request Info---------------------------------
+Inicio: 2025-06-26 16:36:20
+-------------------------------------------------------------------------------
+Método: POST
+URL: /Login/login
+Cuerpo:
+  (Sin cuerpo en la solicitud)
+----------------------------------Request Info---------------------------------
+Fin: 2025-06-26 16:36:20
+-------------------------------------------------------------------------------
 
-<?xml version="1.0"?>
-<package >
-  <metadata>
-    <id>RestUtilities.Common</id>
-    <version>1.0.0</version>
-    <authors>Brayan René Banegas Mejía</authors>
-    <owners>Brayan René Banegas Mejía</owners>
-    <requireLicenseAcceptance>false</requireLicenseAcceptance>
-    <description>Componentes comunes reutilizables para proyectos .NET 8, incluyendo helpers, constantes y modelos base.</description>
-    <tags>Common Helpers Regex JSON XML Utilities .NET8</tags>
-    <copyright>Copyright © 2025</copyright>
-  </metadata>
-</package>
+============= INICIO HTTP CLIENT =============
+TraceId       : 0HNDL05P32V63:00000017
+Método        : POST
+URL           : https://collection-api.ginih-sandbox.com/v2/users/login
+Código Status : 200
+Duración (ms) : 2483
+Encabezados   :
+traceparent: 00-459bc197b9af9e8784f0579f61743952-cbf85bd02274c6a6-00
+
+Cuerpo:
+{"username":"daviviendaqa","password":"Davivi12$$"}
+Respuesta:
+{"status":"success","message":"The credentials are valid","data":{"refreshToken":"eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.CE65ZrqIbq4PMzj0nn2y6oegMAbqkpcK9oSimf-_AdcBzYRc2Sipf59zOfIlbYoxrsxLD9WhQBQMFoui_mAdx24jkRyTLFQzjk8Odexek1KUXw1XcsXEBpUQ5P7CZh9iDvmUoxNsbHikR4JmG8_Iu5YR-PhBbOyCClgrpWh-anrgFssuAu7ai_V3XOqiwnmhRzQQgkdzkxi9as8oks2q-xQLU_qTU54tw8exHIPVS4Q0oU1oy3DTWoCD7ilvZgknBv-3y5Ao39ggMA6DSoiax7wPyNHKoTf-EQdD9bhasCmWC9BJm231BJGqniAdwxjUH75ukvslzqfbclpxokg_lg.lJrycj73bZZdhp-V.Mtr7voXzLoX-cRwFV0AaCxAwYeBsgVyiSm67wmO7i2iWjq68lAOX_AcAupg6YvGCIzaxiUyR45gj-V88YDsRncHa6K6qOxG5o3Avow3w42ht7rG26PFnn8ZQzl3tEHigvFUXP02z_ZSXmzUnaAzM9zCxXYCd_3vVwL79HG3g5B6rU81gcVhP2r_bfg4bJjfbTBNUdTw6SaRk1UR_4Su-JG9Ui8tbuUjpcEkFTzHiWFMknXy_IkBjfp5eNa0bhrT1zgtLlYe3gp2Ys-oaq__3tj58l-rPkGu2Y9D6W55ZbDVRl5-gs6sGTYKBT43B7Uai-eN-ErSv_3UgppH3DbWJDjynSIt5LXOrT2SqwvHicILL9amfxgb09xDIKiMIx01JxbZ_aGdbvs8P5OHDvwcma4WwbOcml6TEY8Kb54jBIrHy4t-qEc9zMLNLHuUNVtko6sv4yAN2WnqlqiFPvX-xVW9ak706m_aAQwrvgmvOaBJC8bbKczLVIuGO4F6m_0ke8cHVdJTpGN38_VjWrnVWejEStCV3HRKlFyk-b8ypn3GNQsKshhYWMGknAWzyp0ufD2rDuVoT_eTPglo1KoA5BPN3s0gbldNX3BlBaKHnT0bFmuqmgMp1CUWFuDmx9yF6gml2LzJqI4bi_ND8LJXrXmQILh4RxVYuf7JfN6A3DqBsLXwd5MjXeEU0a5f9Bl_T9tZddGQWITwWNNf-q2torxi1-72H-W-2YsN4afqhAE7ooypvtTWu54dntrPkNxNXjvNwvNiUlwFC2g52U-vpzT7fDAQgAmLRRZe28G5EKcTZZS0Cwpdgfxw76ZyKb6S8eXfQHf8lw00VgEGlB09FiQrQRv-JCkKOVilpUqHhfx995Os_sPf30PPfT7uZajxf85r59box2Psxq1D8jejTn5_RypQBd_p1hMUT-ZWJqY40ECbXYw0fQGizluqdbGwilkc7-2cIdGu-PmF7nJEkhY30eLS_gHowTmBimV1YzieD3yp0NA0VNzCxNcsAw7L3mY0Dvn5UNFvDOL5Q9-9tCvFSnmaXAMMvlI7IGcHkgr07BwusHPLq7YGjH52irGwXwInx8FPP2pRu-xFqbRk59ECIHTWQO1bwC6T0S2qQXClIbIgi26vbWGd4MocZV9ynNj42B7SCLCeJ1jsolxPtBpPaun5zrpDJqMHnwic_zKoZ0-85Rp0W0vlxsmN9rmQwE4VJMWphfuNcMwRb5cWIW9ztAlqzTwBiReosK8odur2jYSPea6KtfGBlCoB7PqaIpB575Wam6UlEa0A.CYM86ZvfS76lfGzbCaA3iQ","createdAt":"2025-06-26T22:36:22.551Z"},"code":{"value":100,"code":100,"name":"authorized"},"timestamp":"2025-06-26T22:36:23.127Z"}
+============= FIN HTTP CLIENT =============
+
+----------------------------------Response Info---------------------------------
+Inicio: 2025-06-26 16:37:27
+-------------------------------------------------------------------------------
+Código Estado: 200
+Cuerpo:
+
+                              {
+                                "status": "OK",
+                                "message": "The credentials are valid",
+                                "data": {
+                                  "refreshToken": "eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.CE65ZrqIbq4PMzj0nn2y6oegMAbqkpcK9oSimf-_AdcBzYRc2Sipf59zOfIlbYoxrsxLD9WhQBQMFoui_mAdx24jkRyTLFQzjk8Odexek1KUXw1XcsXEBpUQ5P7CZh9iDvmUoxNsbHikR4JmG8_Iu5YR-PhBbOyCClgrpWh-anrgFssuAu7ai_V3XOqiwnmhRzQQgkdzkxi9as8oks2q-xQLU_qTU54tw8exHIPVS4Q0oU1oy3DTWoCD7ilvZgknBv-3y5Ao39ggMA6DSoiax7wPyNHKoTf-EQdD9bhasCmWC9BJm231BJGqniAdwxjUH75ukvslzqfbclpxokg_lg.lJrycj73bZZdhp-V.Mtr7voXzLoX-cRwFV0AaCxAwYeBsgVyiSm67wmO7i2iWjq68lAOX_AcAupg6YvGCIzaxiUyR45gj-V88YDsRncHa6K6qOxG5o3Avow3w42ht7rG26PFnn8ZQzl3tEHigvFUXP02z_ZSXmzUnaAzM9zCxXYCd_3vVwL79HG3g5B6rU81gcVhP2r_bfg4bJjfbTBNUdTw6SaRk1UR_4Su-JG9Ui8tbuUjpcEkFTzHiWFMknXy_IkBjfp5eNa0bhrT1zgtLlYe3gp2Ys-oaq__3tj58l-rPkGu2Y9D6W55ZbDVRl5-gs6sGTYKBT43B7Uai-eN-ErSv_3UgppH3DbWJDjynSIt5LXOrT2SqwvHicILL9amfxgb09xDIKiMIx01JxbZ_aGdbvs8P5OHDvwcma4WwbOcml6TEY8Kb54jBIrHy4t-qEc9zMLNLHuUNVtko6sv4yAN2WnqlqiFPvX-xVW9ak706m_aAQwrvgmvOaBJC8bbKczLVIuGO4F6m_0ke8cHVdJTpGN38_VjWrnVWejEStCV3HRKlFyk-b8ypn3GNQsKshhYWMGknAWzyp0ufD2rDuVoT_eTPglo1KoA5BPN3s0gbldNX3BlBaKHnT0bFmuqmgMp1CUWFuDmx9yF6gml2LzJqI4bi_ND8LJXrXmQILh4RxVYuf7JfN6A3DqBsLXwd5MjXeEU0a5f9Bl_T9tZddGQWITwWNNf-q2torxi1-72H-W-2YsN4afqhAE7ooypvtTWu54dntrPkNxNXjvNwvNiUlwFC2g52U-vpzT7fDAQgAmLRRZe28G5EKcTZZS0Cwpdgfxw76ZyKb6S8eXfQHf8lw00VgEGlB09FiQrQRv-JCkKOVilpUqHhfx995Os_sPf30PPfT7uZajxf85r59box2Psxq1D8jejTn5_RypQBd_p1hMUT-ZWJqY40ECbXYw0fQGizluqdbGwilkc7-2cIdGu-PmF7nJEkhY30eLS_gHowTmBimV1YzieD3yp0NA0VNzCxNcsAw7L3mY0Dvn5UNFvDOL5Q9-9tCvFSnmaXAMMvlI7IGcHkgr07BwusHPLq7YGjH52irGwXwInx8FPP2pRu-xFqbRk59ECIHTWQO1bwC6T0S2qQXClIbIgi26vbWGd4MocZV9ynNj42B7SCLCeJ1jsolxPtBpPaun5zrpDJqMHnwic_zKoZ0-85Rp0W0vlxsmN9rmQwE4VJMWphfuNcMwRb5cWIW9ztAlqzTwBiReosK8odur2jYSPea6KtfGBlCoB7PqaIpB575Wam6UlEa0A.CYM86ZvfS76lfGzbCaA3iQ",
+                                  "createdAt": "2025-06-26T22:36:22.551Z"
+                                },
+                                "code": {
+                                  "value": "100",
+                                  "name": "authorized",
+                                  "code": "100"
+                                },
+                                "timeStamp": "2025-06-26T22:36:23.127Z"
+                              }
+----------------------------------Response Info---------------------------------
+Fin: 2025-06-26 16:37:27
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+Controlador: Login
+Action: Login
+Fin: 2025-06-26 16:37:27
+-------------------------------------------------------------------------------
+
+
+[Tiempo Total de Ejecución]: 66554 ms
+---------------------------Fin de Log-------------------------
+Final: 2025-06-26 16:37:27
+-------------------------------------------------------------------
+
