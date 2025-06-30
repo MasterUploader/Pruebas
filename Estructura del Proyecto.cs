f2d1aaadb0ddaa3b78a3b3f5c0c5833e93d0a99b
MasterUploader <MasterUploader@users.noreply.github.com>
2025-06-30 17:02:59 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,87 +1,30 @@
----------------------------Inicio de Log-------------------------
-Inicio: 2025-06-30 16:50:48
--------------------------------------------------------------------
+string sqlQuery = "INSERT INTO BCAH96DTA.UTH01CCC (CCC00GUID, CCC01CORR, CCC02FECH, CCC03HORA, CCC04CAJE, CCC05BANC, CCC06SUCU, CCC07TERM, CCC08LIMI, CCC09NTOK, CCC10STAT, CCC11MESS, CCC12DTID, CCC12DTNA, CCC13DTPO, CCC13TIST, CCC14MDTI, CCC15MDHM, CCC16MDNT, CCC17COVA, CCC18CONA, CCC19ERRO, CCC20MENS) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+using var command = _connection.GetDbCommand(_contextAccessor.HttpContext!);
+command.CommandText = sqlQuery;
 
--------------------------------------------------------------------------------
-Controlador: Login
-Action: Login
-Inicio: 2025-06-30 16:50:48
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
+command.CommandType = System.Data.CommandType.Text;
+command.Parameters.Add("CCC00GUID", OleDbType.Char).Value = getCompaniesDto.CamposObligatoriosModel.Guid;
+command.Parameters.Add("CCC01CORR", OleDbType.Char).Value = correlativo.ToString();
+command.Parameters.Add("CCC02FECH", OleDbType.Char).Value = getCompaniesDto.CamposObligatoriosModel.Fecha;
+command.Parameters.Add("CCC03HORA", OleDbType.Char).Value = getCompaniesDto.CamposObligatoriosModel.Hora;
+command.Parameters.Add("CCC04CAJE", OleDbType.Char).Value = getCompaniesDto.CamposObligatoriosModel.Cajero;
+command.Parameters.Add("CCC05BANC", OleDbType.Char).Value = getCompaniesDto.CamposObligatoriosModel.Banco;
+command.Parameters.Add("CCC06SUCU", OleDbType.Char).Value = getCompaniesDto.CamposObligatoriosModel.Sucursal;
+command.Parameters.Add("CCC07TERM", OleDbType.Char).Value = getCompaniesDto.CamposObligatoriosModel.Terminal;
+command.Parameters.Add("CCC08LIMI", OleDbType.Char).Value = getCompaniesDto.Limit;
+command.Parameters.Add("CCC09NTOK", OleDbType.Char).Value = getCompaniesDto.NextToken;
+command.Parameters.Add("CCC10STAT", OleDbType.Char).Value = list2.Status;
+command.Parameters.Add("CCC11MESS", OleDbType.Char).Value = list2.Message;
+command.Parameters.Add("CCC12DTID", OleDbType.Char).Value = list3.Id;
+command.Parameters.Add("CCC12DTNA", OleDbType.Char).Value = list3.Name;
+command.Parameters.Add("CCC13DTPO", OleDbType.Char).Value = (list3.PayableOptions == null) ? "Nulo" : string.Join(",", list3.PayableOptions);
+command.Parameters.Add("CCC13TIST", OleDbType.Char).Value = list2.Timestamp.ToString();
+command.Parameters.Add("CCC14MDTI", OleDbType.Char).Value = list2.Metadata.Items;
+command.Parameters.Add("CCC15MDHM", OleDbType.Char).Value = ((list2.Metadata.HasMore.ToString() == null) ? "NO" : list2.Metadata.HasMore.ToString());
+command.Parameters.Add("CCC16MDNT", OleDbType.Char).Value = ((list2.Metadata.NextToken == null) ? "" : list2.Metadata.NextToken);
+command.Parameters.Add("CCC17COVA", OleDbType.Char).Value = ((list2.Code.Value.ToString() == null) ? "124" : list2.Code.Value.ToString());
+command.Parameters.Add("CCC18CONA", OleDbType.Char).Value = ((list2.Code.Name == null) ? "" : list2.Code.Name);
+command.Parameters.Add("CCC19ERRO", OleDbType.Char).Value = getCompaniesResponseDto.Error;
+command.Parameters.Add("CCC20MENS", OleDbType.Char).Value = ((getCompaniesResponseDto.Mensaje == null) ? "Proceso ejecutado Satisfactoriamente" : getCompaniesResponseDto.Mensaje);
 
-----------------------------------Request Info---------------------------------
-Inicio: 2025-06-30 16:50:48
--------------------------------------------------------------------------------
-Método: POST
-URL: /Login/login
-Cuerpo:
-  (Sin cuerpo en la solicitud)
-----------------------------------Request Info---------------------------------
-Fin: 2025-06-30 16:50:48
--------------------------------------------------------------------------------
-
-   : POST
-    Path                : /Login/login
-    Query               : 
-    ContentType         : N/A
-    ContentLength       : 0
-    ClientPort          : 59070
-    LocalIp             : ::1
-    LocalPort           : 7266
-    ConnectionId        : 0HNDO4VMSSTQJ
-    Referer             : https://localhost:7266/swagger/index.html
-----------------------------------------------------------------------
----------------------------Enviroment Info-------------------------
-Fin: 2025-06-30 16:50:48
--------------------------------------------------------------------
-
-============= INICIO HTTP CLIENT =============
-TraceId       : 0HNDO4VMSSTQJ:00000009
-Método        : POST
-URL           : https://collection-api.ginih-sandbox.com/v2/users/login
-Código Status : 200
-Duración (ms) : 1395
-Encabezados   :
-traceparent: 00-6d14e8724412c054fe076abccc48f1d7-44fcb034fb63274c-00
-
-Cuerpo:
-{"username":"daviviendaqa","password":"Davivi12$$"}
-Respuesta:
-{"status":"success","message":"The credentials are valid","data":{"refreshToken":"eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.Uf5OFdbYB-srwjyoE5md5pX5yVxBwXAVXrY46a9ITGg6wifbWMdXc7005roDSrniNpYANXAjkQbyaTyDtPvjEkeSs93Fw5-eLwg8cJd20rTQqIKn09pfQ4sL3WwEXIE5_-nKaqn6FDO-Ej_gj8wqnHR_I-9roAp4KJrNpyCWXofOR5TketW70p8uwUjQv20Ary_Ka3PBJ6C6nVTzA96VFwtPc-aMB0y7jhvvgVXTBBW9oIakvQtGjV-kyIbwt2EPmBnTXgdKhX-Rb6x1lvLf2NbXmyD-512FdT5qX5n0IHtkQRaFAgkyfy4UO3DkPCWxg7UrsVONVeIKf9ntb8zudQ.VtjV7LAnYspMH4kP.Ax4DRE44Aby2q2Vj6lTEZAvXA117nA9Dy1XTtH50av0Px9lQ38LxhjzoGOv59OD7VBDnA_3vkIrtcgQQ8OH2ma7CkLDCrqIRt2VnYyYvN-GElR1WVQ3yXA_1EIE_sCeZ9BjCVLjiFSK5lNYP5Xw8hRIForfLGwPX6Sye6-IWtxtNkO4VAwmHqQacLuP3j1dsKdNHswL4sjiz9JckylmBjwP08nn9_KpLac_QRyAiiK5ZEndGeVs6wARKqIo7OAJJM_1tJThMP-qQfY_s2l5OCLR5rbgmX5zrSEBWeQww4GYzc2YuuuDcIdoXa8HvecUzRgPKA7DNNkTUA497f_kfanqblH4gygP9rJpEiyP7DFlo1_m1z0jS557dVU8murS36urg0DdByuKstVAAdi6z25v4Dvybm2QcHpb5V92gawgdC3JA1ehHfsfg_gT55WlkaQhHs1ykodjjON9R6HDpK_qobJ6gYXV9rBro4G5xa_GTgchPfcdMO3nqKj1tKMGnatQeDVaEW5xTnEhoQzGjzTkumRhll-VlLdGwLJe0Cd7D3LAX6CEujWWWcjUH2ANELCmcXe_22rjV8N3WsGGXSQeTkXOTo6hT7hxJoZMV6ECWOsbWGC4tpUlpstZwlMMN9ysCaxjK29KGbX3V4a1z414LjNLpJD7QrShaWJQEE2Ls_pRyKQBm6vNvBNCjCLnGCXL6VRl2wSzjBZfSi9dcgkCfev9wJG8xNfigfi_eWOmE7IhmkvmT0EUuKxkMi5N0-_ppdlLbGDYKqoBVHByErI737Tr4kquznHKCGs8rUeKpPaYXn98mQBai7DCJlNMwctWBqHwDa9HgVZqBC6L1PLVPP6iQ_n9rDz55wCW7i0JrvA2buWhZjdYfl4SFUbg2_o2cGSFZiX8-b2l_GA1xopf8Vts_3BmPiY2eqQyyy76dR-34yA4EIH11h-0y6HdaXg9X9HQ9AL7QIW-5HtDIbQcEfR_w3AL6KrEZkDtv1o-HvBA6VxkawrxLAlZSxWTfrbQstPgzuaNlbx6Cfz7fcKMWsJGWnKCifGERr-eX59oOwiBm0CYYRgaga25QUsZMZ66o5_psMT2ddg1h7mf2cG8aDzXrC4nuVrCPZM-WDiPIbfCdk8xqbno2sthNTXYtTeLMDcFNVw4EgD11KwoRgUmuyfam2-oFNdtneW6iP8py02TQmnZMEpwp4_JoorgfYMishdR_inHOkqTFm8zGW2Ifar03OWfHOjA1gNqG8_2KJlUxgLOtnqzNXa5wFvRpnzfRCQnhG6wf9RQ.oOBC433O9x_0UeDlvrjc0g","createdAt":"2025-06-30T22:50:49.036Z"},"code":{"value":100,"code":100,"name":"authorized"},"timestamp":"2025-06-30T22:50:49.889Z"}
-============= FIN HTTP CLIENT =============
-
-----------------------------------Response Info---------------------------------
-Inicio: 2025-06-30 16:50:52
--------------------------------------------------------------------------------
-Código Estado: 200
-Cuerpo:
-
-                              {
-                                "status": "OK",
-                                "message": "The credentials are valid",
-                                "data": {
-                                  "refreshToken": "eyJjdHkiOiJKV1QiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiUlNBLU9BRVAifQ.Uf5OFdbYB-srwjyoE5md5pX5yVxBwXAVXrY46a9ITGg6wifbWMdXc7005roDSrniNpYANXAjkQbyaTyDtPvjEkeSs93Fw5-eLwg8cJd20rTQqIKn09pfQ4sL3WwEXIE5_-nKaqn6FDO-Ej_gj8wqnHR_I-9roAp4KJrNpyCWXofOR5TketW70p8uwUjQv20Ary_Ka3PBJ6C6nVTzA96VFwtPc-aMB0y7jhvvgVXTBBW9oIakvQtGjV-kyIbwt2EPmBnTXgdKhX-Rb6x1lvLf2NbXmyD-512FdT5qX5n0IHtkQRaFAgkyfy4UO3DkPCWxg7UrsVONVeIKf9ntb8zudQ.VtjV7LAnYspMH4kP.Ax4DRE44Aby2q2Vj6lTEZAvXA117nA9Dy1XTtH50av0Px9lQ38LxhjzoGOv59OD7VBDnA_3vkIrtcgQQ8OH2ma7CkLDCrqIRt2VnYyYvN-GElR1WVQ3yXA_1EIE_sCeZ9BjCVLjiFSK5lNYP5Xw8hRIForfLGwPX6Sye6-IWtxtNkO4VAwmHqQacLuP3j1dsKdNHswL4sjiz9JckylmBjwP08nn9_KpLac_QRyAiiK5ZEndGeVs6wARKqIo7OAJJM_1tJThMP-qQfY_s2l5OCLR5rbgmX5zrSEBWeQww4GYzc2YuuuDcIdoXa8HvecUzRgPKA7DNNkTUA497f_kfanqblH4gygP9rJpEiyP7DFlo1_m1z0jS557dVU8murS36urg0DdByuKstVAAdi6z25v4Dvybm2QcHpb5V92gawgdC3JA1ehHfsfg_gT55WlkaQhHs1ykodjjON9R6HDpK_qobJ6gYXV9rBro4G5xa_GTgchPfcdMO3nqKj1tKMGnatQeDVaEW5xTnEhoQzGjzTkumRhll-VlLdGwLJe0Cd7D3LAX6CEujWWWcjUH2ANELCmcXe_22rjV8N3WsGGXSQeTkXOTo6hT7hxJoZMV6ECWOsbWGC4tpUlpstZwlMMN9ysCaxjK29KGbX3V4a1z414LjNLpJD7QrShaWJQEE2Ls_pRyKQBm6vNvBNCjCLnGCXL6VRl2wSzjBZfSi9dcgkCfev9wJG8xNfigfi_eWOmE7IhmkvmT0EUuKxkMi5N0-_ppdlLbGDYKqoBVHByErI737Tr4kquznHKCGs8rUeKpPaYXn98mQBai7DCJlNMwctWBqHwDa9HgVZqBC6L1PLVPP6iQ_n9rDz55wCW7i0JrvA2buWhZjdYfl4SFUbg2_o2cGSFZiX8-b2l_GA1xopf8Vts_3BmPiY2eqQyyy76dR-34yA4EIH11h-0y6HdaXg9X9HQ9AL7QIW-5HtDIbQcEfR_w3AL6KrEZkDtv1o-HvBA6VxkawrxLAlZSxWTfrbQstPgzuaNlbx6Cfz7fcKMWsJGWnKCifGERr-eX59oOwiBm0CYYRgaga25QUsZMZ66o5_psMT2ddg1h7mf2cG8aDzXrC4nuVrCPZM-WDiPIbfCdk8xqbno2sthNTXYtTeLMDcFNVw4EgD11KwoRgUmuyfam2-oFNdtneW6iP8py02TQmnZMEpwp4_JoorgfYMishdR_inHOkqTFm8zGW2Ifar03OWfHOjA1gNqG8_2KJlUxgLOtnqzNXa5wFvRpnzfRCQnhG6wf9RQ.oOBC433O9x_0UeDlvrjc0g",
-                                  "createdAt": "2025-06-30T22:50:49.036Z"
-                                },
-                                "code": {
-                                  "value": "100",
-                                  "name": "authorized",
-                                  "code": "100"
-                                },
-                                "timeStamp": "2025-06-30T22:50:49.889Z"
-                              }
-----------------------------------Response Info---------------------------------
-Fin: 2025-06-30 16:50:52
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
-Controlador: Login
-Action: Login
-Fin: 2025-06-30 16:50:52
--------------------------------------------------------------------------------
-
-
-[Tiempo Total de Ejecución]: 3702 ms
----------------------------Fin de Log-------------------------
-Final: 2025-06-30 16:50:52
--------------------------------------------------------------------
-
+command.ExecuteNonQuery();
