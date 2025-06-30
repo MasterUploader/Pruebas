05f320638784af2dedf1f3f0c3c9728bfb97b678
MasterUploader <MasterUploader@users.noreply.github.com>
2025-06-30 17:19:07 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,53 +1,109 @@
-string sqlQuery = @"
-INSERT INTO BCAH96DTA.UTH01CCC (
-    CCC00GUID, CCC01CORR, CCC02FECH, CCC03HORA, CCC04CAJE, CCC05BANC, CCC06SUCU, CCC07TERM, CCC08LIMI, CCC09NTOK, 
-    CCC10STAT, CCC11MESS, CCC12DTID, CCC12DTNA, CCC13DTPO, CCC13TIST, CCC14MDTI, CCC15MDHM, CCC16MDNT, CCC17COVA, 
-    CCC18CONA, CCC19ERRO, CCC20MENS
-) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    
-using var command = _connection.GetDbCommand(_contextAccessor.HttpContext!);
-command.CommandText = sqlQuery;
-command.CommandType = CommandType.Text;
+---------------------------Inicio de Log-------------------------
+Inicio: 2025-06-30 17:17:41
+-------------------------------------------------------------------
 
-AddOleDbParameter(command, "CCC00GUID", OleDbType.Char, getCompaniesDto.CamposObligatoriosModel.Guid);
-AddOleDbParameter(command, "CCC01CORR", OleDbType.Char, correlativo.ToString());
-AddOleDbParameter(command, "CCC02FECH", OleDbType.Char, getCompaniesDto.CamposObligatoriosModel.Fecha);
-AddOleDbParameter(command, "CCC03HORA", OleDbType.Char, getCompaniesDto.CamposObligatoriosModel.Hora);
-AddOleDbParameter(command, "CCC04CAJE", OleDbType.Char, getCompaniesDto.CamposObligatoriosModel.Cajero);
-AddOleDbParameter(command, "CCC05BANC", OleDbType.Char, getCompaniesDto.CamposObligatoriosModel.Banco);
-AddOleDbParameter(command, "CCC06SUCU", OleDbType.Char, getCompaniesDto.CamposObligatoriosModel.Sucursal);
-AddOleDbParameter(command, "CCC07TERM", OleDbType.Char, getCompaniesDto.CamposObligatoriosModel.Terminal);
-AddOleDbParameter(command, "CCC08LIMI", OleDbType.Char, getCompaniesDto.Limit);
-AddOleDbParameter(command, "CCC09NTOK", OleDbType.Char, getCompaniesDto.NextToken);
-AddOleDbParameter(command, "CCC10STAT", OleDbType.Char, list2.Status);
-AddOleDbParameter(command, "CCC11MESS", OleDbType.Char, list2.Message);
-AddOleDbParameter(command, "CCC12DTID", OleDbType.Char, list3.Id);
-AddOleDbParameter(command, "CCC12DTNA", OleDbType.Char, list3.Name);
-AddOleDbParameter(command, "CCC13DTPO", OleDbType.Char, list3.PayableOptions == null ? "Nulo" : string.Join(",", list3.PayableOptions));
-AddOleDbParameter(command, "CCC13TIST", OleDbType.Char, list2.Timestamp.ToString());
-AddOleDbParameter(command, "CCC14MDTI", OleDbType.Char, list2.Metadata.Items);
-AddOleDbParameter(command, "CCC15MDHM", OleDbType.Char, list2.Metadata.HasMore?.ToString() ?? "NO");
-AddOleDbParameter(command, "CCC16MDNT", OleDbType.Char, list2.Metadata.NextToken ?? "");
-AddOleDbParameter(command, "CCC17COVA", OleDbType.Char, list2.Code.Value?.ToString() ?? "124");
-AddOleDbParameter(command, "CCC18CONA", OleDbType.Char, list2.Code.Name ?? "");
-AddOleDbParameter(command, "CCC19ERRO", OleDbType.Char, getCompaniesResponseDto.Error);
-AddOleDbParameter(command, "CCC20MENS", OleDbType.Char, getCompaniesResponseDto.Mensaje ?? "Proceso ejecutado Satisfactoriamente");
+---------------------------Enviroment Info-------------------------
+Inicio: 2025-06-30 17:17:41
+-------------------------------------------------------------------
+Application: MS_BAN_38_UTH_RECAUDACION_PAGOS
+Environment: Development
+ContentRoot: C:\Git\MS_BAN_38_UTH_RecaudacionPagos\MS_BAN_38_UTH_RecaudacionPagos\MS_BAN_38_UTH_RECAUDACION_PAGOS
+Execution ID: 0HNDO5GVHTG0C:00000007
+Client IP: ::1
+User Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36
+Machine Name: HNCSTG015243WAP
+OS: Microsoft Windows NT 10.0.20348.0
+Host: localhost:7266
+Distribución: N/A
+  -- Extras del HttpContext --
+    Scheme              : https
+    Protocol            : HTTP/2
+    Method              : POST
+    Path                : /Companies/GetCompanies
+    Query               : 
+    ContentType         : application/json-patch+json
+    ContentLength       : 48
+    ClientPort          : 59662
+    LocalIp             : ::1
+    LocalPort           : 7266
+    ConnectionId        : 0HNDO5GVHTG0C
+    Referer             : https://localhost:7266/swagger/index.html
+----------------------------------------------------------------------
+---------------------------Enviroment Info-------------------------
+Fin: 2025-06-30 17:17:41
+-------------------------------------------------------------------
 
-command.ExecuteNonQuery();
+-------------------------------------------------------------------------------
+Controlador: Companies
+Action: GetPayments
+Inicio: 2025-06-30 17:17:41
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
-/// <summary>
-/// Agrega un parámetro a un DbCommand, compatible con OleDbCommand y decoradores como LoggingDatabaseCommand.
-/// </summary>
-/// <param name="cmd">Comando al cual se agregará el parámetro.</param>
-/// <param name="name">Nombre del parámetro.</param>
-/// <param name="type">Tipo OleDb del parámetro.</param>
-/// <param name="value">Valor del parámetro.</param>
-private void AddOleDbParameter(DbCommand cmd, string name, OleDbType type, object? value)
-{
-    var param = cmd.CreateParameter();
-    param.ParameterName = name;
-    if (param is OleDbParameter oleParam)
-        oleParam.OleDbType = type;
-    param.Value = value ?? DBNull.Value;
-    cmd.Parameters.Add(param);
-}
+----------------------------------Request Info---------------------------------
+Inicio: 2025-06-30 17:17:41
+-------------------------------------------------------------------------------
+Método: POST
+URL: /Companies/GetCompanies
+Cuerpo:
+
+                              {
+                                "limit": "string",
+                                "nextToken": "string"
+                              }
+----------------------------------Request Info---------------------------------
+Fin: 2025-06-30 17:17:41
+-------------------------------------------------------------------------------
+
+============= INICIO HTTP CLIENT =============
+TraceId       : 0HNDO5GVHTG0C:00000007
+Método        : GET
+URL           : https://collection-api.ginih-sandbox.com/v2/companies?limit=string&NextToken=string
+Código Status : 400
+Duración (ms) : 1049
+Encabezados   :
+Authorization: eyJraWQiOiJmK3pHeDBkQnFrS1BJVFoxK25FQ0haS3FhaDRSdWZOejBMV25WMDJrTFlBPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJlMzNhZGFhNy02NzViLTQ1MzAtYWJhMi0wYTExMjQ2NWYzMGYiLCJjb2duaXRvOmdyb3VwcyI6WyJBcGkiXSwiZW1haWxfdmVyaWZpZWQiOnRydWUsImFkZHJlc3MiOnsiZm9ybWF0dGVkIjoiU1BTIn0sImNvZ25pdG86cHJlZmVycmVkX3JvbGUiOiJhcm46YXdzOmlhbTo6NDc1OTU4MzA3NjI1OnJvbGVcL3VzLWVhc3QtMV9ZN1pDQ2E4dkEtQXBpR3JvdXBSb2xlIiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfWTdaQ0NhOHZBIiwicGhvbmVfbnVtYmVyX3ZlcmlmaWVkIjp0cnVlLCJjb2duaXRvOnVzZXJuYW1lIjoiZGF2aXZpZW5kYXFhIiwib3JpZ2luX2p0aSI6ImVhOTYzOTU4LTY0MWYtNDExMy1hNDcxLTU4MWZlNWMwODY1NSIsImNvZ25pdG86cm9sZXMiOlsiYXJuOmF3czppYW06OjQ3NTk1ODMwNzYyNTpyb2xlXC91cy1lYXN0LTFfWTdaQ0NhOHZBLUFwaUdyb3VwUm9sZSJdLCJhdWQiOiIzM2NqYTZldHJxZWM5ZGV2Zm1kbjkxNmdzaiIsImV2ZW50X2lkIjoiZWFiOTVmYzUtYmI1Ny00YWMxLTgyMWYtOTBiNzY0ZDYyMjM1IiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE3NTEzMjM5NzIsIm5hbWUiOiJEYXZpdmllbmRhIiwicGhvbmVfbnVtYmVyIjoiKzUwNDk5OTk5OTk5IiwiZXhwIjoxNzUxMzI2MzY5LCJpYXQiOjE3NTEzMjU0NjksImZhbWlseV9uYW1lIjoiVXNlciIsImp0aSI6IjFjMzZiMTFjLTBmZDQtNDI2Yi1hMzY5LTRmMjQ0OWUwNDVlNyIsImVtYWlsIjoiY2xhdWRpby5lcmF6b0BkYXZpdmllbmRhLmNvbS5obiJ9.n6vFxWSZxNcfI4bDDnizOB70Jr2kb_vZOhk8u8Z-oOIlKlItU_61VSLa4X9RH3ILQddPA0B2sVMyRvw9sZVCgAo2krOtPbfWibN6jMxh3ju6MR8-NSrTu37oeWfqXBik8Mym1ikO2ZVn6QIz1pwMmcjMReLz3E1UtdXKo8_f0kErgdTutt7LhRJYpdaQpBvrsMLinn6wEswT84f5-uXs196DUYD5mwuMHxCXzEngl46rhgvF5992y7Vg110fYehrfn4NZoVyPOPG0WH6Ponr2SnQhhcV9LuraPeBCmGTwplMTtmeaRxP4fZ7k8dOm7F1la_pY4T45Uxre-VRNIwosg
+traceparent: 00-eb9d210e75dddca56a59ed2e2d95863b-dc7dd230ff6da5fa-00
+
+Respuesta:
+{"status":"error","message":"Error when retrieve the data, see the result for more information","code":{"code":114,"name":"invalid_request"},"metadata":{"items":0,"hasMore":false,"nextToken":null},"timestamp":"2025-06-30T23:17:50.945Z"}
+============= FIN HTTP CLIENT =============
+
+----------------------------------Response Info---------------------------------
+Inicio: 2025-06-30 17:18:10
+-------------------------------------------------------------------------------
+Código Estado: 400
+Cuerpo:
+
+                              {
+                                "status": "BadRequest",
+                                "message": "La consulta no devolvio valores",
+                                "data": [],
+                                "code": {
+                                  "value": 0,
+                                  "name": "invalid_request",
+                                  "code": "114"
+                                },
+                                "timestamp": "2025-06-30T23:17:50.945+00:00",
+                                "metadata": {
+                                  "items": 0,
+                                  "hasMore": false
+                                },
+                                "error": "1",
+                                "mensaje": "Proceso ejecutado InSatisfactoriamente"
+                              }
+----------------------------------Response Info---------------------------------
+Fin: 2025-06-30 17:18:10
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+Controlador: Companies
+Action: GetPayments
+Fin: 2025-06-30 17:18:10
+-------------------------------------------------------------------------------
+
+
+[Tiempo Total de Ejecución]: 28720 ms
+---------------------------Fin de Log-------------------------
+Final: 2025-06-30 17:18:10
+-------------------------------------------------------------------
+
