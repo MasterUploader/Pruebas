b488bdf411472f10470a69d85b6ebe7c0bae0793
MasterUploader <MasterUploader@users.noreply.github.com>
2025-06-30 18:30:01 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,34 +1,10 @@
-
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.10.35027.167
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Logging", "Logging\Logging.csproj", "{6FF8BFCF-4D38-46E6-83B0-3D85AFCD2E59}"
-EndProject
-Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Connections", "Connections\Connections.csproj", "{D6D422B6-A9D3-4D65-A744-0A5920DC8097}"
-EndProject
-Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Common", "Common\Common.csproj", "{04B467A1-D399-4809-BB7D-493AE7C11440}"
-EndProject
-Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Connections.Abstractions", "Connections.Abstractions\Connections.Abstractions.csproj", "{B3AC7E38-57B1-43D0-986F-8BE863A59CBA}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Release|x64 = Release|x64
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+GlobalSection(ProjectConfigurationPlatforms) = postSolution
 		{6FF8BFCF-4D38-46E6-83B0-3D85AFCD2E59}.Release|x64.ActiveCfg = Release|x64
 		{6FF8BFCF-4D38-46E6-83B0-3D85AFCD2E59}.Release|x64.Build.0 = Release|x64
 		{D6D422B6-A9D3-4D65-A744-0A5920DC8097}.Release|x64.ActiveCfg = Release|x64
 		{D6D422B6-A9D3-4D65-A744-0A5920DC8097}.Release|x64.Build.0 = Release|x64
-		{04B467A1-D399-4809-BB7D-493AE7C11440}.Release|x64.ActiveCfg = Release|Any CPU
-		{04B467A1-D399-4809-BB7D-493AE7C11440}.Release|x64.Build.0 = Release|Any CPU
-		{B3AC7E38-57B1-43D0-986F-8BE863A59CBA}.Release|x64.ActiveCfg = Release|Any CPU
-		{B3AC7E38-57B1-43D0-986F-8BE863A59CBA}.Release|x64.Build.0 = Release|Any CPU
+		{04B467A1-D399-4809-BB7D-493AE7C11440}.Release|x64.ActiveCfg = Release|x64
+		{04B467A1-D399-4809-BB7D-493AE7C11440}.Release|x64.Build.0 = Release|x64
+		{B3AC7E38-57B1-43D0-986F-8BE863A59CBA}.Release|x64.ActiveCfg = Release|x64
+		{B3AC7E38-57B1-43D0-986F-8BE863A59CBA}.Release|x64.Build.0 = Release|x64
 	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {7FF49303-7B7F-442E-9C1D-6F8AD3763918}
-	EndGlobalSection
-EndGlobal
