00c2dc041906feaef127be1f99363c121cf83fc8
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-01 16:44:54 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,72 +1,298 @@
 using Connections.Interfaces;
-using Connections.Managers;
-using Logging.Abstractions;
-using Logging.Decorators;
-using Microsoft.AspNetCore.Http;
-using System.Data.Common;
-using System.Data.OleDb;
+using Microsoft.AspNetCore.Mvc;
+using MS_BAN_38_UTH_RECAUDACION_PAGOS.Models.Dtos.AutenticacionDtos;
+using MS_BAN_38_UTH_RECAUDACION_PAGOS.Repository.IRepository.Autenticacion;
+using MS_BAN_38_UTH_RECAUDACION_PAGOS.Utils;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
 
-namespace Connections.Providers.Database;
+namespace MS_BAN_38_UTH_RECAUDACION_PAGOS.Repository;
 
 /// <summary>
-/// Proveedor de conexión a base de datos AS400 utilizando OleDb.
-/// Esta implementación está optimizada para ejecución directa de comandos SQL y logging estructurado.
+/// Clase Login Repository.
 /// </summary>
-public partial class AS400ConnectionProvider : LoggingDatabaseConnectionWrapper
+public class LoginRepository(IHttpClientFactory _httpClientFactory, IDatabaseConnection _connection, IHttpContextAccessor _contextAccessor) : ILoginRepository
 {
-    private readonly OleDbConnection _oleDbConnection;
-    private readonly ILoggingService _loggingService;
 
     /// <summary>
-    /// Inicializa una nueva instancia de <see cref="AS400ConnectionProvider"/>.
+    /// Para obtener un refresh-token necesitas hacer una llamada al endpoint de login. La respuesta a esta llamada obtendrá un secret token que necesitarás para obtener JWT válidos para los request. 
+    /// El JWT dura 5 minutos y el refresh-token tiene una duración por defecto de 2 años (Recibirás una notificación para que puedas renovarlo antes de vencer). Si ya tienes acceso al portal de integración, puedes acceder o volver a generar uno nuevo en la vista developer -> secret.
     /// </summary>
-    /// <param name="connectionString">Cadena de conexión a AS400 en formato OleDb.</param>
-    /// <param name="loggingService">Servicio de logging estructurado.</param>
-    public AS400ConnectionProvider(string connectionString, ILoggingService loggingService) : base(new OleDbConnection(connectionString), loggingService)
+    /// <param name="usuarioLoginDto">Objeto de Transferencia Dto</param>
+    /// <returns>Retorna un Objeto de Tipo PostLoginResponseDto</returns>
+
+    [HttpPost]
+    public async Task<PostLoginResponseDto> Login(PostUsuarioLoginDto usuarioLoginDto)
     {
-        _oleDbConnection = new OleDbConnection(connectionString);
-        _loggingService = loggingService;
+        try
+        {
+            //Obtenemos las variables globales
+            string _baseUrl = GlobalConnection.Current.Host;
+
+            PostLoginResponseDto _postLoginResponseDto = new();
+
+            using var client = _httpClientFactory.CreateClient("GINIH");
+            if (!string.IsNullOrEmpty(_baseUrl) && Uri.IsWellFormedUriString(_baseUrl, UriKind.RelativeOrAbsolute))
+            {
+                client.BaseAddress = new Uri(_baseUrl);
+            }
+
+            var content = new StringContent(JsonConvert.SerializeObject(usuarioLoginDto), Encoding.UTF8);
+
+            using HttpResponseMessage response = await client.PostAsync(client.BaseAddress + "/users/login", content);
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+            var deserialized = JsonConvert.DeserializeObject<PostLoginResponseDto>(responseContent);
+
+            if (response.IsSuccessStatusCode && deserialized is not null)
+            {
+                _postLoginResponseDto = deserialized;
+                _postLoginResponseDto.Status = response.StatusCode.ToString();
+                var saveToken = new Token(deserialized, _connection, _contextAccessor);
+                var badResponse = new PostLoginResponseDto
+                {
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "No se pudo guardar datos en la tabla de token"
+                };
+                return saveToken.SavenTokenUTH() ? _postLoginResponseDto : badResponse;
+            }
+            else if (!response.IsSuccessStatusCode && deserialized is not null)
+            {
+                _postLoginResponseDto = deserialized;
+                _postLoginResponseDto.Status = response.StatusCode.ToString();
+                return _postLoginResponseDto;
+            }
+            _postLoginResponseDto.Status = response.StatusCode.ToString();
+            _postLoginResponseDto.Message = deserialized!.Message ?? "No devolvio valores la consulta";
+            return _postLoginResponseDto;
+        }
+        catch (Exception ex)
+        {
+            PostLoginResponseDto _postLoginResponseDto = new()
+            {
+                Status = HttpStatusCode.NotFound.ToString(),
+                Message = ex.Message
+            };
+
+            return _postLoginResponseDto;
+        }
+    }
+}
+
+
+
+
+using Connections.Interfaces;
+using MS_BAN_38_UTH_RECAUDACION_PAGOS.Models.Dtos.AutenticacionDtos;
+using SUNITP.LIB.ManagerProcedures;
+using SUNITP.LIB.ManagerProcedures.Concrete;
+using SUNITP.LIB.QueryStringGenerator;
+using System.Data.OleDb;
+using System.Globalization;
+
+namespace MS_BAN_38_UTH_RECAUDACION_PAGOS.Utils;
+/// <summary>
+/// Clase Utilitaria Token, contiene multiples métodos requeridos para manipular los tokens de Ginih.
+/// </summary>
+public class Token 
+{
+    private readonly IDatabaseConnection _connection;
+    private readonly IHttpContextAccessor _contextAccessor;
+    private EasyMappingTool response = new();
+    private readonly string? _tableName = "TOKENUTH";
+    private readonly string? _library = "BCAH96DTA";
+    private readonly string _status = string.Empty;
+    private readonly string _message = string.Empty;
+    private readonly string _rToken = string.Empty;
+    private readonly string _createdAt = string.Empty;
+    private readonly string _timeStamp = string.Empty;
+    private readonly string _value = string.Empty;
+    private readonly string _name = string.Empty;
+    private string _vence = string.Empty;
+    private string _creado = string.Empty;
+    private string _token = string.Empty;
+
+    /// <summary>
+    /// Constructor de la Clase Token.
+    /// </summary>
+    /// <param name="tokenStructure">Instancia de PostLoginResponseDto</param>
+    /// <param name="connection">Instancia de IDatabaseConnection. </param>
+    public Token(PostLoginResponseDto tokenStructure, IDatabaseConnection connection, IHttpContextAccessor contextAccessor)
+    {
+        _status = tokenStructure.Status;
+        _message = tokenStructure.Message;
+        _rToken = tokenStructure.Data.RefreshToken;
+        _createdAt = tokenStructure.Data.CreatedAt;
+        _timeStamp = tokenStructure.TimeStamp;
+        _value = tokenStructure.Code.Value;
+        _name = tokenStructure.Code.Name;
+        _connection = connection;
+        _contextAccessor = contextAccessor;
     }
 
     /// <summary>
-    /// Abre la conexión si no está ya abierta.
+    /// Constructo de Clase Token sin parametros de ingreso, inicializa campos.
     /// </summary>
-    public void Open()
+    public Token(IDatabaseConnection connection, IHttpContextAccessor contextAccessor)
     {
-        if (_oleDbConnection.State != System.Data.ConnectionState.Open)
-            _oleDbConnection.Open();
+        _connection = connection;
+        _contextAccessor = contextAccessor;
     }
 
     /// <summary>
-    /// Cierra la conexión si está abierta.
+    /// Guarda el Token en la tabla en el as400
     /// </summary>
-    public void Close()
+    /// <returns>Retorna un valor boleano segun sea exitoso o no el almacenamiento</returns>
+    public bool SavenTokenUTH()
     {
-        if (_oleDbConnection.State != System.Data.ConnectionState.Closed)
-            _oleDbConnection.Close();
+        try
+        {
+            var temp = DateTime.ParseExact(_createdAt, "yyyy-MM-ddTHH:mm:ss.fffZ", CultureInfo.InvariantCulture).AddYears(2);
+            _vence = temp.ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
+
+            _connection.Open();
+
+            var oleDBCommand = _connection.GetDbCommand(_contextAccessor.HttpContext!);
+            if (oleDBCommand.Connection is not OleDbConnection oleDbConnection)
+            {
+                return false;
+            }
+            var oleDBConnection = (OleDbConnection)oleDBCommand.Connection;
+
+            var sqsg = new ServiceQueryStringGenerator();
+            EasyCrudDataModels ecdm = new(oleDBConnection);
+            var fQuery = new FieldsQuery();
+            var validacion = fQuery.FieldQuery("ID", "1", OleDbType.Integer, 1, "0");
+
+            sqsg._iQueryStringGenerator.SelectAll();
+            sqsg._iQueryStringGenerator.From(_library, _tableName);
+            sqsg._iQueryStringGenerator.WhereAnd(validacion, "=");
+            var responseS = ecdm.SelectExecute(sqsg);
+
+            if (responseS.Count == 0)
+            {
+                sqsg = new ServiceQueryStringGenerator();
+                sqsg._iQueryStringGenerator.InsertIntoFrom(_library, _tableName);
+                sqsg._iQueryStringGenerator.InsertValue("ID", "1", OleDbType.Integer, 1, 0);
+                sqsg._iQueryStringGenerator.InsertValue("STATUS", _status, OleDbType.VarChar, 50, 0);
+                sqsg._iQueryStringGenerator.InsertValue("MESSAGE", _message, OleDbType.VarChar, 100, 0);
+                sqsg._iQueryStringGenerator.InsertValue("RTOKEN", _rToken, OleDbType.VarChar, 2000, 0);
+                sqsg._iQueryStringGenerator.InsertValue("CREATEDAT", _createdAt, OleDbType.VarChar, 100, 0);
+                sqsg._iQueryStringGenerator.InsertValue("TIMESTAMP", _timeStamp, OleDbType.VarChar, 100, 0);
+                sqsg._iQueryStringGenerator.InsertValue("VALUE", _value, OleDbType.VarChar, 3, 0);
+                sqsg._iQueryStringGenerator.InsertValue("NAME", _name, OleDbType.VarChar, 100, 0);
+                sqsg._iQueryStringGenerator.InsertValue("VENCE", _vence, OleDbType.VarChar, 100, 0);
+
+                //respuesta del query
+                response = ecdm.InsertExecute(sqsg);
+            }
+            else
+            {
+                sqsg = new ServiceQueryStringGenerator();
+                sqsg._iQueryStringGenerator.UpdateFrom(_library, _tableName);
+                sqsg._iQueryStringGenerator.UpdateSet("ID", "1", OleDbType.Integer, 1, 0);
+                sqsg._iQueryStringGenerator.UpdateSet("STATUS", _status, OleDbType.VarChar, 50, 0);
+                sqsg._iQueryStringGenerator.UpdateSet("MESSAGE", _message, OleDbType.VarChar, 100, 0);
+                sqsg._iQueryStringGenerator.UpdateSet("RTOKEN", _rToken, OleDbType.VarChar, 2000, 0);
+                sqsg._iQueryStringGenerator.UpdateSet("CREATEDAT", _createdAt, OleDbType.VarChar, 100, 0);
+                sqsg._iQueryStringGenerator.UpdateSet("TIMESTAMP", _timeStamp, OleDbType.VarChar, 100, 0);
+                sqsg._iQueryStringGenerator.UpdateSet("VALUE", _value, OleDbType.VarChar, 3, 0);
+                sqsg._iQueryStringGenerator.UpdateSet("NAME", _name, OleDbType.VarChar, 100, 0);
+                sqsg._iQueryStringGenerator.UpdateSet("VENCE", _vence, OleDbType.VarChar, 100, 0);
+                sqsg._iQueryStringGenerator.WhereAnd(validacion, "=");
+
+                //respuesta del query
+                response = ecdm.UpdateExecute(sqsg);
+            }
+
+
+            if (response.GetEasyParameter("_defaultError").value.Equals(""))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch (Exception ex)
+        {
+            //Temporal hasta manejar los logs
+            Console.Clear();
+            Console.WriteLine(ex.Message);
+            return false;
+        }
     }
 
     /// <summary>
-    /// Indica si la conexión está actualmente abierta.
+    /// Metodo que obtiene el token secreto de la tabla en el as400
     /// </summary>
-    public bool IsConnected => _oleDbConnection?.State == System.Data.ConnectionState.Open;
+    /// <param name="rToken">Token registrado.</param>
+    /// <returns>Retorna un string de tipo out por parametro</returns>
 
-    /// <summary>
-    /// Obtiene un <see cref="DbCommand"/> decorado con soporte de logging estructurado.
-    /// </summary>
-    /// <param name="context">Contexto HTTP opcional para trazabilidad adicional.</param>
-    /// <returns>Comando decorado con logging.</returns>
-    public DbCommand GetDbCommand(HttpContext? context = null)
+    public bool GetToken(out string rToken)
     {
-        var command = _oleDbConnection.CreateCommand();
-        return new LoggingDbCommandWrapper(command, _loggingService); // ← El decorador correcto
-    }
+        try
+        {
+            _connection.Open();
+            var oleDBCommand = _connection.GetDbCommand(_contextAccessor.HttpContext!);
+            if (oleDBCommand.Connection is not OleDbConnection oleDbConnection)
+            {
+                rToken = "";
+                return false;
+            }
 
-    /// <summary>
-    /// Libera los recursos de la conexión.
-    /// </summary>
-    public void Dispose()
-    {
-        _oleDbConnection?.Dispose();
+            var oleDBConnection = (OleDbConnection)oleDBCommand.Connection;
+
+            var sqsg = new ServiceQueryStringGenerator();
+            EasyCrudDataModels ecdm = new(oleDBConnection);
+
+            var fQuery = new FieldsQuery();
+            var validacion = fQuery.FieldQuery("ID", "1", OleDbType.Integer, 1, "0");
+
+            sqsg._iQueryStringGenerator.SelectAll();
+            sqsg._iQueryStringGenerator.From(_library, _tableName);
+            sqsg._iQueryStringGenerator.WhereAnd(validacion, "=");
+            var responseS = ecdm.SelectExecute(sqsg);
+
+            //Cerrar Conexión
+            _connection.Close();
+
+            if (responseS.Count == 0)
+            {
+                rToken = "";
+                return false;
+            }
+
+            foreach (var item in responseS)
+            {
+                _vence = item.GetValue("VENCE");
+                _creado = item.GetValue("CREATEDAT");
+                _token = item.GetValue("RTOKEN");
+            }
+
+            DateTime date1 = DateTime.ParseExact(_vence, "yyyy-MM-ddTHH:mm:ss.fffZ", CultureInfo.InvariantCulture);
+            DateTime date2 = DateTime.ParseExact(_creado, "yyyy-MM-ddTHH:mm:ss.fffZ", CultureInfo.InvariantCulture);
+
+            var diferenceTime = date1 - date2;
+
+            if (diferenceTime.Days > 0)
+            {
+                rToken = _token;
+                return true;
+            }
+            rToken = "";
+            return false;
+        }
+        catch (Exception ex)
+        {
+            //Temporal hasta manejar los logs
+            Console.Clear();
+            Console.WriteLine(ex.Message);
+            _connection.Close();
+            rToken = "";
+            return false;
+        }
+
     }
 }
