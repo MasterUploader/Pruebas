9977b89ed2998a92a1dba7b1038769b58a64f9f3
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-03 16:47:23 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,51 +1,77 @@
-El codigo del insert que sea similar a este o en su defecto usando CreateParameter
+private async Task GuardarDetalleReporteAsync(
+    IDatabaseConnection _connection,
+    IHttpContextAccessor _contextAccessor,
+    string agentCd,
+    string agentTransTypeCode,
+    string contractTypeCd,
+    List<DetAct> detActList)
+{
+    if (detActList == null || detActList.Count == 0)
+        return;
 
-int correlativo = 0;
-FieldsQuery param = new();
+    var today = DateTime.Now.ToString("ddMMyyyy");
+    int correlativoGrupo = 1;
 
-string sqlQuery = "INSERT INTO BCAH96DTA.tabla (campos) VALUES(?)";
-using var command = _connection.GetDbCommand(_contextAccessor.HttpContext!);
-command.CommandText = sqlQuery;
+    foreach (var act in detActList)
+    {
+        if (act?.Details?.DetailList == null)
+            continue;
 
-command.CommandType = System.Data.CommandType.Text;
+        int correlativoDetalle = 1;
 
-param.AddOleDbParameter(command, "APU00GUID", OleDbType.Char, getPaymentsDto.CamposObligatoriosModel.Guid);
-param.AddOleDbParameter(command, "APU01CORR", OleDbType.Numeric, correlativo);
-param.AddOleDbParameter(command, "APU02FECH", OleDbType.Char, getPaymentsDto.CamposObligatoriosModel.Fecha);
-param.AddOleDbParameter(command, "APU03HORA", OleDbType.Char, getPaymentsDto.CamposObligatoriosModel.Hora);
-param.AddOleDbParameter(command, "APU04CAJE", OleDbType.Char, getPaymentsDto.CamposObligatoriosModel.Cajero);
-param.AddOleDbParameter(command, "APU05BANC", OleDbType.Char, getPaymentsDto.CamposObligatoriosModel.Banco);
-param.AddOleDbParameter(command, "APU06SUCU", OleDbType.Char, getPaymentsDto.CamposObligatoriosModel.Sucursal);
-param.AddOleDbParameter(command, "APU07TERM", OleDbType.Char, getPaymentsDto.CamposObligatoriosModel.Terminal);
-param.AddOleDbParameter(command, "APU08STAT", OleDbType.Char, getPaymentsResponseDto.Status);
-param.AddOleDbParameter(command, "APU09MSSG", OleDbType.Char, getPaymentsResponseDto.Message);
-param.AddOleDbParameter(command, "APU10DTID", OleDbType.Char, getPaymentsResponseDto.Data.Id);
-param.AddOleDbParameter(command, "APU11DTNA", OleDbType.Char, getPaymentsResponseDto.Data.Name);
-param.AddOleDbParameter(command, "APU12CUID", OleDbType.Char, getPaymentsResponseDto.Data.Customer.Id);
-param.AddOleDbParameter(command, "APU13CUNA", OleDbType.Char, getPaymentsResponseDto.Data.Customer.Name);
-param.AddOleDbParameter(command, "APU14COID", OleDbType.Char, getPaymentsResponseDto.Data.Company.Id);
-param.AddOleDbParameter(command, "APU15CONA", OleDbType.Char, getPaymentsResponseDto.Data.Company.Name);
-param.AddOleDbParameter(command, "APU16DREF", OleDbType.Char, getPaymentsResponseDto.Data.DocumentReference);
-param.AddOleDbParameter(command, "APU17REFE", OleDbType.Char, getPaymentsResponseDto.Data.ReferenceId);
-param.AddOleDbParameter(command, "APU18AMVA", OleDbType.Char, getPaymentsResponseDto.Data.Amount.Value);
-param.AddOleDbParameter(command, "APU19AMCU", OleDbType.Char, getPaymentsResponseDto.Data.Amount.Currency);
-param.AddOleDbParameter(command, "APU20SUTO", OleDbType.Char, getPaymentsResponseDto.Data.Amount.Breakdown.Subtotal);
-param.AddOleDbParameter(command, "APU19PFEE", OleDbType.Char, getPaymentsResponseDto.Data.Amount.Breakdown.ProcessingFee);
-param.AddOleDbParameter(command, "APU20SCHA", OleDbType.Char, getPaymentsResponseDto.Data.Amount.Breakdown.Surcharge);
-param.AddOleDbParameter(command, "APU21DICO", OleDbType.Char, getPaymentsResponseDto.Data.Amount.Breakdown.Discount);
-param.AddOleDbParameter(command, "APU22BTAX", OleDbType.Char, getPaymentsResponseDto.Data.Amount.Breakdown.Tax);
-param.AddOleDbParameter(command, "APU23TOTA", OleDbType.Char, getPaymentsResponseDto.Data.Amount.Breakdown.Total);
-param.AddOleDbParameter(command, "APU24CRAT", OleDbType.Char, getPaymentsResponseDto.Data.CreatedAt);
-param.AddOleDbParameter(command, "APU25TIST", OleDbType.Char, getPaymentsResponseDto.TimeStamp);
-param.AddOleDbParameter(command, "APU26COVA", OleDbType.Char, getPaymentsResponseDto.Code.Value);
-param.AddOleDbParameter(command, "APU27CONA", OleDbType.Char, getPaymentsResponseDto.Code.Name);
-param.AddOleDbParameter(command, "APU28ERRO", OleDbType.Char, getPaymentsResponseDto.Error);
-param.AddOleDbParameter(command, "APU29MENS", OleDbType.Char, getPaymentsResponseDto.Mensaje);
+        foreach (var detail in act.Details.DetailList)
+        {
+            FieldsQuery param = new();
 
-command.ExecuteNonQuery();
+            string insertQuery = @"
+                INSERT INTO BCAH96DTA.TU_TABLA_REPORTE (
+                    REP00UID, REP01DCOR, REP02ATTC, REP03ACTC, REP04ADT,
+                    REP05ACD, REP06SCD, REP06OCCD, REP08OCCD, REP09DCCD,
+                    REP10DCCD, REP11PTCD, REP12OACD, REP13DCOR, REP14MTC,
+                    REP15CNM, REP16AON, REP17OAM, REP18DAM, REP19FAM,
+                    REP20DISC, REP21COP, REP22COP, REP23COP, REP24COP,
+                    REP25COP, REP26COP, REP27COP, REP28COP, REP29COP
+                ) VALUES (
+                    ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+                )";
 
-REP00UID es unico
-REP01DCOR es de 1 a M
-REP13DCOR es de 1 a M
+            using var command = _connection.GetDbCommand(_contextAccessor.HttpContext!);
+            command.CommandText = insertQuery;
+            command.CommandType = CommandType.Text;
 
-Pueden haber muchos REP01DCOR y dentro de este muchos REP13DCOR.
+            param.AddOleDbParameter(command, "REP00UID", OleDbType.Char, Guid.NewGuid().ToString());
+            param.AddOleDbParameter(command, "REP01DCOR", OleDbType.Numeric, correlativoGrupo);
+            param.AddOleDbParameter(command, "REP02ATTC", OleDbType.Char, agentTransTypeCode);
+            param.AddOleDbParameter(command, "REP03ACTC", OleDbType.Char, contractTypeCd);
+            param.AddOleDbParameter(command, "REP04ADT", OleDbType.Char, act.ActivityDate);
+            param.AddOleDbParameter(command, "REP05ACD", OleDbType.Char, agentCd);
+            param.AddOleDbParameter(command, "REP06SCD", OleDbType.Char, act.ServiceCode);
+            param.AddOleDbParameter(command, "REP06OCCD", OleDbType.Char, act.OriginCountryCode);
+            param.AddOleDbParameter(command, "REP08OCCD", OleDbType.Char, act.OriginCurrencyCode);
+            param.AddOleDbParameter(command, "REP09DCCD", OleDbType.Char, act.DestinationCountryCode);
+            param.AddOleDbParameter(command, "REP10DCCD", OleDbType.Char, act.DestinationCurrencyCode);
+            param.AddOleDbParameter(command, "REP11PTCD", OleDbType.Char, act.PaymentTypeCode);
+            param.AddOleDbParameter(command, "REP12OACD", OleDbType.Char, act.OriginAgentCode);
+            param.AddOleDbParameter(command, "REP13DCOR", OleDbType.Numeric, correlativoDetalle);
+            param.AddOleDbParameter(command, "REP14MTC", OleDbType.Char, detail.MovementTypeCode);
+            param.AddOleDbParameter(command, "REP15CNM", OleDbType.Char, detail.ConfirmationNumber);
+            param.AddOleDbParameter(command, "REP16AON", OleDbType.Char, detail.AgentOrderNumber);
+            param.AddOleDbParameter(command, "REP17OAM", OleDbType.Numeric, detail.OriginAmount);
+            param.AddOleDbParameter(command, "REP18DAM", OleDbType.Numeric, detail.DestinationAmount);
+            param.AddOleDbParameter(command, "REP19FAM", OleDbType.Numeric, detail.FeeAmount);
+            param.AddOleDbParameter(command, "REP20DISC", OleDbType.Numeric, detail.DiscountAmount);
+
+            // Rellenar campos REP21COP a REP29COP con espacios
+            for (int i = 21; i <= 29; i++)
+            {
+                string paramName = $"REP{i:D2}COP";
+                param.AddOleDbParameter(command, paramName, OleDbType.Char, " ");
+            }
+
+            await command.ExecuteNonQueryAsync();
+            correlativoDetalle++;
+        }
+
+        correlativoGrupo++;
+    }
+}
