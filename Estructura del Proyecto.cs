78ffe626e3617048e01f3d2134fd7cff284cce57
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-03 11:15:25 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,7 +1,76 @@
-<DATA>
-                        <DET_ACT ACTIVITY_DT="20250626" AGENT_CD="HSH" SERVICE_CD="MTR" ORIG_COUNTRY_CD="USA" ORIG_CURRENCY_CD="USD" DEST_COUNTRY_CD="HND" DEST_CURRENCY_CD="HNL" PAYMENT_TYPE_CD="CSA" O_AGENT_CD="BTS">
-                            <DETAILS>
-                                <DETAIL MOVEMENT_TYPE_CODE="PAYI" CONFIRMATION_NM="79901025626" AGENT_ORDER_NM="79901025626" ORIGIN_AM="200.0000" DESTINATION_AM="5104.6800" FEE_AM="0.0000" DISCOUNT_AM="0.0000" />
-                            </DETAILS>
-                        </DET_ACT>
-                    </DATA>
+using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+[XmlRoot("DATA")]
+public class Data
+{
+    /// <summary>
+    /// Lista de actividades detalladas.
+    /// </summary>
+    [XmlElement("DET_ACT")]
+    public List<DetAct> DetActList { get; set; }
+}
+
+public class DetAct
+{
+    [XmlAttribute("ACTIVITY_DT")]
+    public string ActivityDate { get; set; }
+
+    [XmlAttribute("AGENT_CD")]
+    public string AgentCode { get; set; }
+
+    [XmlAttribute("SERVICE_CD")]
+    public string ServiceCode { get; set; }
+
+    [XmlAttribute("ORIG_COUNTRY_CD")]
+    public string OriginCountryCode { get; set; }
+
+    [XmlAttribute("ORIG_CURRENCY_CD")]
+    public string OriginCurrencyCode { get; set; }
+
+    [XmlAttribute("DEST_COUNTRY_CD")]
+    public string DestinationCountryCode { get; set; }
+
+    [XmlAttribute("DEST_CURRENCY_CD")]
+    public string DestinationCurrencyCode { get; set; }
+
+    [XmlAttribute("PAYMENT_TYPE_CD")]
+    public string PaymentTypeCode { get; set; }
+
+    [XmlAttribute("O_AGENT_CD")]
+    public string OriginAgentCode { get; set; }
+
+    [XmlElement("DETAILS")]
+    public Details Details { get; set; }
+}
+
+public class Details
+{
+    [XmlElement("DETAIL")]
+    public List<Detail> DetailList { get; set; }
+}
+
+public class Detail
+{
+    [XmlAttribute("MOVEMENT_TYPE_CODE")]
+    public string MovementTypeCode { get; set; }
+
+    [XmlAttribute("CONFIRMATION_NM")]
+    public string ConfirmationNumber { get; set; }
+
+    [XmlAttribute("AGENT_ORDER_NM")]
+    public string AgentOrderNumber { get; set; }
+
+    [XmlAttribute("ORIGIN_AM")]
+    public decimal OriginAmount { get; set; }
+
+    [XmlAttribute("DESTINATION_AM")]
+    public decimal DestinationAmount { get; set; }
+
+    [XmlAttribute("FEE_AM")]
+    public decimal FeeAmount { get; set; }
+
+    [XmlAttribute("DISCOUNT_AM")]
+    public decimal DiscountAmount { get; set; }
+}
