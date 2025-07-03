7570f863f53f394d67f2a4b8ea16a033e6f9027a
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-03 08:33:20 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,40 +1,23 @@
-<?xml version="1.0"?>
-<package >
-  <metadata>
-    <id>RestUtilities.QueryBuilder</id>
-    <version>1.0.0</version>
-    <authors>RestUtilities Team</authors>
-    <owners>RestUtilities</owners>
-    <requireLicenseAcceptance>false</requireLicenseAcceptance>
-    <description>Librería modular para la construcción dinámica, segura y multiplataforma de consultas SQL (SELECT, INSERT, UPDATE, JOIN, WHERE, etc.) basada en modelos, expresiones lambda y compatibilidad con múltiples motores (SQL Server, Oracle, AS400, etc.).</description>
-    <copyright>Copyright © 2025</copyright>
-    <tags>sql querybuilder lambda orm dbcontext dynamic safe generator restutilities</tags>
-    <repository type="git" url="https://github.com/RestUtilities/QueryBuilder" />
-    <dependencies>
-      <dependency id="Microsoft.Extensions.DependencyInjection.Abstractions" version="8.0.0" />
-    </dependencies>
-  </metadata>
-</package>
-
-
-<Project Sdk="Microsoft.NET.Sdk">
-
-  <PropertyGroup>
-    <TargetFramework>net8.0</TargetFramework>
-    <GeneratePackageOnBuild>true</GeneratePackageOnBuild>
-    <Version>1.0.0</Version>
-    <Authors>RestUtilities Team</Authors>
-    <Company>RestUtilities</Company>
-    <Description>Librería modular para generar consultas SQL dinámicas multiplataforma (compatible con SQL Server, Oracle, AS400, etc.) con soporte para modelos, atributos, expresiones lambda y validaciones de tipo.</Description>
-    <PackageId>RestUtilities.QueryBuilder</PackageId>
-    <RepositoryUrl>https://github.com/RestUtilities/QueryBuilder</RepositoryUrl>
-    <PackageTags>sql querybuilder lambda expressions orm dbcontext restutilities</PackageTags>
-    <Nullable>enable</Nullable>
-    <LangVersion>latest</LangVersion>
-  </PropertyGroup>
-
-  <ItemGroup>
-    <PackageReference Include="Microsoft.Extensions.DependencyInjection.Abstractions" Version="8.0.0" />
-  </ItemGroup>
-
-</Project>
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Body>
+        <GetRPTResponse xmlns="http://www.btsincusa.com/gp/">
+            <GetRPTResult>
+                <RESPONSE xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="RSDD_RESPONSE" xmlns="http://www.btsincusa.com/gp/">
+                    <OPCODE>0000</OPCODE>
+                    <PROCESS_MSG>THE OPERATION HAS BEEN COMPLETED.</PROCESS_MSG>
+                    <ERROR_PARAM_FULL_NAME></ERROR_PARAM_FULL_NAME>
+                    <PROCESS_DT>20250703</PROCESS_DT>
+                    <PROCESS_TM>091349</PROCESS_TM>
+                    <DATA>
+                        <DET_ACT ACTIVITY_DT="20250626" AGENT_CD="HSH" SERVICE_CD="MTR" ORIG_COUNTRY_CD="USA" ORIG_CURRENCY_CD="USD" DEST_COUNTRY_CD="HND" DEST_CURRENCY_CD="HNL" PAYMENT_TYPE_CD="CSA" O_AGENT_CD="BTS">
+                            <DETAILS>
+                                <DETAIL MOVEMENT_TYPE_CODE="PAYI" CONFIRMATION_NM="79901025626" AGENT_ORDER_NM="79901025626" ORIGIN_AM="200.0000" DESTINATION_AM="5104.6800" FEE_AM="0.0000" DISCOUNT_AM="0.0000" />
+                            </DETAILS>
+                        </DET_ACT>
+                    </DATA>
+                </RESPONSE>
+            </GetRPTResult>
+        </GetRPTResponse>
+    </soap:Body>
+</soap:Envelope>
