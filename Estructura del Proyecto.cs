d25697f68a8fcec27fed465fec361ab21a11fbe0
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-07 10:20:07 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,141 +1,53 @@
-     A                                      UNIQUE
-     A          R IBTSACTA
-     A            INOCONFIR     11          TEXT('NO.CONFIRMACION')
-     A                                      COLHDG(-
-     A                                      'NUMERO' -
-     A                                      'CONFIRMACION')
-     A            IDATRECI       8A         TEXT('FECHA.RECIBIDA')
-     A                                      COLHDG(-
-     A                                      'FECHA' -
-     A                                      'RECIBIDA')
-     A            IHORRECI       9A         TEXT('HORA RECIBIDA')
-     A                                      COLHDG(-
-     A                                      'HORA' -
-     A                                      'RECIBIDA')
-     A            IDATCONF       8A         TEXT('FECHA.CONFIRMO')
-     A                                      COLHDG(-
-     A                                      'FECHA' -
-     A                                      'CONFIRMO')
-     A            IHORCONF       9A         TEXT('HORA CONFIRMO')
-     A                                      COLHDG(-
-     A                                      'HORA' -
-     A                                      'CONFIRMO')
-     A            IDATVAL        8A         TEXT('FECHA.VALIDO')
-     A                                      COLHDG(-
-     A                                      'FECHA' -
-     A                                      'VALIDO')
-     A            IHORVAL        9A         TEXT('HORA VALIDO')
-     A                                      COLHDG(-
-     A                                      'HORA' -
-     A                                      'VALIDO')
-     A            IDATPAGO       8A         TEXT('FECHA.PAGO')
-     A                                      COLHDG(-
-     A                                      'FECHA' -
-     A                                      'PAGO')
-     A            IHORPAGO       9A         TEXT('HORA PAGO')
-     A                                      COLHDG(-
-     A                                      'HORA' -
-     A                                      'PAGO')
-     A            IDATACRE       8A         TEXT('FECHA.ACREDITO')
-     A                                      COLHDG(-
-     A                                      'FECHA' -
-     A                                      'ACREDITO')
-     A            IHORACRE       9A         TEXT('HORA ACREDITO')
-     A                                      COLHDG(-
-     A                                      'HORA' -
-     A                                      'ACREDITO')
-     A            IDATRECH       8A         TEXT('FECHA.RECHAZO')
-     A                                      COLHDG(-
-     A                                      'FECHA' -
-     A                                      'RECHAZO')
-     A            IHORRECH       9A         TEXT('HORA RECHAZO')
-     A                                      COLHDG(-
-     A                                      'HORA' -
-     A                                      'RECHAZO')
-     A            ITIPPAGO       3          TEXT('TIPO.PAGO')
-     A                                      COLHDG(-
-     A                                      'TIPO' -
-     A                                      'PAGO')
-     C* ==========NUEVOS CAMPOS======================
-     A            ISERVICD       3          TEXT('SERVICIOCD')
-     A                                      COLHDG(-
-     A                                      'SERVICIO')
-     A            IDESPAIS       3          TEXT('PAIS_DESTINO')
-     A                                      COLHDG(-
-     A                                      'PAIS' -
-     A                                      'DESTINO')
-     A            IDESMONE       3          TEXT('MONEDA_DESTINO')
-     A                                      COLHDG(-
-     A                                      'MANEDA' -
-     A                                      'DESTINO')
-     A            ISAGENCD      10          TEXT('S_AGENTE')
-     A                                      COLHDG(-
-     A                                      'S_AGENTE' -
-     A                                      'CD')
-     A            ISPAISCD       3          TEXT('S_PAISCD')
-     A                                      COLHDG(-
-     A                                      'S_PAIS' -
-     A                                      'CD')
-     A            ISTATECD       3          TEXT('S_ESTADO')
-     A                                      COLHDG(-
-     A                                      'S_ESTADO' -
-     A                                      'CD')
-     A            IRAGENCD      10          TEXT('R_AGENTE_CD')
-     A                                      COLHDG(-
-     A                                      'R AGENTE' -
-     A                                      'CD')
-     C* ===========================================
-     A            ITICUENTA      3          TEXT('TIPO.CUENTA')
-     A                                      COLHDG(-
-     A                                      'TIPO' -
-     A                                      'CUENTA')
-     A            INOCUENTA     15          TEXT('NO.CUENTA')
-     A                                      COLHDG(-
-     A                                      'NUMERO' -
-     A                                      'CUENTA')
-     A            INUMREFER     20          TEXT('NO.REFERE')
-     A                                      COLHDG(-
-     A                                      'NUMERO' -
-     A                                      'REFERENCIA')
-     A            ISTSREM        3A         TEXT('ESTATUS DE REMESAS')
-     A            ISTSPRO       10A         TEXT('ESTATUS DE PROCESO')
-     A            IERR           4A         TEXT('MENSAJE')
-     A            IERRDSC       70A         TEXT('DESCRI.MENSAJE')
-     A            IDSCRECH      70A         TEXT('DESCRI.RECHAZO')
-     A* DATOS RECIBIDOS DE RESPUESTA QRY
-     A            ACODPAIS       3
-     A            ACODMONED      3
-     A            AMTOENVIA     20
-     A            AMTOCALCU     20
-     A            AFACTCAMB     21
-     A* remitente
-     A            BPRIMNAME     40
-     A            BSECUNAME     40
-     A            BAPELLIDO     40
-     A            BSEGUAPE      40
-     A            BDIRECCIO     65
-     A            BCIUDAD       40
-     A            BESTADO        3
-     A            BPAIS          3
-     A            BCODPOST      10
-     A            BTELEFONO     15
-     A* beneficiario
-     A            CPRIMNAME     40
-     A            CSECUNAME     40
-     A            CAPELLIDO     40
-     A            CSEGUAPE      40
-     A            CDIRECCIO     65
-     A            CCIUDAD       40
-     A            CESTADO        3
-     A            CPAIS          3
-     A            CCODPOST      10
-     A            CTELEFONO     15
-     A*    IDENTIDAD DE LA PERSONA QUE SE LE PAGO
-     A            DTIDENT       20
-     A*    DATOS DE REFERENCIA
-     A            ESALEDT        8
-     A            EMONREFER      3
-     A            ETASAREFE     21
-     A            EMTOREF       20
-     A
-     A          K INOCONFIR
+private async Task InsertarEnIbtSactaAsync(
+    IDatabaseConnection _connection,
+    IHttpContextAccessor _contextAccessor,
+    DepositsResponse response)
+{
+    if (response?.Deposits == null || response.Deposits.Count == 0)
+        return;
+
+    foreach (var deposit in response.Deposits)
+    {
+        if (deposit?.Data == null) continue;
+
+        var data = deposit.Data;
+        FieldsQuery param = new();
+
+        string insertSql = @"
+            INSERT INTO BCAH96DTA.IBTSACTA (
+                INOCONFIR, ISERVICD, IDESPAIS, IDESMONE,
+                ISAGENCD, ISPAISCD, ISTATECD, IRAGENCD,
+                ITICUENTA, INOCUENTA,
+                ACODPAIS, ACODMONED, AMTOENVIA, AMTOCALCU, AFACTCAMB,
+                ESALEDT, EMONREFER, ETASAREFE, EMTOREF
+            ) VALUES (
+                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+            )";
+
+        using var command = _connection.GetDbCommand(_contextAccessor.HttpContext!);
+        command.CommandText = insertSql;
+        command.CommandType = CommandType.Text;
+
+        param.AddOleDbParameter(command, "INOCONFIR", OleDbType.Char, data.ConfirmationNumber);
+        param.AddOleDbParameter(command, "ISERVICD", OleDbType.Char, data.ServiceCode);
+        param.AddOleDbParameter(command, "IDESPAIS", OleDbType.Char, data.DestinationCountryCode);
+        param.AddOleDbParameter(command, "IDESMONE", OleDbType.Char, data.DestinationCurrencyCode);
+        param.AddOleDbParameter(command, "ISAGENCD", OleDbType.Char, data.SenderAgentCode);
+        param.AddOleDbParameter(command, "ISPAISCD", OleDbType.Char, data.SenderCountryCode);
+        param.AddOleDbParameter(command, "ISTATECD", OleDbType.Char, data.SenderStateCode);
+        param.AddOleDbParameter(command, "IRAGENCD", OleDbType.Char, data.RecipientAgentCode);
+        param.AddOleDbParameter(command, "ITICUENTA", OleDbType.Char, data.RecipientAccountTypeCode);
+        param.AddOleDbParameter(command, "INOCUENTA", OleDbType.Char, data.RecipientAccountNumber);
+        param.AddOleDbParameter(command, "ACODPAIS", OleDbType.Char, data.OriginCountryCode);
+        param.AddOleDbParameter(command, "ACODMONED", OleDbType.Char, data.OriginCurrencyCode);
+        param.AddOleDbParameter(command, "AMTOENVIA", OleDbType.Char, data.OriginAmount);
+        param.AddOleDbParameter(command, "AMTOCALCU", OleDbType.Char, data.DestinationAmount);
+        param.AddOleDbParameter(command, "AFACTCAMB", OleDbType.Char, data.ExchangeRateFx);
+        param.AddOleDbParameter(command, "ESALEDT", OleDbType.Char, data.SaleDate);
+        param.AddOleDbParameter(command, "EMONREFER", OleDbType.Char, data.MarketRefCurrencyCode);
+        param.AddOleDbParameter(command, "ETASAREFE", OleDbType.Char, data.MarketRefCurrencyFx);
+        param.AddOleDbParameter(command, "EMTOREF", OleDbType.Char, data.MarketRefCurrencyAmount);
+
+        await command.ExecuteNonQueryAsync();
+    }
+}
