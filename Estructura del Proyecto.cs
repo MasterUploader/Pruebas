3baa97f855df7fe7f6d32d3ce588a8568de4ec51
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-08 09:00:19 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,4 +1,18 @@
-%Subst(%Char(%timestamp():*iso):12:2) +
-                              %Subst(%Char(%timestamp():*iso):15:2) +
-                              %Subst(%Char(%timestamp():*iso):18:2) +
-                              %Subst(%Char(%timestamp():*iso):21:3);
+Revisa este codigo es para el campo ITIPPAGO
+
+
+if        pos_fina = 0
+     c                   eval      pos_fina = 1
+     c                   endif
+     c                   clear                   pos
+ ****c                   EVAL      Pos    = %Scan('<PAYMENT_TYPE_CD>': MSG:
+     c                             pos_fina)
+     c                   if        pos > 0
+ ****c                   eval      pos  += 17
+     c                   eval      pos_ini = pos
+ ****c                   EVAL      Pos    = %Scan('</PAYMENT_TYPE_CD>' :MSG:
+     c                             pos_fina)
+     c                   eval      pos_fina = pos
+     c                   eval      lent    = pos_fina - pos_ini
+     c                   eval      @ITIPPAGO  = %trim(%subst(msg:pos_ini: lent))
+     c                   endif
