d01a9a0e465660b5def00854e7ff17eb03d3ca24
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-09 14:01:02 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,68 +1,94 @@
-using QueryBuilder.Services;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+Seria de convertir este codigo para que use la libreria:
 
-namespace QueryBuilder.DbContextSupport;
 
-/// <summary>
-/// Adaptador que combina la generación de consultas con su ejecución a través de Entity Framework.
-/// </summary>
-public class DbContextQueryBuilderAdapter
+foreach (var deposit in response.Deposits)
 {
-    private readonly SqlQueryService _queryService;
-    private readonly DbContextQueryExecutor _executor;
+    if (deposit?.Data == null) continue;
 
-    /// <summary>
-    /// Inicializa una nueva instancia del adaptador para DbContext.
-    /// </summary>
-    /// <param name="queryService">Servicio que construye sentencias SQL.</param>
-    /// <param name="executor">Ejecutor que utiliza DbContext para ejecutar las consultas.</param>
-    public DbContextQueryBuilderAdapter(SqlQueryService queryService, DbContextQueryExecutor executor)
-    {
-        _queryService = queryService;
-        _executor = executor;
-    }
+    // Estatus de proceso basado en opcode
+    string statusProceso = response.OpCode == "1308" ? "RECIBIDA" : "RECH-DENEG";
 
-    /// <summary>
-    /// Genera y ejecuta una consulta SELECT basada en un modelo definido por el usuario.
-    /// </summary>
-    /// <typeparam name="TModel">Modelo que representa la tabla base de datos.</typeparam>
-    /// <typeparam name="TResult">Tipo esperado del resultado (entidad o DTO).</typeparam>
-    /// <param name="filter">Filtro expresado como predicado.</param>
-    /// <returns>Lista de resultados obtenidos desde la base de datos.</returns>
-    public async Task<List<TResult>> SelectAsync<TModel, TResult>(Func<TModel, bool> filter)
-        where TModel : class, new()
-        where TResult : class
-    {
-        var sql = _queryService.BuildSelectQuery<TModel>(filter);
-        return await _executor.ExecuteQueryAsync<TResult>(sql);
-    }
+    var d = deposit.Data;
+    FieldsQuery param = new();
 
-    /// <summary>
-    /// Ejecuta una consulta de actualización generada dinámicamente para el modelo proporcionado.
-    /// </summary>
-    /// <typeparam name="TModel">Modelo fuente.</typeparam>
-    /// <param name="updateValues">Objeto con los valores a actualizar.</param>
-    /// <param name="filter">Condición de filtro.</param>
-    /// <returns>Cantidad de filas afectadas.</returns>
-    public async Task<int> UpdateAsync<TModel>(object updateValues, Func<TModel, bool> filter)
-        where TModel : class, new()
-    {
-        var sql = _queryService.BuildUpdateQuery<TModel>((TModel)updateValues, filter);
-        return await _executor.ExecuteCommandAsync(sql);
-    }
+    string insertSql = @"
+    INSERT INTO BCAH96DTA.BTSACTA (
+        INOCONFIR, IDATRECI, IHORRECI, IDATCONF, IHORCONF, IDATVAL, IHORVAL, IDATPAGO, IHORPAGO,
+        IDATACRE, IHORACRE, IDATRECH, IHORRECH, ITIPPAGO, ISERVICD, IDESPAIS, IDESMONE, ISAGENCD,
+        ISPAISCD, ISTATECD, IRAGENCD, ITICUENTA, INOCUENTA, INUMREFER, ISTSREM, ISTSPRO, IERR,
+        IERRDSC, IDSCRECH, ACODPAIS, ACODMONED, AMTOENVIA, AMTOCALCU, AFACTCAMB,
+        BPRIMNAME, BSECUNAME, BAPELLIDO, BSEGUAPE, BDIRECCIO, BCIUDAD, BESTADO, BPAIS,
+        BCODPOST, BTELEFONO, CPRIMNAME, CSECUNAME, CAPELLIDO, CSEGUAPE, CDIRECCIO,
+        CCIUDAD, CESTADO, CPAIS, CCODPOST, CTELEFONO, DTIDENT, ESALEDT, EMONREFER,
+        ETASAREFE, EMTOREF
+    ) VALUES (
+        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+    )";
 
-    /// <summary>
-    /// Ejecuta una inserción de datos basada en el modelo proporcionado.
-    /// </summary>
-    /// <typeparam name="TModel">Modelo base.</typeparam>
-    /// <param name="insertValues">Objeto con los valores a insertar.</param>
-    /// <returns>Cantidad de filas insertadas.</returns>
-    public async Task<int> InsertAsync<TModel>(object insertValues)
-        where TModel : class, new()
-    {
-        var sql = _queryService.BuildInsertQuery<TModel>((TModel)insertValues);
-        return await _executor.ExecuteCommandAsync(sql);
-    }
+    using var command = _databaseConnection.GetDbCommand(_httpContextAccessor.HttpContext!);
+    command.CommandText = insertSql;
+    command.CommandType = CommandType.Text;
+
+    // 🔹 Valores conocidos del XML
+    param.AddOleDbParameter(command, "INOCONFIR", OleDbType.Char, d.ConfirmationNumber);
+    param.AddOleDbParameter(command, "IDATRECI", OleDbType.Char, DateTime.Now.ToString("yyyyMMdd"));
+    param.AddOleDbParameter(command, "IHORRECI", OleDbType.Char, DateTime.Now.ToString("HHmmssfff"));
+    param.AddOleDbParameter(command, "IDATCONF", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "IHORCONF", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "IDATVAL", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "IHORVAL", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "IDATPAGO", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "IHORPAGO", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "IDATACRE", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "IHORACRE", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "IDATRECH", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "IHORRECH", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "ITIPPAGO", OleDbType.Char, d.PaymentTypeCode);
+    param.AddOleDbParameter(command, "ISERVICD", OleDbType.Char, d.ServiceCode);
+    param.AddOleDbParameter(command, "IDESPAIS", OleDbType.Char, d.DestinationCountryCode);
+    param.AddOleDbParameter(command, "IDESMONE", OleDbType.Char, d.DestinationCurrencyCode);
+    param.AddOleDbParameter(command, "ISAGENCD", OleDbType.Char, d.SenderAgentCode);
+    param.AddOleDbParameter(command, "ISPAISCD", OleDbType.Char, d.SenderCountryCode);
+    param.AddOleDbParameter(command, "ISTATECD", OleDbType.Char, d.SenderStateCode);
+    param.AddOleDbParameter(command, "IRAGENCD", OleDbType.Char, d.RecipientAgentCode);
+    param.AddOleDbParameter(command, "ITICUENTA", OleDbType.Char, d.RecipientAccountTypeCode);
+    param.AddOleDbParameter(command, "INOCUENTA", OleDbType.Char, d.RecipientAccountNumber);
+    param.AddOleDbParameter(command, "INUMREFER", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "ISTSREM", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "ISTSPRO", OleDbType.Char, statusProceso);
+    param.AddOleDbParameter(command, "IERR", OleDbType.Char, response.OpCode ?? " ");
+    param.AddOleDbParameter(command, "IERRDSC", OleDbType.Char, response.ProcessMsg ?? " ");
+    param.AddOleDbParameter(command, "IDSCRECH", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "ACODPAIS", OleDbType.Char, d.OriginCountryCode);
+    param.AddOleDbParameter(command, "ACODMONED", OleDbType.Char, d.OriginCurrencyCode);
+    param.AddOleDbParameter(command, "AMTOENVIA", OleDbType.Char, d.OriginAmount);
+    param.AddOleDbParameter(command, "AMTOCALCU", OleDbType.Char, d.DestinationAmount);
+    param.AddOleDbParameter(command, "AFACTCAMB", OleDbType.Char, d.ExchangeRateFx);
+    param.AddOleDbParameter(command, "BPRIMNAME", OleDbType.Char, d.Sender.FirstName);
+    param.AddOleDbParameter(command, "BSECUNAME", OleDbType.Char, d.Sender.MiddleName);
+    param.AddOleDbParameter(command, "BAPELLIDO", OleDbType.Char, d.Sender.LastName);
+    param.AddOleDbParameter(command, "BSEGUAPE", OleDbType.Char, d.Sender.MotherMaidenName);
+    param.AddOleDbParameter(command, "BDIRECCIO", OleDbType.Char, d.Sender.Address.AddressLine);
+    param.AddOleDbParameter(command, "BCIUDAD", OleDbType.Char, d.Sender.Address.City);
+    param.AddOleDbParameter(command, "BESTADO", OleDbType.Char, d.Sender.Address.StateCode);
+    param.AddOleDbParameter(command, "BPAIS", OleDbType.Char, d.Sender.Address.CountryCode);
+    param.AddOleDbParameter(command, "BCODPOST", OleDbType.Char, d.Sender.Address.ZipCode);
+    param.AddOleDbParameter(command, "BTELEFONO", OleDbType.Char, d.Sender.Address.Phone);
+    param.AddOleDbParameter(command, "CPRIMNAME", OleDbType.Char, d.Recipient.FirstName);
+    param.AddOleDbParameter(command, "CSECUNAME", OleDbType.Char, d.Recipient.MiddleName);
+    param.AddOleDbParameter(command, "CAPELLIDO", OleDbType.Char, d.Recipient.LastName);
+    param.AddOleDbParameter(command, "CSEGUAPE", OleDbType.Char, d.Recipient.MotherMaidenName);
+    param.AddOleDbParameter(command, "CDIRECCIO", OleDbType.Char, d.Recipient.Address.AddressLine);
+    param.AddOleDbParameter(command, "CCIUDAD", OleDbType.Char, d.Recipient.Address.City);
+    param.AddOleDbParameter(command, "CESTADO", OleDbType.Char, d.Recipient.Address.StateCode);
+    param.AddOleDbParameter(command, "CPAIS", OleDbType.Char, d.Recipient.Address.CountryCode);
+    param.AddOleDbParameter(command, "CCODPOST", OleDbType.Char, d.Recipient.Address.ZipCode);
+    param.AddOleDbParameter(command, "CTELEFONO", OleDbType.Char, d.Recipient.Address.Phone);
+    param.AddOleDbParameter(command, "DTIDENT", OleDbType.Char, " ");
+    param.AddOleDbParameter(command, "ESALEDT", OleDbType.Char, d.SaleDate);
+    param.AddOleDbParameter(command, "EMONREFER", OleDbType.Char, d.MarketRefCurrencyCode);
+    param.AddOleDbParameter(command, "ETASAREFE", OleDbType.Char, d.MarketRefCurrencyFx);
+    param.AddOleDbParameter(command, "EMTOREF", OleDbType.Char, d.MarketRefCurrencyAmount);
+
+    await command.ExecuteNonQueryAsync();
 }
