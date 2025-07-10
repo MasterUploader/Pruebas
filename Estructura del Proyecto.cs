2055382aead18034e72f80dc2d8372a63cb48cf8
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-10 13:53:32 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,56 +1,24 @@
-using QueryBuilder.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-
-namespace QueryBuilder.Helpers;
-
-public static class SqlMetadataHelper
+/// <summary>
+/// Obtiene un diccionario de pares columna/valor para una instancia de modelo.
+/// </summary>
+public static Dictionary<string, object> GetColumnValuePairs<T>(T instance)
 {
-    /// <summary>
-    /// Obtiene el nombre completo de la tabla para el tipo de modelo especificado,
-    /// utilizando el atributo [SqlTable] o el nombre de la clase si no está definido.
-    /// </summary>
-    public static string GetFullTableName<T>()
+    var result = new Dictionary<string, object>();
+    var props = typeof(T).GetProperties();
+
+    foreach (var prop in props)
     {
-        var type = typeof(T);
-        var tableAttr = type.GetCustomAttribute<SqlTableAttribute>();
-        return tableAttr != null
-            ? $"{tableAttr.Schema}.{tableAttr.TableName}"
-            : type.Name;
+        if (prop.GetCustomAttribute<SqlIgnoreAttribute>() != null)
+            continue;
+
+        var nameAttr = prop.GetCustomAttribute<SqlColumnNameAttribute>();
+        var columnName = nameAttr?.Name ?? prop.Name;
+        var value = prop.GetValue(instance);
+        result[columnName] = value ?? DBNull.Value;
     }
 
-    /// <summary>
-    /// Obtiene los nombres de columnas SQL que corresponden al modelo,
-    /// excluyendo las propiedades marcadas con [SqlIgnore].
-    /// </summary>
-    /// <typeparam name="T">Tipo del modelo.</typeparam>
-    /// <returns>Lista de nombres de columnas SQL.</returns>
-    public static List<string> GetSqlColumns<T>()
-    {
-        var type = typeof(T);
-        var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-        var columns = new List<string>();
-
-        foreach (var prop in properties)
-        {
-            // Ignorar si tiene [SqlIgnore]
-            if (prop.GetCustomAttribute<SqlIgnoreAttribute>() != null)
-                continue;
-
-            // Usar el nombre definido en [SqlColumnName] si existe
-            var nameAttr = prop.GetCustomAttribute<SqlColumnNameAttribute>();
-            if (nameAttr != null)
-            {
-                columns.Add(nameAttr.Name);
-                continue;
-            }
-
-            // Usar el nombre de la propiedad como nombre de columna por defecto
-            columns.Add(prop.Name);
-        }
-
-        return columns;
-    }
+    return result;
 }
+
+
+
