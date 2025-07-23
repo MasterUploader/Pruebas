abcc2713b99f0fdcf81dab9152efe6d0a6dc73c9
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-23 15:26:42 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Linq.Expressions;
 using QueryBuilder.Helpers;
-using QueryBuilder.Models;
+using QueryBuilder.Builders;
 
 namespace QueryBuilder.Translators;
 
@@ -11,18 +11,49 @@ namespace QueryBuilder.Translators;
 public static class LambdaWhereTranslator
 {
     /// <summary>
-    /// Traduce una expresión lambda a SQL y la agrega al contexto.
+    /// Traduce una expresión lambda a SQL y la agrega al builder.
     /// </summary>
     /// <typeparam name="T">Tipo de la entidad.</typeparam>
-    /// <param name="context">Contexto de traducción de consulta.</param>
+    /// <param name="builder">Instancia de SelectQueryBuilder.</param>
     /// <param name="expression">Expresión lambda booleana.</param>
-    public static void Translate<T>(QueryTranslationContext context, Expression<Func<T, bool>> expression)
+    public static void Translate<T>(SelectQueryBuilder builder, Expression<Func<T, bool>> expression)
     {
         string condition = ExpressionToSqlConverter.Convert(expression);
 
-        if (string.IsNullOrWhiteSpace(context.WhereClause))
-            context.WhereClause = condition;
+        if (string.IsNullOrWhiteSpace(builder.WhereClause))
+            builder.WhereClause = condition;
         else
-            context.WhereClause += $" AND {condition}";
+            builder.WhereClause += $" AND {condition}";
+    }
+}
+
+using System;
+using System.Linq.Expressions;
+using QueryBuilder.Translators;
+
+namespace QueryBuilder.Builders;
+
+/// <summary>
+/// Constructor de consultas SELECT SQL.
+/// </summary>
+public class SelectQueryBuilder
+{
+    /// <summary>
+    /// Cláusula WHERE acumulada.
+    /// </summary>
+    internal string? WhereClause { get; set; }
+
+    // ...otras propiedades internas como Tabla, Columnas, Orden, etc.
+
+    /// <summary>
+    /// Agrega una condición WHERE a la consulta.
+    /// </summary>
+    /// <typeparam name="T">Tipo de entidad (para acceso tipado).</typeparam>
+    /// <param name="expression">Expresión lambda booleana a traducir.</param>
+    /// <returns>Instancia del builder para encadenamiento fluido.</returns>
+    public SelectQueryBuilder Where<T>(Expression<Func<T, bool>> expression)
+    {
+        LambdaWhereTranslator.Translate(this, expression);
+        return this;
     }
 }
