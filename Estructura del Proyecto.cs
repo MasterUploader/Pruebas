f591676d4653f41df20f0841779df1eedff3c5b9
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-23 16:29:44 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -2,10 +2,19 @@ Con este codigo
 var query = QueryBuilder.Core.QueryBuilder
     .From("USUADMIN", "BCAH96DTA")
     .Select("TIPUSU", "ESTADO", "PASS")
-    .Where<USUADMIN>(x => x.TIPUSU == "A" || x.TIPUSU == "B")
-    .Where<USUADMIN>(x => x.ESTADO != null)
+    .Where<USUADMIN>(x => (x.TIPUSU == "A" || x.TIPUSU == "B") && x.ESTADO != null)
     .Build();
 
 Da este error:
 
 System.InvalidOperationException: 'variable 'x' of type 'CAUAdministracion.Models.USUADMIN' referenced from scope '', but it is not defined'
+
+    Y con este codigo
+
+    var query = QueryBuilder.Core.QueryBuilder
+    .From("USUADMIN", "BCAH96DTA")
+    .Select("TIPUSU", "ESTADO", "PASS")
+    .Where<USUADMIN>(c => c.USUARIO == username)
+    .Build();
+
+Genera así SELECT TIPUSU, ESTADO, PASS FROM BCAH96DTA.USUADMIN, en el cual hace falta el where, no se esta generando correctamente, hay que revisar lo que genera el where dentro de la libreria
