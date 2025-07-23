c3079f128e23f5eafa557c4afd80bb435b102d69
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-23 09:53:45 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,26 +1,5 @@
-El codigo quedaria así, no puedo enviarle el query.sql dentro del GetDbCommand, porque este recibe un context
+El select lo esta generando así:
+SELECT TIPUSU, ESTADO, PASS FROM BCAH96DTA.USUADMIN WHERE (USUARIO = username)
 
-var query = QueryBuilder.Core.QueryBuilder
-    .From("USUADMIN", "BCAH96DTA")
-    .Select("TIPUSU", "ESTADO", "PASS")
-    .Where<USUADMIN>(c => c.USUARIO == username)
-    .Build();
-
-using var command = _as400.GetDbCommand(_httpContextAccessor.HttpContext!);
-//Validamos si la coneción se establecio
-if (command.Connection.State == System.Data.ConnectionState.Closed)
-    command.Connection.Open();
-
-
-Usuario datos = null;
-
-using var reader = command.ExecuteReader();
-if (reader.Read())
-{
-    datos = new Usuario
-    {
-        TIPUSU = reader["TIPUSU"].ToString(),
-        ESTADO = reader["ESTADO"].ToString(),
-        PASS = reader["PASS"].ToString()
-    };
-}
+Cuando deberia ser así
+SELECT TIPUSU, ESTADO, PASS FROM BCAH96DTA.USUADMIN WHERE USUARIO = 'admsoporte'
