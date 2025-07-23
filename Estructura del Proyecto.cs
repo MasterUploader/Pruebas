0026dcf583a2e5ab760f434fb63b695a10daef57
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-23 15:38:19 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,6 +1,11 @@
+Este codigo 
 var query = QueryBuilder.Core.QueryBuilder
     .From("USUADMIN", "BCAH96DTA")
     .Select("TIPUSU", "ESTADO", "PASS")
-    .Where<Usuario>(x => x.TIPUSU == "A" || x.TIPUSU == "B")
-    .Where<Usuario>(x => x.ESTADO != null)
+    .Where<USUADMIN>(x => x.TIPUSU == "A" || x.TIPUSU == "B")
+    .Where<USUADMIN>(x => x.ESTADO != null)
     .Build();
+
+Solo me genero esto
+
+SELECT TIPUSU, ESTADO, PASS FROM BCAH96DTA.USUADMIN WHERE (ESTADO <> NULL)
