4fcd3266f9ad7470c8c69f1db75514af87731f0a
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-24 16:56:58 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,8 +1,8 @@
-Estoy empezando a usar la libreria, pero me genero esta sentencia
+Necesito este SQL convertido a usar el paquete QueryBuilder
 
-"SELECT TIPUSU, ESTADO, PASS FROM BCAH96DTA.USUADMINUSUADMIN WHERE (USUARIO = 'admsoporte')"
-
-Cuando deberia ser
-"SELECT TIPUSU, ESTADO, PASS FROM BCAH96DTA.USUADMIN WHERE (USUARIO = 'admsoporte')"
-
-Donde podria estar el problema, o requieres que te pase algun codigo?
+command.CommandText = $@"
+        UPDATE BCAH96DTA.RSAGE01
+        SET NOMAGE = '{agencia.NomAge}', ZONA = {agencia.Zona},
+            MARQUESINA = '{agencia.Marquesina}', RSTBRANCH = '{agencia.RstBranch}',
+            NOMBD = '{agencia.NomBD}', NOMSER = '{agencia.NomSer}', IPSER = '{agencia.IpSer}'
+        WHERE CODCCO = {agencia.Codcco}";
