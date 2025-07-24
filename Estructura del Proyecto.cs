294cade8ab20e1fa50375c39b42d1598e9a861e7
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-24 17:01:44 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,8 +1,14 @@
-Necesito este SQL convertido a usar el paquete QueryBuilder
+var query = new UpdateQueryBuilder("RSAGE01", "BCAH96DTA")
+    .Set("NOMAGE", agencia.NomAge)
+    .Set("ZONA", agencia.Zona)
+    .Set("MARQUESINA", agencia.Marquesina)
+    .Set("RSTBRANCH", agencia.RstBranch)
+    .Set("NOMBD", agencia.NomBD)
+    .Set("NOMSER", agencia.NomSer)
+    .Set("IPSER", agencia.IpSer)
+    .Where("CODCCO", agencia.Codcco)
+    .Build();
 
-command.CommandText = $@"
-        UPDATE BCAH96DTA.RSAGE01
-        SET NOMAGE = '{agencia.NomAge}', ZONA = {agencia.Zona},
-            MARQUESINA = '{agencia.Marquesina}', RSTBRANCH = '{agencia.RstBranch}',
-            NOMBD = '{agencia.NomBD}', NOMSER = '{agencia.NomSer}', IPSER = '{agencia.IpSer}'
-        WHERE CODCCO = {agencia.Codcco}";
+using var command = _as400.GetDbCommand(_httpContextAccessor.HttpContext!);
+command.CommandText = query.Sql;
+command.ExecuteNonQuery();
