d62701025d41605c8402241735d36bb01e57a9b9
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-25 10:04:49 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,23 +1,36 @@
-Como quedaria este insert:
+/// <summary>
+/// Inserta una nueva agencia en la base de datos.
+/// </summary>
+/// <param name="agencia">Objeto que contiene los datos de la agencia a insertar.</param>
+/// <returns>True si la inserción fue exitosa; false en caso contrario.</returns>
+public bool InsertarAgencia(AgenciaModel agencia)
+{
+    try
+    {
+        _as400.Open();
 
- public bool InsertarAgencia(AgenciaModel agencia)
- {
-     try
-     {
-         _as400.Open();
-         using var command = _as400.GetDbCommand(_httpContextAccessor.HttpContext!);
+        // Generar la sentencia SQL con QueryBuilder
+        var query = new InsertQueryBuilder("RSAGE01", "BCAH96DTA")
+            .Values(
+                ("CODCCO", agencia.Codcco),
+                ("NOMAGE", agencia.NomAge),
+                ("ZONA", agencia.Zona),
+                ("MARQUESINA", agencia.Marquesina),
+                ("RSTBRANCH", agencia.RstBranch),
+                ("NOMBD", agencia.NomBD),
+                ("NOMSER", agencia.NomSer),
+                ("IPSER", agencia.IpSer)
+            )
+            .Build();
 
-         command.CommandText = $@"
-                 INSERT INTO BCAH96DTA.RSAGE01 
-                 (CODCCO, NOMAGE, ZONA, MARQUESINA, RSTBRANCH, NOMBD, NOMSER, IPSER)
-                 VALUES 
-                 ({agencia.Codcco}, '{agencia.NomAge}', {agencia.Zona}, '{agencia.Marquesina}', 
-                  '{agencia.RstBranch}', '{agencia.NomBD}', '{agencia.NomSer}', '{agencia.IpSer}')";
+        // Crear y ejecutar el comando
+        using var command = _as400.GetDbCommand(_httpContextAccessor.HttpContext!);
+        command.CommandText = query.Sql;
 
-         return command.ExecuteNonQuery() > 0;
-     }
-     finally
-     {
-         _as400.Close();
-     }
- }
+        return command.ExecuteNonQuery() > 0;
+    }
+    finally
+    {
+        _as400.Close();
+    }
+}
