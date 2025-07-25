837f0727c3426b480a70130cfde62f75f42bab27
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-25 14:28:12 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,10 +1,4 @@
-/// <summary>
-/// Crea una nueva instancia de <see cref="CaseWhenBuilder"/> y agrega un <c>WHEN</c> inicial basado en una expresión lambda tipada.
-/// </summary>
-/// <typeparam name="T">Tipo de entidad sobre la cual se basa la condición.</typeparam>
-/// <param name="condition">Expresión booleana que representa la condición para el <c>WHEN</c> (por ejemplo: <c>x => x.TIPO == "A"</c>).</param>
-/// <returns>Instancia de <see cref="CaseWhenBuilder"/> con el <c>WHEN</c> inicial agregado.</returns>
-public static CaseWhenBuilder Start<T>(Expression<Func<T, bool>> condition)
-{
-    return new CaseWhenBuilder().When(condition);
-}
+Excelente hasta el momento, me genero el sql así SELECT CODCCO, NOMAGE FROM BCAH96DTA.RSAGE01 WHERE (MARQUESINA = 'SI') ORDER BY CASE WHEN (CODCCO = 0) THEN 0 ELSE 1 END ASC, NOMAGE ASC,
+
+Es posible que los dos ASC sean opcionales y que si no los coloco quede asi:
+SELECT CODCCO, NOMAGE FROM BCAH96DTA.RSAGE01 WHERE (MARQUESINA = 'SI') ORDER BY CASE WHEN (CODCCO = 0) THEN 0 ELSE 1 END, NOMAGE
