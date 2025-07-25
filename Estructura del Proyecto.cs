784991724a0645854caa324d93137607cc9e4c3c
MasterUploader <MasterUploader@users.noreply.github.com>
2025-07-25 14:04:25 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,4 +1,12 @@
-string endpoint = context.Request.Path.Value?
-    .Trim('/')
-    .Split('/')
-    .LastOrDefault() ?? "UnknownEndpoint";
+var query = new SelectQueryBuilder("RSAGE01", "BCAH96DTA")
+    .Select("CODCCO", "NOMAGE")
+    .Where<RSAGE01>(x => x.MARQUESINA == "SI")
+    .OrderByCase(
+        CaseWhenBuilder
+            .When<RSAGE01>(x => x.CODCCO == "0")
+            .Then(0)
+            .Else(1),
+        null // sin alias porque es para ORDER BY
+    )
+    .OrderBy("NOMAGE")
+    .Build();
