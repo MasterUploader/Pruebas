3469690535cc6cb80cf06f3d508ce1cadf15cdaa
MasterUploader <MasterUploader@users.noreply.github.com>
2025-08-12 14:53:50 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,31 +1,55 @@
-// Helpers OUT
-public ProgramCallBuilder OutString(string name, int size, object? initialValue = null)
-    => Out(name: name, dbType: DbType.String, size: size, precision: null, scale: null, initialValue: initialValue);
+Convierte este codigo a usar la libreria
 
-public ProgramCallBuilder OutChar(string name, int size, object? initialValue = null)
-    => Out(name: name, dbType: DbType.AnsiStringFixedLength, size: size, precision: null, scale: null, initialValue: initialValue);
+string callProgramCL = "{CALL BCAH96.EVE03CTACL(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}";
+using (OleDbCommand command = new(callProgramCL, conection.Connect.OleDbConnection))
+{
 
-public ProgramCallBuilder OutDecimal(string name, byte precision, byte scale, object? initialValue = null)
-    => Out(name: name, dbType: DbType.Decimal, size: null, precision: precision, scale: scale, initialValue: initialValue);
 
-public ProgramCallBuilder OutInt32(string name, int? initialValue = null)
-    => Out(name: name, dbType: DbType.Int32, size: null, precision: null, scale: null, initialValue: initialValue);
+    //Encabezado
+    command.Parameters.AddWithValue("v1", responseModel.HRequestId.Trim());
+    command.Parameters.AddWithValue("v2", responseModel.HChannel.Trim());
+    command.Parameters.AddWithValue("v3", responseModel.HTerminal.Trim());
+    command.Parameters.AddWithValue("v4", responseModel.HOrganization.Trim());
+    command.Parameters.AddWithValue("v5", responseModel.HUserId.Trim());
+    command.Parameters.AddWithValue("v6", responseModel.HProvider.Trim());
+    command.Parameters.AddWithValue("v7", responseModel.HSessionId.Trim());
+    command.Parameters.AddWithValue("v8", responseModel.HClientIp.Trim());
+    command.Parameters.AddWithValue("v9", responseModel.HTimestamp.Trim());
 
-public ProgramCallBuilder OutDateTime(string name, DateTime? initialValue = null)
-    => Out(name: name, dbType: DbType.DateTime, size: null, precision: null, scale: null, initialValue: initialValue);
+    //Parámetros de entrada
+    command.Parameters.AddWithValue("v10", requestBodyCreTarjetaAdicional.tipoMensaje);
+    command.Parameters.AddWithValue("v11", requestBodyCreTarjetaAdicional.codigoEmisor);
+    command.Parameters.AddWithValue("v12", requestBodyCreTarjetaAdicional.numeroCuenta);
+    command.Parameters.AddWithValue("v13", requestBodyCreTarjetaAdicional.localidadTramita);
+    command.Parameters.AddWithValue("v14", requestBodyCreTarjetaAdicional.localidadRetira);
+    command.Parameters.AddWithValue("v15", requestBodyCreTarjetaAdicional.identificacionAdicional);
+    command.Parameters.AddWithValue("v16", requestBodyCreTarjetaAdicional.tipoTarjeta);
+    command.Parameters.AddWithValue("v17", requestBodyCreTarjetaAdicional.disenoTarjeta);
+    command.Parameters.AddWithValue("v18", requestBodyCreTarjetaAdicional.usuarioSiscard);
+    command.Parameters.AddWithValue("v19", requestBodyCreTarjetaAdicional.version);
 
-// Helpers IN
-public ProgramCallBuilder InString(string? value, int? size = null)
-    => In(value, dbType: DbType.String, size: size);
 
-public ProgramCallBuilder InChar(string? value, int size)
-    => In(value, dbType: DbType.AnsiStringFixedLength, size: size);
+    command.Parameters.AddWithValue("v20", header.ReponseId);
+    command.Parameters.AddWithValue("v21", header.Timestamp);
+    command.Parameters.AddWithValue("v22", appTimer.Elapsed);
+    command.Parameters.AddWithValue("v23", StatusCode);
+    command.Parameters.AddWithValue("v24", Message);
 
-public ProgramCallBuilder InDecimal(decimal? value, byte? precision = null, byte? scale = null)
-    => In(value, dbType: DbType.Decimal, precision: precision, scale: scale);
+    //Parámetros de salida            
+    command.Parameters.AddWithValue("v25", responseCreTarjetaAdicional.status);
+    command.Parameters.AddWithValue("v26", responseCreTarjetaAdicional.statusCode);
+    command.Parameters.AddWithValue("v27", responseCreTarjetaAdicional.statusMessage);
+    command.Parameters.AddWithValue("v28", responseCreTarjetaAdicional.numeroTarjetaAdicional);
 
-public ProgramCallBuilder InInt32(int? value)
-    => In(value, dbType: DbType.Int32);
 
-public ProgramCallBuilder InDateTime(DateTime? value)
-    => In(value, dbType: DbType.DateTime);
+    command.Parameters.AddWithValue("v29", "00");
+    command.Parameters.AddWithValue("v30", "Proceso realizado exitosamente");
+
+
+    //Ejecuta el procedimiento y verifica si se insertaron los datos correctamente. 
+    int respuesta = command.ExecuteNonQuery();
+    if (respuesta > 0)
+
+        return true;
+
+    return false;
