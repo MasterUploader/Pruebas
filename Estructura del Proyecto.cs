39ddd3c3e91db0bb04f06c08837c9f8f87767fb1
MasterUploader <MasterUploader@users.noreply.github.com>
2025-08-14 11:32:01 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,1 +1,26 @@
-npm install --save-dev @types/jasmine@5.1.8 @types/lodash@4.17.20 jasmine-core@5.2.2 typescript@5.8.3
+Entregame el codigo con los cambios
+
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { AuthInterceptor } from './core/services/auth.interceptor';
+import { AppRoutingModule, routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
+//Nuevo
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes, withHashLocation()),
+  provideClientHydration(),
+  provideAnimationsAsync(),
+  provideHttpClient(withFetch()),
+  importProvidersFrom(HttpClientModule, AppRoutingModule),
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true,
+  },
+  { provide: LocationStrategy, useClass: HashLocationStrategy },]
+};
