3b1b8e2fbdc42c51e6d721af950da7b3f7fd7f04
MasterUploader <MasterUploader@users.noreply.github.com>
2025-08-20 14:23:36 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,23 +1,14 @@
-Revisa si el guard esta bien y si asegura que cuando la sesion venza se cierre el login
-
-import { CanActivateFn, Router, RouterModule } from '@angular/router';
-import { NgModule , inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { routes } from '../../app.routes';
 
-export const authGuard: CanActivateFn = () => {
-  const authService = inject(AuthService);
+export const authGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
   const router = inject(Router);
-  if(authService.sessionIsActive()){
-    return true;
-  }else{
-    router.navigateByUrl('/login');
-    return false;
-  }
-};
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule{}
+  // Si está activa la sesión, puede pasar
+  if (auth.sessionIsActive()) return true;
+
+  // Mejor devolver UrlTree (no navegar imperativamente)
+  return router.createUrlTree(['/login']);
+};
