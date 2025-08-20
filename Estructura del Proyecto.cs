21fb85eacd347ddad73508d65e1d3253ba5f8314
MasterUploader <MasterUploader@users.noreply.github.com>
2025-08-20 10:47:42 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,6 +1,5 @@
-Tengo este codigo en un sitio en angular 20
-
-  <div class="overlay" *ngIf="isLoading" role="alert" aria-live="assertive">
+@if (isLoading) {
+  <div class="overlay" role="alert" aria-live="assertive">
     <div class="overlay-content" role="dialog" aria-label="Autenticando">
       <mat-progress-spinner
         mode="indeterminate"
@@ -9,26 +8,14 @@ Tengo este codigo en un sitio en angular 20
       <div class="overlay-text">Autenticando</div>
     </div>
   </div>
+      }
 
-
+@if (errorMessage) {
   <div
-          class="inline-error mat-mdc-form-field-error"
-          *ngIf="errorMessage"
-          role="alert"
-        >
-          <mat-icon aria-hidden="true">error</mat-icon>
-          <span>{{ errorMessage }}</span>
-        </div>
-
-Me dice que las lineas 
-
-<div class="overlay" *ngIf="isLoading" role="alert" aria-live="assertive">
-
-y 
-
-<div class="inline-error mat-mdc-form-field-error"
-          *ngIf="errorMessage"
-          role="alert"
-        >
-
-el *ngIf esta deprecado, como puedo corregir ese codigo en particular
+    class="inline-error mat-mdc-form-field-error"
+    role="alert"
+  >
+    <mat-icon aria-hidden="true">error</mat-icon>
+    <span>{{ errorMessage }}</span>
+  </div>
+  }
