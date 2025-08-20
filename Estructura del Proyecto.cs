16d02517a5358bda671c54401278b63a7576bbe2
MasterUploader <MasterUploader@users.noreply.github.com>
2025-08-20 10:54:40 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,3 +1,20 @@
-Por ejemplo tengo lineas como esta <mat-error *ngIf="loginForm.controls['userName'].hasError('required')">
+<mat-form-field appearance="fill" class="full-width">
+  <mat-label>Usuario</mat-label>
+  <input
+    matInput
+    formControlName="userName"
+    placeholder="Introduce tu usuario"
+    autocomplete="username"
+    required
+  />
 
-Me dice tambien que el *ngIf esta deprecado, como lo corrijo
+  @if (loginForm.controls['userName'].hasError('required')) {
+    <mat-error>El usuario es obligatorio</mat-error>
+  }
+  @else if (loginForm.controls['userName'].hasError('minlength')) {
+    <mat-error>El usuario debe tener al menos 3 caracteres</mat-error>
+  }
+  @else if (loginForm.controls['userName'].hasError('maxlength')) {
+    <mat-error>El usuario no puede superar los 20 caracteres</mat-error>
+  }
+</mat-form-field>
