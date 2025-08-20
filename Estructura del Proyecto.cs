ebb6465b4ebb002d176cea09d3d234956c271589
MasterUploader <MasterUploader@users.noreply.github.com>
2025-08-20 10:51:57 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,21 +1,3 @@
-@if (isLoading) {
-  <div class="overlay" role="alert" aria-live="assertive">
-    <div class="overlay-content" role="dialog" aria-label="Autenticando">
-      <mat-progress-spinner
-        mode="indeterminate"
-        diameter="48"
-      ></mat-progress-spinner>
-      <div class="overlay-text">Autenticando</div>
-    </div>
-  </div>
-      }
+Por ejemplo tengo lineas como esta <mat-error *ngIf="loginForm.controls['userName'].hasError('required')">
 
-@if (errorMessage) {
-  <div
-    class="inline-error mat-mdc-form-field-error"
-    role="alert"
-  >
-    <mat-icon aria-hidden="true">error</mat-icon>
-    <span>{{ errorMessage }}</span>
-  </div>
-  }
+Me dice tambien que el *ngIf esta deprecado, como lo corrijo
