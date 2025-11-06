db637b478901e530190dec8e2caeec857f7d9873
MasterUploader <MasterUploader@users.noreply.github.com>
2025-11-06 15:48:10 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,1 +1,18 @@
--NoProfile -ExecutionPolicy Bypass -File "C:\Ruta\ClearNuGetCaches_SAFE.ps1" -Apply -VerboseMode
+Necesito una tabla para AS400 que contenga los siguientes campos, todos son char y valida segun los tamaños que ya manejas.
+GUID
+FechaPosteo
+HoraPosteo
+numeroCuenta
+montoDebitado
+montoAcreditado
+codigoComercio
+nombreComercio
+terminal
+descripcion
+naturalezaContable
+numeroDeCorte
+idTransaccionUnico
+estado
+descripcionEstado
+CodigoError
+DescripcionError
