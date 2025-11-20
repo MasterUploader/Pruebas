f00a347fc24d2017aae30b5ca8626d71ba8154a7
MasterUploader <MasterUploader@users.noreply.github.com>
2025-11-20 11:18:22 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,28 +1,128 @@
+Así tengo el codigo:
+
 using Microsoft.Owin;
-using Microsoft.Owin.Cors;
 using Owin;
-using System.Threading.Tasks;
-using System.Web.Http;
-using System.Web.Cors;
 
-public class Startup
+[assembly: OwinStartup(typeof(Presentation.RestService.Startup))]
+
+namespace Presentation.RestService
 {
-    public void Configuration(IAppBuilder app)
+    public partial class Startup
     {
-        var policy = new CorsPolicy
+        public void Configuration(IAppBuilder app)
         {
-            AllowAnyHeader = true,
-            AllowAnyMethod = true,
-            SupportsCredentials = true
-        };
-        policy.Origins.Add("http://localhost:4200");
-        app.UseCors(new CorsOptions
-        {
-            PolicyProvider = new CorsPolicyProvider { Policy = policy }
-        });
-
-        var config = new HttpConfiguration();
-        WebApiConfig.Register(config);
-        app.UseWebApi(config);
+            ConfigureAuth(app);
+        }
     }
 }
+
+
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using Presentation.RestService.Models;
+using Presentation.RestService.Providers;
+using System;
+
+namespace Presentation.RestService
+{
+    public partial class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public static string PublicClientId { get; private set; }
+
+        // Para obtener más información para configurar la autenticación, visite http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure el contexto de base de datos y el administrador de usuarios para usar una única instancia por solicitud
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+            // Permitir que la aplicación use una cookie para almacenar información para el usuario que inicia sesión
+            // y una cookie para almacenar temporalmente información sobre un usuario que inicia sesión con un proveedor de inicio de sesión de terceros
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Configure la aplicación para el flujo basado en OAuth
+            PublicClientId = "self";
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/Token"),
+                Provider = new ApplicationOAuthProvider(PublicClientId),
+                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                // En el modo de producción establezca AllowInsecureHttp = false
+                AllowInsecureHttp = true
+            };
+
+            // Permitir que la aplicación use tokens portadores para autenticar usuarios
+            app.UseOAuthBearerTokens(OAuthOptions);
+
+            // Quitar los comentarios de las siguientes líneas para habilitar el inicio de sesión con proveedores de inicio de sesión de terceros
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            //app.UseFacebookAuthentication(
+            //    appId: "",
+            //    appSecret: "");
+
+            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            //{
+            //    ClientId = "",
+            //    ClientSecret = ""
+            //});
+        }
+    }
+}
+
+
+
+
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Presentation.RestService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de Web API
+            // Configure Web API para usar solo la autenticación de token de portador.
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //nuevo codigo
+
+            //Evita el error access-control-allow-origin
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
+            //Retornar Json
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+            config.EnableSystemDiagnosticsTracing();
+        }
+    }
+}
+
+
