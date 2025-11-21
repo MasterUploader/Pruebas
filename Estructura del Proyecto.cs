901a01da1c2bd914288c9cfd4f842b970cef4cb8
MasterUploader <MasterUploader@users.noreply.github.com>
2025-11-21 15:34:53 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,20 +1,24 @@
 SELECT
-    wo.WORKORDERID AS "Request id",
-    wo.TITLE AS "Subject",
-    wo.CREATEDTIME AS "Created time",
-    gd_main.QUEUENAME AS "Grupo",
+    wo.WORKORDERID AS [Request id],
+    wo.TITLE AS [Subject],
+    wo.CREATEDTIME AS [Created time],
+    gd_main.QUEUENAME AS [Grupo],
 
     (
-        -- Subconsulta para columna "Grupos pasados"
-        SELECT STRING_AGG(gd_sub.QUEUENAME, ', ') AS assessindex
-        FROM WO_Assessment wa
-        JOIN WO_Group_Info wfi
-            ON wa.ASSESSMENTID = wfi.ASSESSMENTID
-        LEFT JOIN QueueDefinition gd_sub
-            ON wfi.NEXTGROUPID = gd_sub.QUEUEID
-        WHERE wa.WORKORDERID = wo.WORKORDERID
-          AND wfi.NEXTGROUPID IS NOT NULL
-    ) AS "Grupos pasados"
+        SELECT STUFF(
+            (
+                SELECT DISTINCT ', ' + gd_sub.QUEUENAME
+                FROM WO_Assessment wa
+                JOIN WO_Group_Info wfi
+                    ON wa.ASSESSMENTID = wfi.ASSESSMENTID
+                LEFT JOIN QueueDefinition gd_sub
+                    ON wfi.NEXTGROUPID = gd_sub.QUEUEID
+                WHERE wa.WORKORDERID = wo.WORKORDERID
+                  AND wfi.NEXTGROUPID IS NOT NULL
+                FOR XML PATH(''), TYPE
+            ).value('.', 'NVARCHAR(MAX)')
+        ,1,2,'')
+    ) AS [Grupos pasados]
 
 FROM workorder wo
 
@@ -25,7 +29,6 @@ LEFT JOIN QueueDefinition gd_main
     ON wo_main.QUEUEID = gd_main.QUEUEID
 
 WHERE
-
     EXISTS (
         SELECT 1
         FROM WO_Assessment wa_hist
@@ -35,8 +38,10 @@ WHERE
             ON wfi_hist.NEXTGROUPID = gd_hist.QUEUEID
         WHERE
             wa_hist.WORKORDERID = wo.WORKORDERID
-            AND gd_hist.QUEUENAME IN (CAST('Receptoría SPS' AS VARCHAR(200)),
-                                                                   CAST( 'Receptoría TGU' AS VARCHAR(200))
+            AND gd_hist.QUEUENAME IN (
+                CAST('Receptoría SPS' AS VARCHAR(200)),
+                CAST('Receptoría TGU' AS VARCHAR(200))
+            )
     )
 
     AND wo.CREATEDTIME >= '2025-01-01 00:00:00'
