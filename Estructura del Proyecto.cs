f80f7bc4c0facc869104f6467e52295e161943e1
MasterUploader <MasterUploader@users.noreply.github.com>
2025-11-21 09:32:59 -0600

    Update Estructura del Proyecto.cs

--- Estructura del Proyecto.cs
+++ Estructura del Proyecto.cs
@@ -1,17 +1,10 @@
-# Web API 2.2 – 5.3.0
-Install-Package Microsoft.AspNet.WebApi -Version 5.3.0
-Install-Package Microsoft.AspNet.WebApi.Core -Version 5.3.0
-Install-Package Microsoft.AspNet.WebApi.Owin -Version 5.3.0
-Install-Package Microsoft.AspNet.WebApi.Client -Version 5.3.0
-Install-Package Microsoft.AspNet.WebApi.Cors -Version 5.3.0
-
-# OWIN (toda la familia en 4.2.3, como ya tienes los redirects)
-Install-Package Microsoft.Owin -Version 4.2.3
-Install-Package Microsoft.Owin.Host.SystemWeb -Version 4.2.3
-Install-Package Microsoft.Owin.Security -Version 4.2.3
-Install-Package Microsoft.Owin.Security.Cookies -Version 4.2.3
-Install-Package Microsoft.Owin.Security.OAuth -Version 4.2.3
-Install-Package Microsoft.Owin.Cors -Version 4.2.3
-
-# (Opcional pero recomendado) Quita paquetes ASP.NET Core que no uses en WebApi clásico
-Get-Package Microsoft.AspNetCore.* | % { Uninstall-Package $_.Id -Force -RemoveDependencies }
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+// Aliases para evitar ambigüedades y usar los atributos de WebApi 2
+using HostAuthenticationAttribute = System.Web.Http.HostAuthenticationAttribute;
+using OverrideAuthenticationAttribute = System.Web.Http.OverrideAuthenticationAttribute;
